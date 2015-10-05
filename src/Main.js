db65082ad84b653aa98e54f1484bfcc0e78bd793
Gerald Choqueux <gerald.choqueux@ign.fr>
2015-10-05 17:26:19 +0200

    require NodeMesh

--- src/Main.js
+++ src/Main.js
@@ -4,4 +4,36 @@
  * and open the template in the editor.
  */
 
-var Node = require('Scene/Node');
+/* global requirejs */
+
+
+requirejs.config({
+    baseUrl: 'src/',
+    paths : {
+       
+        'THREE' : "https://rawgit.com/mrdoob/three.js/master/build/three.min"       
+    },
+	
+	
+    shim: {
+        
+        THREE: {
+            exports: 'THREE'
+        }
+
+    },
+    
+    waitSeconds : 30
+});
+
+
+requirejs(['Renderer/NodeMesh'], 
+    function(NodeMesh) 
+    {
+                      
+        var node = new NodeMesh();
+        
+        console.log("Node = ", node);// true
+        
+    }
+);
