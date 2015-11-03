e2420479ec7c253d54f2abe9dd65a600d8f29df6
Gerald Choqueux <gerald.choqueux@ign.fr>
2015-11-03 14:40:05 +0100

    reel elevation

--- src/Renderer/Shader/GlobeVS.glsl
+++ src/Renderer/Shader/GlobeVS.glsl
@@ -6,16 +6,16 @@ varying vec3 vNormal;
 void main() {
 
         vUv = uv;
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
         vec3 dv = texture2D( dTextures_00[0], vUv ).xyz;
 
         // TODO calculer la vrai normal...
         vNormal     = normalize( position );
 
-        float df    = dv.x *0.01    ;
-                        
+        float df    = dv.x;
+                                                                                                                                                                                                                                                                                                                                                
         vec3 displacedPosition = position +  vNormal  * df;
 
         gl_Position = projectionMatrix * modelViewMatrix * vec4( displacedPosition ,1.0 );
 
-}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
