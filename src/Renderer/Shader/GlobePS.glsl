5c6a4a8136f0206dc29483db68fe80d941ffb409
Gerald Choqueux <gerald.choqueux@ign.fr>
2015-11-04 11:25:56 +0100

    Atmosphere

--- src/Renderer/Shader/GlobePS.glsl
+++ src/Renderer/Shader/GlobePS.glsl
@@ -20,10 +20,7 @@ uniform int         debug;
 varying vec2        vUv;
 
 void main() {
-    
-
-    
-
+ 
     float latitude  = bLatitude.x + periArcLati*(1.0-vUv.y);
    
     /*
@@ -32,7 +29,6 @@ void main() {
         gl_FragColor = vec4( 0.4, 0.8, 1.0, 1.0);
     else 
     */
-
 
     if(latitude < poleSud )
         gl_FragColor = vec4( 0.85, 0.85, 0.91, 1.0);
