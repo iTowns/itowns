a2131a354e1775611e563bd246c57b4f8bbb76d1
Gerald Choqueux <gerald.choqueux@ign.fr>
2016-02-25 16:36:55 +0100

    factorization

--- src/Core/Commander/Providers/IoDriver_XBIL.js
+++ src/Core/Commander/Providers/IoDriver_XBIL.js
@@ -25,19 +25,44 @@ define('Core/Commander/Providers/IoDriver_XBIL', ['Core/Commander/Providers/IoDr
 
     IoDriver_XBIL.prototype.constructor = IoDriver_XBIL;
     
-    IoDriver_XBIL.prototype.parse = function(buffer) {
+    IoDriver_XBIL.prototype.parseXBil = function(buffer) {
+        
+//        var parseMinMax = function(result,start,length) {
+//                   
+//            for (var i = start; i <  length; i++) {
+//                var val = result.floatArray[i];                   
+//                if (val > -10.0 && val !== undefined){
+//                    result.max = Math.max(result.max, val);
+//                    result.min = Math.min(result.min, val);
+//                }
+//            }            
+//        };        
         
         if (buffer){
 
             var result = new portableXBIL(buffer);
             // Compute min max using subampling
-            for (var i = 0; i < result.floatArray.byteLength; i+=64) {
+            //console.log(result.floatArray.length);
+            for (var i = 0; i < result.floatArray.length; i+=16) {
                 var val = result.floatArray[i];                   
                 if (val > -10.0 && val !== undefined){
                     result.max = Math.max(result.max, val);
                     result.min = Math.min(result.min, val);
                 }
             }
+            
+            /*
+            var subSize = result.floatArray.length/64;
+            
+            var arrayP = [];
+            
+            for (var i = 0; i < result.floatArray.length; i+= subSize )
+            {
+                arrayP.push(when(parseMinMax(result,i,subSize)));
+            }
+            
+            when.all(arrayP);
+            */
             if (result.min === 1000000)
                 return undefined;
 
@@ -46,7 +71,18 @@ define('Core/Commander/Providers/IoDriver_XBIL', ['Core/Commander/Providers/IoDr
         else
             return undefined;
     };
-
+    
+    IoDriver_XBIL.prototype.parseMinMax = function(result,start,length) {
+        
+        for (var i = 0; i < result.floatArray.length; i++) {
+            var val = result.floatArray[i];                   
+            if (val > -10.0 && val !== undefined){
+                result.max = Math.max(result.max, val);
+                result.min = Math.min(result.min, val);
+            }
+        }        
+    };
+    
     IoDriver_XBIL.prototype.read = function(url) {
 
         // TODO new Promise is supported?
@@ -60,11 +96,11 @@ define('Core/Commander/Providers/IoDriver_XBIL', ['Core/Commander/Providers/IoDr
 
             xhr.responseType = "arraybuffer";
             xhr.crossOrigin = '';          
-            xhr["parse"] = this.parse;
+            xhr["parseXBil"] = this.parseXBil;
 
             xhr.onload = function() {
                  
-                resolve(this.parse(this.response));
+                resolve(this.parseXBil(this.response));
               
             };
 
