503fe2952ddbd911a8a0125151cf074f75392208
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2016-07-07 09:58:41 +0200

    Rework UV shader parameters
    Using descriptive names for uv/uv1 helps understanding the rendering
    architecture (WGS84/PM textures etc)

--- src/Globe/TileGeometry.js
+++ src/Globe/TileGeometry.js
@@ -32,15 +32,32 @@ define('Globe/TileGeometry', [
         this.index = null;
         this.position = null;
         this.normal = null;
-        this.uv_0 = null;
-        this.uv_1 = null;
+        // 2 UV set per tile: wgs84 and pm
+        //    - wgs84: 1 texture per tile because tiles are using wgs84 projection
+        //    - pm: use multiple textures per tile.
+        //      +-------------------------+
+        //      |                         |
+        //      |     Texture 0           |
+        //      +-------------------------+
+        //      |                         |
+        //      |     Texture 1           |
+        //      +-------------------------+
+        //      |                         |
+        //      |     Texture 2           |
+        //      +-------------------------+
+        //        * u = wgs84.u
+        //        * v = textureid + v in this texture
+        this.uv = {
+            'wgs84': null,
+            'pm': null
+        };
 
         var cBuff = cache.getRessource(nSegment);
 
         if(cBuff)
         {
             this.index = cBuff.index;
-            this.uv_0 = cBuff.uv_0;
+            this.uv.wgs84 = cBuff.uv.wgs84;
         }
     }
 
@@ -58,12 +75,12 @@ define('Globe/TileGeometry', [
         this.setIndex(buffersAttrib.index);
         this.addAttribute('position', buffersAttrib.position);
         this.addAttribute('normal', buffersAttrib.normal);
-        this.addAttribute('uv', buffersAttrib.uv_0);
-        this.addAttribute('uv1', buffersAttrib.uv_1);
+        this.addAttribute('uv_wgs84', buffersAttrib.uv.wgs84);
+        this.addAttribute('uv_pm', buffersAttrib.uv.pm);
 
         buffersAttrib.position = null;
         buffersAttrib.normal= null;
-        buffersAttrib.uv_1 = null;
+        buffersAttrib.uv.pm = null;
 
         if(!cache.getRessource(params.segment))
             cache.addRessource(params.segment, buffersAttrib);
@@ -91,8 +108,8 @@ define('Globe/TileGeometry', [
         buffers.position = new Float32Array(nVertex * 3);
         buffers.bufferIndex = buffersAttrib.index === null ? new Uint32Array(triangles * 3 * 2) : null;
         buffers.normal = new Float32Array(nVertex * 3);
-        buffers.uv_0 = buffersAttrib.uv_0 === null ? new Float32Array(nVertex * 2) : null;
-        buffers.uv_1 = new Float32Array(nVertex);
+        buffers.uv.wgs84 = buffersAttrib.uv.wgs84 === null ? new Float32Array(nVertex * 2) : null;
+        buffers.uv.pm = new Float32Array(nVertex);
 
         var widthSegments = Math.max(2, Math.floor(nSeg) || 2);
         var heightSegments = Math.max(2, Math.floor(nSeg) || 2);
@@ -105,21 +122,21 @@ define('Globe/TileGeometry', [
 
         builder.Prepare(params);
 
-        var UV_0 = function(){};
-        var UV_1 = function(){};
+        var UV_WGS84 = function(){};
+        var UV_PM = function(){};
 
-        if(buffersAttrib.uv_0 === null)
-            UV_0 = function(u,v)
-            {
-                buffers.uv_0[idVertex * 2 + 0] = u;
-                buffers.uv_0[idVertex * 2 + 1] = 1 - v;
+        if(buffersAttrib.uv.wgs84 === null) {
+            UV_WGS84 = function(u,v) {
+                buffers.uv.wgs84[idVertex * 2 + 0] = u;
+                buffers.uv.wgs84[idVertex * 2 + 1] = v;
             };
+        }
 
-        if(buffersAttrib.uv_1 === null && builder.getUV_1)
-            UV_1 = function(u)
-            {
-                buffers.uv_1[idVertex] = u;
+        if(buffersAttrib.uv.pm === null && builder.getUV_PM) {
+            UV_PM = function(u) {
+                buffers.uv.pm[idVertex] = u;
             };
+        }
 
         for (y = 0; y <= heightSegments; y++) {
 
@@ -129,7 +146,7 @@ define('Globe/TileGeometry', [
 
             builder.vProjecte(v,params);
 
-            var uv_1 = builder.getUV_1(params);
+            var uv_pm = builder.getUV_PM(params);
 
             for (x = 0; x <= widthSegments; x++) {
 
@@ -151,8 +168,8 @@ define('Globe/TileGeometry', [
                 buffers.normal[id_m3 + 1] = normal.y;
                 buffers.normal[id_m3 + 2] = normal.z;
 
-                UV_0(u,v);
-                UV_1(uv_1);
+                UV_WGS84(u,v);
+                UV_PM(uv_pm);
 
                 if (y !== 0 && y !== heightSegments)
                     if (x === widthSegments)
@@ -222,8 +239,8 @@ define('Globe/TileGeometry', [
             };
 
             buildUVSkirt = function(){
-                buffers.uv_0[idVertex * 2 + 0] = buffers.uv_0[id * 2 + 0];
-                buffers.uv_0[idVertex * 2 + 1] = buffers.uv_0[id * 2 + 1];
+                buffers.uv.wgs84[idVertex * 2 + 0] = buffers.uv.wgs84[id * 2 + 0];
+                buffers.uv.wgs84[idVertex * 2 + 1] = buffers.uv.wgs84[id * 2 + 1];
             };
         }
 
@@ -244,7 +261,7 @@ define('Globe/TileGeometry', [
 
             buildUVSkirt();
 
-            buffers.uv_1[idVertex] = buffers.uv_1[id];
+            buffers.uv.pm[idVertex] = buffers.uv.pm[id];
 
             var idf = (i + 1) % skirt.length;
 
@@ -267,17 +284,17 @@ define('Globe/TileGeometry', [
             buffersAttrib.index = new THREE.BufferAttribute(buffers.bufferIndex, 1);
         buffersAttrib.position = new THREE.BufferAttribute(buffers.position, 3);
         buffersAttrib.normal= new THREE.BufferAttribute(buffers.normal, 3);
-        if(buffersAttrib.uv_0 === null)
-            buffersAttrib.uv_0 = new THREE.BufferAttribute(buffers.uv_0, 2);
-        buffersAttrib.uv_1 = new THREE.BufferAttribute(buffers.uv_1, 1);
+        if(buffersAttrib.uv.wgs84 === null)
+            buffersAttrib.uv.wgs84 = new THREE.BufferAttribute(buffers.uv.wgs84, 2);
+        buffersAttrib.uv.pm = new THREE.BufferAttribute(buffers.uv.pm, 1);
 
         javToo.freeArray(vertices);
 
         buffers.position = null;
         buffers.bufferIndex = null;
         buffers.normal = null;
-        buffers.uv_0 = null;
-        buffers.uv_1 = null;
+        buffers.uv.wgs84 = null;
+        buffers.uv.pm = null;
 
         return buffersAttrib;
 
--- src/Renderer/Shader/GlobeFS.glsl
+++ src/Renderer/Shader/GlobeFS.glsl
@@ -56,12 +56,12 @@ uniform int         debug;
 uniform vec3        lightPosition;
 uniform int         lightingOn;
 
-varying vec2        vUv_0;
-varying float       vUv_1;
+varying vec2        vUv_WGS84;
+varying float       vUv_PM;
 varying vec3        vNormal;
 varying vec4        pos;
 
-#if defined(BORDERLINE)
+#if defined(DEBUG)
     const float sLine = 0.008;
 #endif
 const float borderS = 0.007;
@@ -143,16 +143,6 @@ void main() {
 
     #endif
 
-    gl_FragColor = vec4( 1.0, 0.3, 0.0, 1.0);
-
-
-    #if defined(DEBUG)
-    if (nbTextures[1] == 0) {
-        gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
-        return;
-    }
-    #endif
-
     if(pickingRender == 1)
     {
 
@@ -165,29 +155,21 @@ void main() {
         #endif
 
     }else
-    #if defined(BORDERLINE)
-         if(vUv_0.x < sLine || vUv_0.x > 1.0 - sLine || vUv_0.y < sLine || vUv_0.y > 1.0 - sLine)
+    #if defined(DEBUG)
+         if(vUv_WGS84.x < sLine || vUv_WGS84.x > 1.0 - sLine || vUv_WGS84.y < sLine || vUv_WGS84.y > 1.0 - sLine)
              gl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0);
          else
     #endif
-    if(selected == 1 && (vUv_0.x < borderS || vUv_0.x > 1.0 - borderS || vUv_0.y < borderS || vUv_0.y > 1.0 - borderS))
+    if(selected == 1 && (vUv_WGS84.x < borderS || vUv_WGS84.x > 1.0 - borderS || vUv_WGS84.y < borderS || vUv_WGS84.y > 1.0 - borderS))
         gl_FragColor = vec4( 1.0, 0.3, 0.0, 1.0);
    else
     {
+        // Reconstruct PM uv and PM subtexture id (see TileGeometry)
         vec2 uvPM ;
-        uvPM.x           = vUv_0.x;
-        float y         = vUv_1;
-        int idd         = int(floor(y));
-        uvPM.y           = y - float(idd);
-        vec2 uvWGS84 = vec2(vUv_0.x,1.0-vUv_0.y);
-
-        // if(nbTextures[1] == idd)
-        // {
-        //     idd     = nbTextures[1] - 1 ;
-        //     uvPM.y   = 0.0;
-        // }
-
-        gl_FragColor    = vec4( 0.04, 0.23, 0.35, 1.0);
+        uvPM.x             = vUv_WGS84.x;
+        float y            = vUv_PM;
+        int pmSubTextureIndex = int(floor(y));
+        uvPM.y             = y - float(pmSubTextureIndex);
 
         #if defined(USE_LOGDEPTHBUF) && defined(USE_LOGDEPTHBUF_EXT)
             float depth = gl_FragDepthEXT / gl_FragCoord.w;
@@ -196,59 +178,67 @@ void main() {
             float fog = 1.0;
         #endif
 
-        if (0 <= idd && idd < nbTextures[1])
-        {
+        vec4 diffuseColor =  vec4( 1.0, 1.0, 1.0, 1.0);
+        int validTextureCount = 0;
 
-            vec4 params;
-            vec2 paramsB;
-            int pit;
-            bool projWGS84;
-            vec4 diffuseColor =  vec4( 1.0, 1.0, 1.0, 1.0);
+        // TODO Optimisation des uv1 peuvent copier pas lignes!!
+        for (int layer = 0; layer < 8; layer++) {
+           if(layer == nColorLayer) {
+                break;
+           }
 
-            // TODO Optimisation des uv1 peuvent copier pas lignes!!
-            for (int layer = 0; layer < 8; layer++)
-            {
+            vec4 params = getParamLayers(layerSequence[layer]);
+            vec2 paramsB = getParamBLayers(layerSequence[layer]);
 
+            if(params.z == 1.0 && params.w > 0.0) {
+                bool projWGS84 = params.y == 0.0;
+                int layerTexturesOffset = int(params.x);
+                int textureIndex = layerTexturesOffset + (projWGS84 ? 0 : pmSubTextureIndex);
 
-               if(layer == nColorLayer)
-                    break;
+                if (0 <= textureIndex && textureIndex < nbTextures[1]) {
+                    validTextureCount++;
 
-                params = getParamLayers(layerSequence[layer]);
-                paramsB = getParamBLayers(layerSequence[layer]);
+                    vec4 layerColor = colorAtIdUv(
+                        dTextures_01,
+                        pitScale_L01,
+                        textureIndex,
+                        projWGS84 ? vUv_WGS84 : uvPM);
+                    float lum = 1.0;
 
-                if(params.z == 1.0 && params.w > 0.0)
-                {
-
-                        pit = int(params.x);
-                        projWGS84 = params.y == 0.0;
-                        vec4 layerColor = colorAtIdUv(dTextures_01,pitScale_L01, pit + (projWGS84 ? 0 : idd),projWGS84 ? uvWGS84 : uvPM);
-                        float lum = 1.0;
-
-                        if(paramsB.x > 0.0)
-                        {
-                            vec3 white = vec3(1.0,1.0,1.0);
-                            vec3 coul = vec3(layerColor.xyz);
-                            float a = 1.0 - length(coul-white);
-                            a =  max(a,0.05);
-                            if(paramsB.x > 2.0)
-                            {
-                                a = (layerColor.r + layerColor.g + layerColor.b)/3.0;
-                                layerColor*= layerColor*layerColor;
-                            }
-
-                            lum = 1.0-pow(abs(a),paramsB.x);
+                    if(paramsB.x > 0.0) {
+                        vec3 white = vec3(1.0,1.0,1.0);
+                        vec3 coul = vec3(layerColor.xyz);
+                        float a = 1.0 - length(coul-white);
+                        a =  max(a,0.05);
+                        if(paramsB.x > 2.0) {
+                            a = (layerColor.r + layerColor.g + layerColor.b)/3.0;
+                            layerColor*= layerColor*layerColor;
                         }
 
-                        diffuseColor = mix( diffuseColor,layerColor, lum*params.w);
+                        lum = 1.0-pow(abs(a),paramsB.x);
+                    }
+
+                    diffuseColor = mix( diffuseColor,layerColor, lum*params.w);
                 }
+#if defined(DEBUG)
+                else {
+                    // Invalid texture -> error color
+                    diffuseColor = vec4(1.0, 0.0, 1.0, 1.0);
+                }
+#endif
 
             }
 
-            gl_FragColor = RTC == 1 ? mix(fogColor, diffuseColor, fog ) : diffuseColor;
-
         }
 
-        if(lightingOn == 1){   // Add lighting
+        if (validTextureCount > 0) {
+            gl_FragColor = RTC == 1 ? mix(fogColor, diffuseColor, fog ) : diffuseColor;
+        } else {
+            gl_FragColor = vec4( 0.04, 0.23, 0.35, 1.0);
+        }
+
+
+        if(lightingOn == 1) {   // Add lighting
             float light = min(2. * dot(vNormal, lightPosition),1.);
             gl_FragColor.rgb *= light;
         }
