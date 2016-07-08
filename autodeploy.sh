fb2ecf457eb6050120b301f3572e8bd353ee7833
Gregoire Maillet <gregoire.maillet@gmail.com>
2016-07-08 11:59:57 +0200

    Add an automatic deployment on gh-pages
    Creation of npm run doc command to generate API documentation with jsdoc
    Adding autodeploy.sh in after_sucess travis using a deploy_key that is stored encoded on github, decoded with openssl in .travis.yml
    this is based on that example (https://gist.github.com/domenic/ec8b0fc8ab45f39403dd)

---
+++ autodeploy.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -xe # Verbose output and exit with nonzero exit code if anything fails
+
+# There are two npm versions used in travis (.travis.yml)
+# and only one deploy is needed
+if [ "$TRAVIS_NODE_VERSION" != "4" ]; then
+	echo "Skipping deploy for npm version != 4"
+	exit 0
+fi
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+# Commits to other branches shouldn't try to deploy
+if [ "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy for a commit on non-master branch"
+    exit 0
+fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+COMMIT_AUTHOR_EMAIL=`git show  --pretty=format:"%ae" -q`
+COMMIT_AUTHOR_NAME=`git show  --pretty=format:"%an" -q`
+
+# Clone the existing gh-pages for this repo into out/
+git clone $REPO --single-branch --branch $TARGET_BRANCH out
+
+# Copy build results
+cp -R dist out/
+# generate the API documentation
+npm run doc -- -d out/API_Doc
+# Copy demo
+cp index.html out/
+# Copy the decoded deploy key (decoding made with openssl command in .travis.yml)
+cp deploy_key out/
+
+# Now let's go have some fun with the cloned repo
+cd out
+git config user.name "$COMMIT_AUTHOR_NAME"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# Commit the "changes", i.e. the new version.
+git add dist index.html API_Doc
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
