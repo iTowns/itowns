4aed6a4dead087c1572da477fe20aff374fb9e91
Jeremy Gaillard <jeremy.c.gaillard@gmail.com>
2016-08-18 17:20:48 +0200

    Individual coloration of buildings in building tiles
    The building material has a table of colors uniform from which the shader will pick depending on the index of the geometry.
    
    The uniform can be updated to change the color during execution.

--- src/Renderer/MultiGeometryMaterial.js
+++ src/Renderer/MultiGeometryMaterial.js
@@ -5,11 +5,26 @@
  */
 
 
+import THREE from 'THREE';
 import BasicMaterial from 'Renderer/BasicMaterial';
+import MultiGeometryFS from 'Renderer/Shader/MultiGeometryFS.glsl';
 
-function MultiGeometryMaterial(color, id) {
+function MultiGeometryMaterial(color, id, geometryCount) {
     //Constructor
     BasicMaterial.call(this, color, id);
+
+	this.fragmentShader = this.fragmentShaderHeader +
+        "#define GEOMETRY_COUNT " + geometryCount + "\n" + MultiGeometryFS;
+
+    var colors = [];
+    for(var i = 0; i < geometryCount; i++) {
+        colors[i] = new THREE.Vector3(1,1,1);
+    }
+
+    this.uniforms.colors = {
+        type: "v3v",
+        value: colors
+    };
 }
 
 MultiGeometryMaterial.prototype = Object.create(BasicMaterial.prototype);
@@ -22,5 +37,9 @@ MultiGeometryMaterial.prototype.setSelected = function(selected, index) {
 MultiGeometryMaterial.prototype.getSelectedIndex = function() {
     return this.uniforms.selected.value ? this.uniforms.selected.value - 1 : null;
 };
+
+MultiGeometryMaterial.prototype.updateColors = function(colorArray) {
+    this.uniforms.colors.value = colorArray;
+}
 
 export default MultiGeometryMaterial;
