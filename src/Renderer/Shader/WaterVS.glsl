5dac9e8c3c61071b03171b6dfbcaf64a0fe7b4e2
qdnguyen <nguyen.quocdinh@gmail.com>
2016-09-21 14:38:18 +0200

    water shader

--- src/Renderer/Shader/WaterVS.glsl
+++ src/Renderer/Shader/WaterVS.glsl
@@ -9,13 +9,13 @@ varying vec3 surfaceZ;
 varying vec2 vuv;
 void main()
 {
-    vuv = uv;
-    mirrorCoord = modelMatrix * vec4(position, 1.0);
-    worldPosition = mirrorCoord.xyz;
-    modelPosition = position;
-    surfaceX = vec3( modelMatrix[0][0], modelMatrix[0][1], modelMatrix[0][2]);
-    surfaceY = vec3( modelMatrix[1][0], modelMatrix[1][1], modelMatrix[1][2]);
-    surfaceZ = vec3( modelMatrix[2][0], modelMatrix[2][1], modelMatrix[2][2]);
-    mirrorCoord = textureMatrix * mirrorCoord;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+vuv = uv;
+  mirrorCoord = modelMatrix * vec4(position, 1.0);
+  worldPosition = mirrorCoord.xyz;
+  modelPosition = position;
+  surfaceX = vec3( modelMatrix[0][0], modelMatrix[0][1], modelMatrix[0][2]);
+  surfaceY = vec3( modelMatrix[1][0], modelMatrix[1][1], modelMatrix[1][2]);
+  surfaceZ = vec3( modelMatrix[2][0], modelMatrix[2][1], modelMatrix[2][2]);
+  mirrorCoord = textureMatrix * mirrorCoord;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
