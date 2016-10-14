10cacb6b55b5e110856a6ff6dc2b2aeb9138a279
qdnguyen <nguyen.quocdinh@gmail.com>
2016-10-14 15:18:30 +0200

    add arrow to show direction of site

--- src/Core/Commander/Providers/GeoidUtils.js
+++ src/Core/Commander/Providers/GeoidUtils.js
@@ -4,18 +4,31 @@ import IoDriverTXT from 'Core/Commander/Providers/IoDriver_TXT';
 import CoordCarto from 'Core/Geographic/CoordCarto';
 import Text2Json from 'Text2Json';
 
-function _toCartesian(elem,ellipsoid) {
+
+function _toCartesian(elem, ellipsoid) {
     var coordCarto = new CoordCarto();
 
     var longitude = elem[2];
     var latitude  = elem[1];
     var elevation = elem[3];
 
-    return ellipsoid.cartographicToCartesian(coordCarto.setFromDegreeGeo(longitude,latitude,elevation));
-}
 
+    var origin =  ellipsoid.cartographicToCartesian(coordCarto.setFromDegreeGeo(longitude,latitude,elevation));
+    //var tangent = new THREE.Vector3(-origin.y, origin.x, origin.z).normalize();
 
-function _matrixify( source, count )
+    var e       = 10000;
+
+    //O + (e * v_est, e * v_nord, e * v_up)
+    var v_east  = elem[4];
+    var v_north = elem[5];
+    var v_h    = elem[6];
+
+    var direction = new THREE.Vector3(v_east, v_north, v_h).multiplyScalar(e/(365*24*60*60));
+    
+    return {position: origin, direction: direction};
+};
+
+function _matrixify(source, count )
 {
     var matrixified = [];
     var tmp;
@@ -35,7 +48,7 @@ function _matrixify( source, count )
     }
     // return the result
     return matrixified;
-}
+};
 
 function _standarlizeData(data){
     var TXT = Text2Json(data);
@@ -45,29 +58,33 @@ function _standarlizeData(data){
     return tmp.reduce(function(prev, curr) {
                         return prev.concat(curr.split(',').map(parseFloat));
             }, []);
-}
+};
 
 function _GeoidDataToThreeJS(data,ellipsoid) {
     if (!data) {
         return undefined;
     }
 
+    var group = new THREE.Object3D();
+    
     var jdata = _matrixify(_standarlizeData(data),7);
-    
-    var pos = _toCartesian(jdata[0], ellipsoid);
-    
-    var target = new THREE.Vector3(0, 0, 0);
-    var direction = new THREE.Vector3().sub(target, pos);
-    //return new THREE.ArrowHelper(direction.clone().normalize(), pos, direction.length(), 0x00ff00);
-    
-    var material = new THREE.MeshBasicMaterial({ color: 0xff0000 }); 
-    var geometry = new THREE.SphereGeometry(500, 64, 64); 
-    var sphere = new THREE.Mesh(geometry, material); 
-    sphere.position.copy(pos);
-    return sphere;
-}
 
-export default function loadGeoidData(urlFile,ellipsoid) {
+    for(var i = 0; i < 1; i++){//jdata.length
+        var tmp = _toCartesian(jdata[i], ellipsoid);
+        //group.add(new THREE.ArrowHelper(tmp.direction.clone().normalize(), tmp.position, 100, 0x00ff00));
+       return  new THREE.ArrowHelper(tmp.direction.clone().normalize(), tmp.position, 10000, 0x00ff00);
+       /* var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        var geometry = new THREE.SphereGeometry(50000, 64, 64);
+        var sphere = new THREE.Mesh(geometry, material);
+            sphere.position.copy(tmp.position);
+        */
+        group.add(sphere);    
+    }    
+       
+    return group;
+};
+
+export default function loadGeoidData(urlFile,ellipsoid){
     var IoDriver = new IoDriverTXT();
 
     return IoDriver.read(urlFile).then(
