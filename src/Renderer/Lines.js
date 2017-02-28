e5c189be5e08dd201286b02da811a8bb29a7dcee
Gerald Choqueux <gerald.choqueux@ign.fr>
2017-02-28 14:11:26 +0100

    refactor (Material) : rename material

--- src/Renderer/Lines.js
+++ src/Renderer/Lines.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import ItownsLineMaterial from './ItownsLineMaterial';
+import LineMaterial from './LineMaterial';
 
 const Lines = function Lines(options) {
     THREE.Mesh.call(this);
@@ -13,7 +13,7 @@ const Lines = function Lines(options) {
     this.uvs = [];
 
     this.geometry = new THREE.BufferGeometry();
-    this.material = new ItownsLineMaterial(options);
+    this.material = new LineMaterial(options);
     this.widthCallback = null;
 };
 
