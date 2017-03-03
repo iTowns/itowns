4921ef0c84acd43cdd9d036aa32debf5747fdc47
Gerald Choqueux <gerald.choqueux@ign.fr>
2017-03-03 12:33:28 +0100

    feature (material) : add RTC in lines and points   * Points   * Lines   * PointMaterial   * LineMaterial

--- src/Renderer/Shader/LineVS.glsl
+++ src/Renderer/Shader/LineVS.glsl
@@ -1,26 +1,3 @@
-#version 100
-
-precision highp float;
-precision highp int;
-
-//#define SHADER_NAME LineShaderMaterial
-
-#define USE_LOGDEPTHBUF
-#define USE_LOGDEPTHBUF_EXT
-
-#ifdef USE_LOGDEPTHBUF
-
-    #define EPSILON 1e-6
-    #ifdef USE_LOGDEPTHBUF_EXT
-
-        varying float vFragDepth;
-
-    #endif
-
-    uniform float logDepthBufFC;
-
-#endif
-
 attribute vec3  position;
 attribute vec3  previous;
 attribute vec3  next;
@@ -29,16 +6,18 @@ attribute float side;
 attribute float width;
 attribute vec2 uv;
 
+uniform bool        useRTC;
 
-uniform mat4   projectionMatrix;
-uniform mat4   modelViewMatrix;
+uniform mat4 mVPMatRTC;
+uniform mat4 projectionMatrix;
+uniform mat4 modelViewMatrix;
 
-uniform float	THICKNESS;	// the thickness of the line in pixels
-uniform float	MITER_LIMIT;	// 1.0: always miter, -1.0: never miter, 0.75: default
-uniform vec2	WIN_SCALE;	// the size of the viewport in pixels
+uniform float THICKNESS;  // the thickness of the line in pixels
+uniform bool MITER_LIMIT;    // 1.0: always miter, -1.0: never miter, 0.75: default
+uniform vec2 WIN_SCALE;  // the size of the viewport in pixels
 
 uniform float opacity;
-uniform float sizeAttenuation;
+uniform bool sizeAttenuation;
 uniform vec3 color;
 
 varying vec2 vUV;
@@ -48,7 +27,7 @@ varying vec3 vPosition;
 
 vec2 screen_space(vec4 vertex)
 {
-	return vec2( vertex.xy / vertex.w ) * WIN_SCALE;
+    return vec2( vertex.xy / vertex.w ) * WIN_SCALE;
 }
 
 vec2 fix( vec4 i, float aspect ) {
@@ -59,86 +38,100 @@ vec2 fix( vec4 i, float aspect ) {
 
 void main() {
 
-                float aspect = WIN_SCALE.x / WIN_SCALE.y;
-        	float pixelWidthRatio = 1. / (WIN_SCALE.x * projectionMatrix[0][0]);
-                vColor = vec4( color, opacity );
-                vUV = uv;
+    float aspect = WIN_SCALE.x / WIN_SCALE.y;
+    float pixelWidthRatio = 1. / (WIN_SCALE.x * projectionMatrix[0][0]);
+    vColor = vec4( color, opacity );
+    vUV = uv;
 
-		vec4 projectedPrePoint = projectionMatrix * modelViewMatrix * vec4(previous,1.0);
-		vec4 projectedCurPoint = projectionMatrix * modelViewMatrix *vec4(position,1.0);
-		vec4 projectedNextPoint = projectionMatrix * modelViewMatrix *vec4(next,1.0);
+    mat4 projModelViewMatrix = useRTC ? mVPMatRTC : projectionMatrix * modelViewMatrix;
 
-                #ifdef USE_LOGDEPTHBUF
+    vec4 projectedPrePoint = projModelViewMatrix * vec4(previous,1.0);
+    vec4 projectedCurPoint = projModelViewMatrix * vec4(position,1.0);
+    vec4 projectedNextPoint = projModelViewMatrix * vec4(next,1.0);
 
-                    projectedPrePoint.z = log2(max( EPSILON, projectedPrePoint.w + 1.0 )) * logDepthBufFC;
-                    projectedCurPoint.z = log2(max( EPSILON, projectedCurPoint.w + 1.0 )) * logDepthBufFC;
-                    projectedNextPoint.z = log2(max( EPSILON, projectedNextPoint.w + 1.0 )) * logDepthBufFC;
+    #ifdef USE_LOGDEPTHBUF
 
-                    #ifdef USE_LOGDEPTHBUF_EXT
+        #ifndef USE_LOGDEPTHBUF_EXT
 
-                        vFragDepth = 1.0 + gl_Position.w;
+            projectedPrePoint.z = (projectedPrePoint.z - 1.0) * projectedPrePoint.w;
+            projectedCurPoint.z = (projectedCurPoint.z - 1.0) * projectedCurPoint.w;
+            projectedNextPoint.z = (projectedNextPoint.z - 1.0) * projectedNextPoint.w;
 
-                    #else
+        #else
 
-                        projectedPrePoint.z = (projectedPrePoint.z - 1.0) * projectedPrePoint.w;
-                        projectedCurPoint.z = (projectedCurPoint.z - 1.0) * projectedCurPoint.w;
-                        projectedNextPoint.z = (projectedNextPoint.z - 1.0) * projectedNextPoint.w;
+            projectedPrePoint.z = log2(max( EPSILON, projectedPrePoint.w + 1.0 )) * logDepthBufFC;
+            projectedCurPoint.z = log2(max( EPSILON, projectedCurPoint.w + 1.0 )) * logDepthBufFC;
+            projectedNextPoint.z = log2(max( EPSILON, projectedNextPoint.w + 1.0 )) * logDepthBufFC;
 
-                    #endif
+        #endif
 
-                #endif
+    #endif
 
-                
-                vec2 previousScreen = fix( projectedPrePoint,aspect );	// start of previous segment
-                vec2 currentScreen = fix( projectedCurPoint ,aspect);	// end of previous segment, start of current segment
-                vec2 nextScreen = fix( projectedNextPoint ,aspect);	// end of current segment, start of next segment
+    vec2 previousScreen = fix( projectedPrePoint,aspect );  // start of previous segment
+    vec2 currentScreen = fix( projectedCurPoint ,aspect);   // end of previous segment, start of current segment
+    vec2 nextScreen = fix( projectedNextPoint ,aspect); // end of current segment, start of next segment
 
+    float pixelWidth = projectedCurPoint.w * pixelWidthRatio;
+    float w = 1.8*pixelWidth * THICKNESS * width;
 
-                float pixelWidth = projectedCurPoint.w * pixelWidthRatio;
-                float w = 1.8*pixelWidth * THICKNESS * width;
+    if(sizeAttenuation) {
+            w = 1.8 * THICKNESS * width;
+    }
 
-                if(sizeAttenuation == 1.0) {
-                        w = 1.8 * THICKNESS * width;
-                }
+    //starting point uses (next - current)
+    vec2 dir = vec2(0.0);
+    //float len = THICKNESS;
 
-                //starting point uses (next - current)
-                vec2 dir = vec2(0.0);
-                //float len = THICKNESS;
+    if (currentScreen == previousScreen) {
+            dir = normalize(nextScreen - currentScreen);
+    }
+    //ending point uses (current - previous)
+    else if (currentScreen == nextScreen) {
+           dir = normalize(currentScreen - previousScreen);
+    }
+    //somewhere in middle, needs a join
+    else {
+             //get directions from (C - B) and (B - A)
+             vec2 dirA = normalize((currentScreen - previousScreen));
+             if (MITER_LIMIT) {
+                    vec2 dirB = normalize(nextScreen - currentScreen);
+                    //now compute the miter join normal and length
+                    vec2 tangent = normalize(dirA + dirB);
+                    vec2 perp = vec2(-dirA.y, dirA.x);
+                    vec2 miter = vec2(-tangent.y, tangent.x);
+                    dir = tangent;
+                    //len = THICKNESS / dot(miter, perp);
+                    //w = clamp( w / dot( miter, perp ), 0., 4. * THICKNESS * width );
+            } else {
+                 dir = dirA;
+            }
+    }
 
-                if (currentScreen == previousScreen) {
-                        dir = normalize(nextScreen - currentScreen);
-                } 
-                //ending point uses (current - previous)
-                else if (currentScreen == nextScreen) {
-                       dir = normalize(currentScreen - previousScreen);
-                }
-                //somewhere in middle, needs a join
-                else {
-                         //get directions from (C - B) and (B - A)
-                         vec2 dirA = normalize((currentScreen - previousScreen));
-                         if (MITER_LIMIT == 1.0) {
-                                vec2 dirB = normalize(nextScreen - currentScreen);
-                                //now compute the miter join normal and length
-                                vec2 tangent = normalize(dirA + dirB);
-                                vec2 perp = vec2(-dirA.y, dirA.x);
-                                vec2 miter = vec2(-tangent.y, tangent.x);
-                                dir = tangent;
-                                //len = THICKNESS / dot(miter, perp);
-                                //w = clamp( w / dot( miter, perp ), 0., 4. * THICKNESS * width );
-                        } else {
-                             dir = dirA;
-                        }
-                }
+    vec2 normal = vec2(-dir.y, dir.x);
+    normal.x /= aspect;
+    normal *= 0.5 * w;
+    //normal *= len/2.0;
 
-                vec2 normal = vec2(-dir.y, dir.x);
-                normal.x /= aspect;
-                normal *= 0.5 * w;
-                //normal *= len/2.0;
+    vec4 offset = vec4(normal*side, 0.0, 1.0);
+    projectedCurPoint.xy += offset.xy;
 
-                vec4 offset = vec4(normal*side, 0.0, 1.0);
-                projectedCurPoint.xy += offset.xy;
-               
-                vPosition    = ( modelViewMatrix * vec4( position, 1. ) ).xyz;
-                gl_Position  = projectedCurPoint;
-                gl_PointSize = 10.0;
+    vPosition    = ( modelViewMatrix * vec4( position, 1. ) ).xyz;
+    gl_Position  = projectedCurPoint;
+    gl_PointSize = 10.0;
+
+    #ifdef USE_LOGDEPTHBUF
+
+        gl_Position.z = log2(max( EPSILON, gl_Position.w + 1.0 )) * logDepthBufFC;
+
+        #ifdef USE_LOGDEPTHBUF_EXT
+
+            vFragDepth = 1.0 + gl_Position.w;
+
+        #else
+
+            gl_Position.z = (gl_Position.z - 1.0) * gl_Position.w;
+
+        #endif
+
+    #endif
 }
--- src/Renderer/Shader/SimpleVS.glsl
+++ src/Renderer/Shader/SimpleVS.glsl
@@ -1,38 +1,17 @@
-#version 100
-
-precision highp float;
-precision highp int;
-
 #define SHADER_NAME SimpleMaterial
-#define VERTEX_TEXTURES
 
-#define USE_LOGDEPTHBUF
-#define USE_LOGDEPTHBUF_EXT
-
-#ifdef USE_LOGDEPTHBUF
-
-    #define EPSILON 1e-6
-    #ifdef USE_LOGDEPTHBUF_EXT
-
-        varying float vFragDepth;
-
-    #endif
-
-    uniform float logDepthBufFC;
-
-#endif
-
-
-//attribute vec2      uv;
 attribute vec3      position;
 attribute vec3      normal;
+attribute vec3      color;
 
 uniform mat4        projectionMatrix;
 uniform mat4        modelViewMatrix;
 
 uniform mat4       mVPMatRTC;
 uniform bool       useRTC;
+uniform bool       lightOn;
 varying float      light;
+varying vec3       vColor;
 
 // IE error : Initializer for const variable must initialize to a constant value
 //const vec3 dir =  normalize(vec3(1.0,1.0,0.5));
@@ -41,14 +20,18 @@ void main()
 {
   vec3 dir =  normalize(vec3(1.0,1.0,0.5));
 
-  if(useRTC)
+  vColor = color;
+
+  if (useRTC) {
         gl_Position = mVPMatRTC * vec4( position ,1.0 );
-  else
+  }
+  else {
         gl_Position = projectionMatrix * modelViewMatrix * vec4( position,  1.0 );
+  }
 
-    float h  = max(0.05,(1.0 - min(position.y / 50.0,1.0)));
-
-    light    =   h / max(0.25,dot(dir,normal));
+    if (lightOn) {
+        light = (0.666 + 0.333 * dot(dir,normal));
+    }
 
     #ifdef USE_LOGDEPTHBUF
 
