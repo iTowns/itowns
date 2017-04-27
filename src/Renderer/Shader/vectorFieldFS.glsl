ae2df37fbd9d4787fb019db75a5de82b5911d7b0
Alexandre Devaux <heythatsme@gmail.com>
2017-04-27 15:10:20 +0200

    doint remember

--- src/Renderer/Shader/vectorFieldFS.glsl
+++ src/Renderer/Shader/vectorFieldFS.glsl
@@ -24,6 +24,7 @@ void main()
 
     #endif
 
+
     vec2 coord = gl_PointCoord - vec2(0.5);  //from [0,1] to [-0.5,0.5]
     if(length(coord) > 0.5)                  //outside of circle radius?
         discard;
