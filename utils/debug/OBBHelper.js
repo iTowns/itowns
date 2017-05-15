c5b8b29f5615688459665655e3d1d4a5101511b2
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2017-05-15 11:37:08 +0200

    cleanup: clean OBB/OBBHelper code

--- utils/debug/OBBHelper.js
+++ utils/debug/OBBHelper.js
@@ -24,6 +24,7 @@ function OBBHelper(OBB, text) {
 
     THREE.LineSegments.call(this, geometry, new THREE.LineBasicMaterial({
         color: color.getHex(),
+        linewidth: 3,
     }));
 
     this.frustumCulled = false;
@@ -60,36 +61,15 @@ OBBHelper.prototype.setMaterialVisibility = function setMaterialVisibility(show)
 };
 
 OBBHelper.prototype.update = function update(OBB) {
-    var box = OBB.box3D;
-    var min = box.min;
-    var max = box.max;
     var position = this.geometry.attributes.position;
     var array = position.array;
 
-    array[0] = max.x;
-    array[1] = max.y;
-    array[2] = max.z;
-    array[3] = min.x;
-    array[4] = max.y;
-    array[5] = max.z;
-    array[6] = min.x;
-    array[7] = min.y;
-    array[8] = max.z;
-    array[9] = max.x;
-    array[10] = min.y;
-    array[11] = max.z;
-    array[12] = max.x;
-    array[13] = max.y;
-    array[14] = min.z;
-    array[15] = min.x;
-    array[16] = max.y;
-    array[17] = min.z;
-    array[18] = min.x;
-    array[19] = min.y;
-    array[20] = min.z;
-    array[21] = max.x;
-    array[22] = min.y;
-    array[23] = min.z;
+    const pts = OBB._points();
+    for (let i = 0; i < pts.length; i++) {
+        array[i * 3] = pts[i].x;
+        array[i * 3 + 1] = pts[i].y;
+        array[i * 3 + 2] = pts[i].z;
+    }
 
     position.needsUpdate = true;
 
