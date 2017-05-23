42b8d7fcfccd459b2afb08df3c0cfbe03af0b087
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2017-05-23 17:41:23 +0200

    fix: correct ctor and isInside method

--- src/Core/Geographic/Extent.js
+++ src/Core/Geographic/Extent.js
@@ -47,7 +47,7 @@ function Extent(crs, ...values) {
             this._values = new Float64Array(4);
             for (let i = 0; i < values.length; i++) {
                 for (let j = 0; j < 2; j++) {
-                    this._values[2 * i + j] = values[i][j];
+                    this._values[2 * i + j] = values[i]._values[j];
                 }
             }
         } else if (values.length == 1 && values[0].west != undefined) {
@@ -125,6 +125,7 @@ Extent.prototype.offsetToParent = function offsetToParent(other) {
     const scale =
         Math.abs(
             this.east(other._internalStorageUnit) - this.west(other._internalStorageUnit)) / dimension.x;
+
     return new THREE.Vector3(originX, originY, scale);
 };
 
@@ -188,7 +189,7 @@ Extent.prototype.dimensions = function dimensions(unit) {
  *
  * @param point {[object Object]}
  */
-Extent.prototype.isInside = function isInside(coord) {
+Extent.prototype.pointIsInside = function pointIsInside(coord) {
     const c = (this.crs() == coord.crs) ? coord : coord.as(this.crs());
 
     // TODO this ignores altitude
