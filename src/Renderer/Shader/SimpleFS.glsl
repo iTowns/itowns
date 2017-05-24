39590b1d7c842589ce3dd8907e2dbbe27b9c62f5
Jérémy Gaillard <jeremy.gaillard@oslandia.com>
2017-05-24 13:26:02 +0200

    fix: SimpleFS and SimpleVS no longer have obsolete header
    Header is added at runtime when instantiating a new material

--- src/Renderer/Shader/SimpleFS.glsl
+++ src/Renderer/Shader/SimpleFS.glsl
@@ -1,30 +1,4 @@
-#version 100
-
-#extension GL_EXT_frag_depth : enable
-
-#define SHADER_NAME simpleMaterial
-#define VERTEX_TEXTURES
-
-precision highp float;
-precision highp int;
-
-#define USE_LOGDEPTHBUF
-#define USE_LOGDEPTHBUF_EXT
-
-#ifdef USE_LOGDEPTHBUF
-
-	uniform float logDepthBufFC;
-
-	#ifdef USE_LOGDEPTHBUF_EXT
-
-		varying float vFragDepth;
-
-	#endif
-
-#endif
-
 uniform vec3 diffuseColor;
-uniform bool lightingEnabled;
 varying float light;
 
 void main() {
@@ -35,7 +9,5 @@ void main() {
 
     #endif
 
-    vec4 color = lightingEnabled ? vec4( diffuseColor / light,1.0) : vec4( diffuseColor, 1.0);
-
-    gl_FragColor = color;
+    gl_FragColor = vec4(diffuseColor * light, 1.0);
 }
