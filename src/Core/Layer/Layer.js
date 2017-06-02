4de1aab793ba073fd333d6da9ff6e75f003dd65a
Gerald Choqueux <gerald.choqueux@ign.fr>
2017-06-02 14:58:12 +0200

    fix (ImageryLayers) : moveLayerToIndex

--- src/Core/Layer/Layer.js
+++ src/Core/Layer/Layer.js
@@ -114,13 +114,26 @@ Layer.prototype = Object.create(EventDispatcher.prototype);
 Layer.prototype.constructor = Layer;
 
 const ImageryLayers = {
+    // move layer to new index
+    // After the modification :
+    //      * the minimum sequence will always be 0
+    //      * the maximum sequence will always be layers.lenght - 1
+    // the ordering of all layers (Except that specified) doesn't change
     moveLayerToIndex: function moveLayerToIndex(layer, newIndex, imageryLayers) {
-        var oldIndex = layer.sequence;
+        newIndex = Math.min(newIndex, imageryLayers.length - 1);
+        newIndex = Math.max(newIndex, 0);
+        const oldIndex = layer.sequence;
+
         for (const imagery of imageryLayers) {
-            if (imagery.sequence === newIndex) {
-                imagery.sequence = oldIndex;
-                layer.sequence = newIndex;
-                break;
+            if (imagery.id === layer.id) {
+                // change index of specified layer
+                imagery.sequence = newIndex;
+            } else if (imagery.sequence > oldIndex && imagery.sequence <= newIndex) {
+                // down all layers between the old index and new index (to compensate the deletion of the old index)
+                imagery.sequence--;
+            } else if (imagery.sequence >= newIndex && imagery.sequence < oldIndex) {
+                // up all layers between the new index and old index (to compensate the insertion of the new index)
+                imagery.sequence++;
             }
         }
     },
