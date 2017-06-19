5f455ad43f9464080958d5754647f9f2f1455f2b
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2017-06-19 16:51:27 +0200

    chore: update three.js 0.86

--- src/Renderer/Camera.js
+++ src/Renderer/Camera.js
@@ -4,23 +4,12 @@
  * Description: La camera scene, interface avec la camera du 3DEngine.
  */
 
-/* global Float64Array*/
-
 import * as THREE from 'three';
 
 function Camera(width, height, options = {}) {
     this.ratio = width / height;
 
     this.camera3D = options.camera ? options.camera : new THREE.PerspectiveCamera(30, this.ratio);
-
-
-    if (!options.camera) {
-        // do not modify a camera we don't own fully
-        // /!\ WARNING Matrix JS are in Float32Array
-        this.camera3D.matrixWorld.elements = new Float64Array(16);
-        this.camera3D.matrixAutoUpdate = false;
-        this.camera3D.rotationAutoUpdate = false;
-    }
 
     this._viewMatrix = new THREE.Matrix4();
     this._visibilityTestingOffset = new THREE.Vector3();
@@ -51,7 +40,6 @@ Camera.prototype.resize = function resize(width, height) {
 
 Camera.prototype.update = function update() {
     // update matrix
-    this.camera3D.updateMatrix();
     this.camera3D.updateMatrixWorld();
 
     // keep our visibility testing matrix ready
