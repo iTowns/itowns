ff7008c405dce84aea25e94c1720e6a3089b3c7d
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2017-07-21 13:22:23 +0200

    fix: don't block subdivision on frozen layers

--- src/Process/SubdivisionControl.js
+++ src/Process/SubdivisionControl.js
@@ -10,13 +10,16 @@ export default {
         // Prevent subdivision if node is covered by at least one elevation layer
         // and if node doesn't have a elevation texture yet.
         for (const e of context.elevationLayers) {
-            if (e.tileInsideLimit(node, e) && !node.isElevationLayerLoaded()) {
+            if (!e.frozen && e.tileInsideLimit(node, e) && !node.isElevationLayerLoaded()) {
                 return false;
             }
         }
 
         // Prevent subdivision if missing color texture
         for (const c of context.colorLayers) {
+            if (c.frozen || !c.visible) {
+                continue;
+            }
             if (c.tileInsideLimit(node, c) && !node.isColorLayerLoaded(c.id)) {
                 return false;
             }
