8dde254955c1c2f65d2816b093e1f8440ada9ba9
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2017-07-24 10:59:26 +0200

    style: fix new linters errors

--- src/Core/Scheduler/Providers/Provider.js
+++ src/Core/Scheduler/Providers/Provider.js
@@ -1,33 +1,15 @@
-/**
- * Generated On: 2015-10-5
- * Class: Provider
- * Description: Generic class to serve data, local or distant, nodes and services.
- */
-
 function Provider(iodriver) {
-    // Constructor
-
     this.type = null;
     this._IoDriver = iodriver;
 }
 
 Provider.prototype.constructor = Provider;
 
-Provider.prototype.get = function get(/* url*/) {
-    // TODO: Implement Me
-
-};
-
 /**
  * preprocessLayer will be called each time a layer is added.
  * Allows the Provider to perform precomputations on the layer
  */
-Provider.prototype.preprocessLayer = function preprocessLayer(/* layer*/) {
-
-};
-
-Provider.prototype.getInCache = function getInCache(/* url*/) {
-    // TODO: Implement Me
+Provider.prototype.preprocessLayer = function preprocessLayer(/* layer */) {
 
 };
 
--- utils/debug/OBBHelper.js
+++ utils/debug/OBBHelper.js
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/* global Uint16Array Float32Array*/
-
 import * as THREE from 'three';
 
 // TODO regler le probleme glsl
