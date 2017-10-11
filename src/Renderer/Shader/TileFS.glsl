iTowns/itowns

commit b2e616c6507b359638bac824c2b2ea5bbfa304e1

fix (globe): workaround texture issues at North Pole
This commit is a workaround (rather than a proper fix) to avoid
visual glitches at the North Pole.

We simply transmit the number of PM textures to the shader and test
the texture index against this value before using it.

src/Renderer/Shader/TileFS.glsl
@@ -95,7 +95,19 @@ void main() {
 
                 if(paramsA.w > 0.0) {
                     bool projWGS84 = paramsA.y == 0.0;
+                    int pmTextureCount = int(paramsA.y);
                     int textureIndex = int(paramsA.x) + (projWGS84 ? 0 : pmSubTextureIndex);
+
+                    if (!projWGS84 && pmTextureCount <= textureIndex) {
+                        continue;
+                    }
+
+                    #if defined(DEBUG)
+                    if (showOutline && !projWGS84 && (uvPM.x < sLine || uvPM.x > 1.0 - sLine || uvPM.y < sLine || uvPM.y > 1.0 - sLine)) {
+                        gl_FragColor = COrange;
+                        return;
+                    }
+                    #endif
 
                     /* if (0 <= textureIndex && textureIndex < loadedTexturesCount[1]) */ {
 

src/Renderer/LayeredMaterial.js
@@ -375,7 +375,7 @@ LayeredMaterial.prototype.pushLayer = function pushLayer(param) {
     this.uniforms.paramLayers.value[newIndex] = new THREE.Vector4();
 
     this.setTextureOffsetByLayerIndex(newIndex, offset);
-    this.setLayerUV(newIndex, param.tileMT === 'PM' ? 1 : 0);
+    this.setLayerUV(newIndex, param.tileMT === 'PM' ? param.texturesCount : 0);
     this.setLayerFx(newIndex, param.fx);
     this.setLayerOpacity(newIndex, param.opacity);
     this.setLayerVisibility(newIndex, param.visible);

commit 77d44021eb8ecd5c258bbd5c9c3ca539adb5d851

fix: restore PM layers texture display
This was broken in PR https://github.com/iTowns/itowns/pull/492

This commit fixes the issue by comparing the texture count to the
correct index.

Fixes #525

src/Renderer/Shader/TileFS.glsl
@@ -98,7 +98,7 @@ void main() {
                     int pmTextureCount = int(paramsA.y);
                     int textureIndex = int(paramsA.x) + (projWGS84 ? 0 : pmSubTextureIndex);
 
-                    if (!projWGS84 && pmTextureCount <= textureIndex) {
+                    if (!projWGS84 && pmTextureCount <= pmSubTextureIndex) {
                         continue;
                     }
 