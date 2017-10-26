d56def95f35a44beb57387fab5fe86da258e4ea7
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2017-10-26 16:27:36 +0200

    fix (core): use scale.w to compute y value for reading in textures
    This is a followup of a616629d9001a6a988b40598cd4575c60372fbd9, where
    I forgot to change this function.

--- src/Process/LayeredMaterialNodeProcessing.js
+++ src/Process/LayeredMaterialNodeProcessing.js
@@ -75,7 +75,7 @@ function getIndiceWithPitch(i, pitch, w) {
     const currentX = (i % w) / w;  // normalized
     const currentY = Math.floor(i / w) / w; // normalized
     const newX = pitch.x + currentX * pitch.z;
-    const newY = pitch.y + currentY * pitch.z;
+    const newY = pitch.y + currentY * pitch.w;
     const newIndice = Math.floor(newY * w) * w + Math.floor(newX * w);
     return newIndice;
 }
