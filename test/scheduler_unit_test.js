12a1a3ad4d4dff309d688e879c791e23c9ff074c
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2017-11-02 15:41:21 +0100

    test (core): add Scheduler unit test

---
+++ test/scheduler_unit_test.js
@@ -0,0 +1,59 @@
+/* global describe, it */
+import assert from 'assert';
+import Scheduler from '../src/Core/Scheduler/Scheduler';
+
+const scheduler = new Scheduler();
+global.window = {
+    addEventListener() {},
+    setTimeout,
+};
+
+scheduler.addProtocolProvider('test', {
+    executeCommand: (cmd) => {
+        setTimeout(() => {
+            cmd.done = true;
+            cmd._r(cmd);
+        }, 0);
+        return new Promise((resolve) => {
+            cmd._r = resolve;
+        });
+    },
+});
+
+const view = {
+    notifyChange: () => {},
+};
+
+function cmd(layerId = 'foo', prio = 0) {
+    return {
+        layer: {
+            id: layerId,
+            protocol: 'test',
+        },
+        view,
+        priority: prio,
+    };
+}
+
+describe('Command execution', function () {
+    it('should execute one command', function (done) {
+        scheduler.execute(cmd()).then((c) => {
+            assert.ok(c.done);
+            done();
+        });
+    });
+
+    it('should execute 100 commands', function (done) {
+        const promises = [];
+        for (let i = 0; i < 100; i++) {
+            promises.push(scheduler.execute(cmd()));
+        }
+
+        Promise.all(promises).then((commands) => {
+            for (const cmd of commands) {
+                assert.ok(cmd.done);
+            }
+            done();
+        });
+    });
+});
