389da4d2d4e2a70542180497ff8a3bde351f2133
pierre-eric <pierre-eric.pelloux-prayer@oslandia.com>
2017-11-13 14:50:22 +0100

    fix (controls): correct typo in FirstPersonControls

--- src/Renderer/ThreeExtended/FirstPersonControls.js
+++ src/Renderer/ThreeExtended/FirstPersonControls.js
@@ -200,9 +200,9 @@ class FirstPersonControls extends THREE.EventDispatcher {
 
         for (const move of this.moves) {
             if (move.method === 'translateY') {
-                this.camera.position.z += move.sign * this.moveSpeed * dt / 1000;
+                this.camera.position.z += move.sign * this.options.moveSpeed * dt / 1000;
             } else {
-                this.camera[move.method](move.sign * this.moveSpeed * dt / 1000);
+                this.camera[move.method](move.sign * this.options.moveSpeed * dt / 1000);
             }
         }
 
