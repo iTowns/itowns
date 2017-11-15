0e1aea18a37b2063514c961942d6fefaf33d9b82
Augustin Trancart <augustin.trancart@oslandia.com>
2017-11-15 15:56:15 +0100

    chore(example): enlarge debug ui in pointcloud.html

--- examples/pointcloud.js
+++ examples/pointcloud.js
@@ -18,7 +18,7 @@ function showPointcloud(serverUrl, fileName, lopocsTable) {
         '+proj=lcc +lat_1=45.25 +lat_2=46.75 +lat_0=46 +lon_0=3 +x_0=1700000 ' +
         '+y_0=5200000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
 
-    debugGui = new dat.GUI();
+    debugGui = new dat.GUI({ width: 400 });
 
     // TODO: do we really need to disable logarithmicDepthBuffer ?
     view = new itowns.View('EPSG:3946', viewerDiv, { renderer: { logarithmicDepthBuffer: true } });
