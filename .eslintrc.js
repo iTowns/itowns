c28ec96679c55ce9ab0390f603a0d40a94dcb5f3
Guillaume Liegard <guillaume.liegard@ign.fr>
2018-01-24 16:51:00 +0100

    package(lint): Change formatting errors and 'unused var' errors to warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,10 +22,13 @@ module.exports = {
     commonjs: true
   },
   rules: {
+    'no-trailing-spaces': 'warn',
+    'padded-blocks' : 'warn',
+    'no-unused-vars': 'warn',
     'no-plusplus': 'off',
     // this option sets a specific tab width for your code
     // http://eslint.org/docs/rules/indent
-    indent: ['error', 4, {
+    indent: ['warn', 4, {
       SwitchCase: 1,
       VariableDeclarator: 1,
       outerIIFEBody: 1,
