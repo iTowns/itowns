595fd93623341e4713c323f5b4951db19b5bd2d9
Gerald Choqueux <gerald.choqueux@ign.fr>
2018-03-01 17:27:39 +0100

    fix(core): fix wrong 3dtiles's OBB using BuilderEllipsoidTile
    fix #656

--- src/Renderer/ThreeExtended/OBB.js
+++ src/Renderer/ThreeExtended/OBB.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import Coordinates, { C, UNIT } from '../../Core/Geographic/Coordinates';
+import TileGeometry from '../../Core/TileGeometry';
+import BuilderEllipsoidTile from '../../Core/Prefab/Globe/BuilderEllipsoidTile';
 
 function OBB(min, max) {
     THREE.Object3D.call(this);
@@ -103,119 +105,32 @@ for (let i = 0; i < 9; i++) {
 }
 
 // get oriented bounding box of tile
-OBB.extentToOBB = function extentToOBB(extent, minHeight = 0, maxHeight = 0) {
+const builder = new BuilderEllipsoidTile();
+OBB.extentToOBB = function _extentToOBB(extent, minHeight = 0, maxHeight = 0) {
     if (extent._crs != 'EPSG:4326') {
         throw new Error('The extent crs is not a Geographic Coordinates (EPSG:4326)');
     }
-
-    // Calcule the center world position with the extent.
-    extent.center(tmp.cardinals[8]);
-
-    const bboxDimension = extent.dimensions(UNIT.RADIAN);
-    const phiStart = extent.west(UNIT.RADIAN);
-    const phiLength = bboxDimension.x;
-
-    const thetaStart = extent.south(UNIT.RADIAN);
-    const thetaLength = bboxDimension.y;
-    //      0---1---2
-    //      |       |
-    //      7   8   3
-    //      |       |
-    //      6---5---4
-    // tmp.cardinals[0]._values[0] = phiStart;
-    // tmp.cardinals[0]._values[1] = thetaStart;
-    // tmp.cardinals[0]._normal = undefined;
-
-    tmp.cardinals[0].set('EPSG:4326', phiStart, thetaStart, 0);
-    tmp.cardinals[0]._internalStorageUnit = UNIT.RADIAN;
-    tmp.cardinals[1].set('EPSG:4326', phiStart + bboxDimension.x * 0.5, thetaStart, 0);
-    tmp.cardinals[1]._internalStorageUnit = UNIT.RADIAN;
-    tmp.cardinals[2].set('EPSG:4326', phiStart + phiLength, thetaStart, 0);
-    tmp.cardinals[2]._internalStorageUnit = UNIT.RADIAN;
-    tmp.cardinals[3].set('EPSG:4326', phiStart + phiLength, thetaStart + bboxDimension.y * 0.5, 0);
-    tmp.cardinals[3]._internalStorageUnit = UNIT.RADIAN;
-    tmp.cardinals[4].set('EPSG:4326', phiStart + phiLength, thetaStart + thetaLength, 0);
-    tmp.cardinals[4]._internalStorageUnit = UNIT.RADIAN;
-    tmp.cardinals[5].set('EPSG:4326', phiStart + bboxDimension.x * 0.5, thetaStart + thetaLength, 0);
-    tmp.cardinals[5]._internalStorageUnit = UNIT.RADIAN;
-    tmp.cardinals[6].set('EPSG:4326', phiStart, thetaStart + thetaLength, 0);
-    tmp.cardinals[6]._internalStorageUnit = UNIT.RADIAN;
-    tmp.cardinals[7].set('EPSG:4326', phiStart, thetaStart + bboxDimension.y * 0.5, 0);
-    tmp.cardinals[7]._internalStorageUnit = UNIT.RADIAN;
-
-    const cardinalsXYZ = [];
-    const centersLongitude = tmp.cardinals[8].longitude(UNIT.RADIAN);
-    for (const cardinal of tmp.cardinals) {
-        cardinalsXYZ.push(cardinal.as('EPSG:4978').xyz());
+    if (extent._internalStorageUnit != UNIT.RADIAN) {
+        throw new Error('The extent internalStorageUnit is not in radian unit');
     }
 
-    return this.cardinalsXYZToOBB(cardinalsXYZ, centersLongitude, true, tmp.cardinals[8].geodesicNormal, minHeight, maxHeight);
-};
+    const { sharableExtent, quaternion, position } = builder.computeSharableExtent(extent);
+    const paramsGeometry = {
+        extent: sharableExtent,
+        level: 0,
+        segment: 2,
+        disableSkirt: true,
+    };
 
-/**
- * Computes the OBB of a portion of a ellipsoid.
- * @param {Vector3[]} cardinals - 8 cardinals of the portion + the center.
- * @param {number} centerLongitude - the longitude at the center of the portion
- * @param {boolean} isEllipsoid - should be true when computing for the globe, false otherwise
- * @param {Vector3} geodesicNormal - geodesic normal at extent's center
- * @param {number} minHeight
- * @param {number} maxHeight
- * @return {OBB}
- */
-OBB.cardinalsXYZToOBB = function cardinalsXYZToOBB(cardinals, centerLongitude, isEllipsoid, geodesicNormal, minHeight = 0, maxHeight = 0) {
-    tmp.maxV.set(-1000, -1000, -1000);
-    tmp.minV.set(1000, 1000, 1000);
+    const geometry = new TileGeometry(paramsGeometry, builder);
+    const obb = geometry.OBB;
+    obb.updateZ(minHeight, maxHeight);
+    obb.position.copy(position);
+    obb.quaternion.copy(quaternion);
+    obb.update();
 
-    let halfMaxHeight = 0;
-    tmp.normal.copy(geodesicNormal);
-    tmp.tangentPlaneAtOrigin.set(tmp.normal, 0);
-
-    // Compute the rotation transforming the tile so that it's normal becomes (0, 0, 1)
-    tmp.transformNormalToZ.setFromUnitVectors(tmp.normal, tmp.zUp);
-    // Compute the rotation to get the line [1,8,5] aligned on (0, 1, 0)
-    tmp.alignTileOnWorldXY.setFromAxisAngle(tmp.zUp, -centerLongitude);
-    const rotateTile = tmp.alignTileOnWorldXY.multiply(tmp.transformNormalToZ);
-
-    let point5InPlaneX;
-    for (let i = 0; i < cardinals.length; i++) {
-        const vec = tmp.tangentPlaneAtOrigin.projectPoint(cardinals[i], tmp.cardinal3D);
-        const d = vec.distanceTo(cardinals[i].sub(cardinals[8]));
-        halfMaxHeight = Math.max(halfMaxHeight, d * 0.5);
-        vec.applyQuaternion(rotateTile);
-        // compute tile's min/max
-        tmp.maxV.max(vec);
-        tmp.minV.min(vec);
-
-        if (i == 5) {
-            point5InPlaneX = vec.x;
-        }
-    }
-
-    const halfLength = Math.abs(tmp.maxV.y - tmp.minV.y) * 0.5;
-    const halfWidth = Math.abs(tmp.maxV.x - tmp.minV.x) * 0.5;
-
-    const nHalfSize = Math.abs(-halfMaxHeight + (minHeight - maxHeight) * 0.5);
-
-    const max = new THREE.Vector3(halfLength, halfWidth, nHalfSize);
-    const min = new THREE.Vector3(-halfLength, -halfWidth, -nHalfSize);
-
-    // delta is the distance between line `([6],[4])` and the point `[5]`
-    // These points [6],[5],[4] aren't aligned because of the ellipsoid shape
-    const delta = isEllipsoid ? (halfWidth - Math.abs(point5InPlaneX)) : 0;
-    tmp.translate.set(0, delta, -halfMaxHeight);
-
-    const obb = new OBB(min, max);
-
-    obb.lookAt(tmp.normal);
-    obb.translateX(tmp.translate.x);
-    obb.translateY(tmp.translate.y);
-    obb.translateZ(tmp.translate.z);
-
-    // for 3D
-    if (minHeight !== 0 || maxHeight !== 0) {
-        obb.translateZ(-halfMaxHeight + minHeight + nHalfSize);
-    }
-
+    // Calling geometry.dispose() is not needed since this geometry never gets rendered
     return obb;
 };
+
 export default OBB;
