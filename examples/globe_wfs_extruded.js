iTowns/itowns

commit b38aa7b012f9aa8a1eb65dee1cb5f75cfcd16a37

refacto: Harmonize the mimetype configuration (#597)
Previously we had layer.format and layer.options.mimetype coexisting,
with no difference in role. This commits removes layer.options.mimetype
in favor of the former.

BREAKING CHANGE:

- `layer.options.mimetype` is removed in favor of `layer.format`.
- `WFS_Provider`: format should now be a proper mimetype. So `json`
  becomes `application/json`, `geojson` becomes `application/geojson`.

examples/globe_wfs_extruded.js
@@ -88,9 +88,7 @@ globeView.addLayer({
         south: 5138876.75,
         north: 5205890.19,
     },
-    options: {
-        mimetype: 'geojson',
-    },
+    format: 'application/geojson',
 }, globeView.tileLayer);
 
 function colorBuildings(properties) {
@@ -153,9 +151,7 @@ globeView.addLayer({
     level: 14,
     projection: 'EPSG:4326',
     ipr: 'IGN',
-    options: {
-        mimetype: 'json',
-    },
+    format: 'application/json',
 }, globeView.tileLayer);
 
 function configPointMaterial(result) {
@@ -201,9 +197,7 @@ globeView.addLayer({
     level: 12,
     projection: 'EPSG:2154',
     ipr: 'IGN',
-    options: {
-        mimetype: 'json',
-    },
+    format: 'application/json',
 }, globeView.tileLayer);
 
 exports.view = globeView;

examples/wfs.js
@@ -37,9 +37,7 @@ view.addLayer({
     projection: 'EPSG:3946',
     transparent: false,
     extent: extent,
-    options: {
-        mimetype: 'image/jpeg',
-    },
+    format: 'image/jpeg',
 });
 
 view.camera.camera3D.position.set(1839739, 5171618, 910);
@@ -78,9 +76,7 @@ view.addLayer({
         south: 5138876.75,
         north: 5205890.19,
     },
-    options: {
-        mimetype: 'geojson',
-    },
+    format: 'application/geojson',
 }, view.tileLayer);
 
 function colorBuildings(properties) {
@@ -138,9 +134,7 @@ view.addLayer({
         north: 46.03,
     },
     ipr: 'IGN',
-    options: {
-        mimetype: 'json',
-    },
+    format: 'application/json',
 }, view.tileLayer);
 
 function configPointMaterial(result) {
@@ -177,8 +171,6 @@ view.addLayer({
     level: 2,
     projection: 'EPSG:2154',
     ipr: 'IGN',
-    options: {
-        mimetype: 'json',
-    },
+    format: 'application/json',
 }, view.tileLayer);
 exports.view = view;

src/Core/Scheduler/Providers/WFS_Provider.js
@@ -12,6 +12,9 @@ import Feature2Mesh from '../../../Renderer/ThreeExtended/Feature2Mesh';
 
 const cache = CacheRessource();
 
+// TODO : support xml, gml2
+const supportedFormats = ['application/json', 'application/geojson'];
+
 function url(bbox, layer) {
     const box = bbox.as(layer.projection);
     const w = box.west();
@@ -29,7 +32,12 @@ function preprocessDataLayer(layer) {
     if (!layer.typeName) {
         throw new Error('layer.typeName is required.');
     }
-    layer.format = layer.options.mimetype || 'json';
+
+    layer.format = layer.format || 'application/json';
+    if (!supportedFormats.includes(layer.format)) {
+        throw new Error(`Layer ${layer.name}: unsupported layer.format '${layer.format}', must be one of '${supportedFormats.join('\', \'')}'`);
+    }
+
     layer.crs = layer.projection || 'EPSG:4326';
     layer.version = layer.version || '2.0.2';
     layer.opacity = layer.opacity || 1;
@@ -53,19 +61,7 @@ function executeCommand(command) {
     const layer = command.layer;
     const tile = command.requester;
     const destinationCrs = command.view.referenceCrs;
-
-    // TODO : support xml, gml2
-    const supportedFormats = {
-        json: getFeatures,
-        geojson: getFeatures,
-    };
-
-    const func = supportedFormats[layer.format];
-    if (func) {
-        return func(destinationCrs, tile, layer, command).then(result => command.resolve(result));
-    } else {
-        return Promise.reject(new Error(`Unsupported mimetype ${layer.format}`));
-    }
+    return getFeatures(destinationCrs, tile, layer, command).then(result => command.resolve(result));
 }
 
 function assignLayer(object, layer) {

examples/cubic_planar.js
@@ -104,9 +104,7 @@ for (index = 0; index < wmsLayers.length; index++) {
         id: 'wms_imagery' + wms + index,
         name: wms,
         projection: 'EPSG:3946',
-        options: {
-            mimetype: 'image/jpeg',
-        },
+        format: 'image/jpeg',
     }, tileLayer);
 
     view.addLayer({
@@ -120,9 +118,7 @@ for (index = 0; index < wmsLayers.length; index++) {
         name: 'MNT2012_Altitude_10m_CC46',
         projection: 'EPSG:3946',
         heightMapWidth: 256,
-        options: {
-            mimetype: 'image/jpeg',
-        },
+        format: 'image/jpeg',
     }, tileLayer);
 
     // Since the elevation layer use color textures, specify min/max z

examples/planar.js
@@ -32,9 +32,7 @@ view.addLayer({
     id: 'wms_imagery',
     name: 'Ortho2009_vue_ensemble_16cm_CC46',
     projection: 'EPSG:3946',
-    options: {
-        mimetype: 'image/jpeg',
-    },
+    format: 'image/jpeg',
     updateStrategy: {
         type: itowns.STRATEGY_DICHOTOMY,
         options: {},
@@ -51,9 +49,7 @@ view.addLayer({
     name: 'MNT2012_Altitude_10m_CC46',
     projection: 'EPSG:3946',
     heightMapWidth: 256,
-    options: {
-        mimetype: 'image/jpeg',
-    },
+    format: 'image/jpeg',
 });
 // Since the elevation layer use color textures, specify min/max z
 view.tileLayer.materialOptions = {

examples/planar_vector.js
@@ -33,9 +33,7 @@ view.addLayer({
     name: 'Ortho2009_vue_ensemble_16cm_CC46',
     projection: 'EPSG:3946',
     transparent: true,
-    options: {
-        mimetype: 'image/jpeg',
-    },
+    format: 'image/jpeg',
 });
 
 // Add an WMS elevation layer (see WMS_Provider* for valid options)
@@ -49,9 +47,7 @@ view.addLayer({
     name: 'MNT2012_Altitude_10m_CC46',
     projection: view.referenceCrs,
     heightMapWidth: 256,
-    options: {
-        mimetype: 'image/jpeg',
-    },
+    format: 'image/jpeg',
 });
 
 view.addLayer({

src/Core/Scheduler/Providers/Raster_Provider.js
@@ -51,7 +51,6 @@ export default {
             throw new Error('layer.url is required');
         }
 
-        layer.options = layer.options || {};
         // KML and GPX specifications all says that they should be in
         // EPSG:4326. We still support reprojection for them through this
         // configuration option

src/Core/Scheduler/Providers/StaticProvider.js
@@ -48,7 +48,7 @@ function getTexture(tile, layer) {
     }
 
 
-    const fn = layer.options.mimetype.indexOf('image/x-bil') === 0 ?
+    const fn = layer.format.indexOf('image/x-bil') === 0 ?
         OGCWebServiceHelper.getXBilTextureByUrl :
         OGCWebServiceHelper.getColorTextureByUrl;
     return fn(buildUrl(layer, selection.image), layer.networkOptions).then((texture) => {
@@ -91,7 +91,6 @@ export default {
             layer.extent = new Extent(layer.projection, ...layer.extent);
         }
 
-        layer.options = layer.options || {};
         layer.canTileTextureBeImproved = this.canTileTextureBeImproved;
         layer.url = new URL(layer.url, window.location);
         return Fetcher.json(layer.url.href).then((metadata) => {
@@ -105,17 +104,17 @@ export default {
                 });
             }
         }).then(() => {
-            if (!layer.options.mimetype) {
-                // fetch the first image to detect mimetype
+            if (!layer.format) {
+                // fetch the first image to detect format
                 if (layer.images.length) {
                     const url = buildUrl(layer, layer.images[0].image);
                     return fetch(url, layer.networkOptions).then((response) => {
-                        layer.options.mimetype = response.headers.get('Content-type');
-                        if (layer.options.mimetype === 'application/octet-stream') {
-                            layer.options.mimetype = 'image/x-bil';
+                        layer.format = response.headers.get('Content-type');
+                        if (layer.format === 'application/octet-stream') {
+                            layer.format = 'image/x-bil';
                         }
-                        if (!layer.options.mimetype) {
-                            throw new Error('Could not detect layer\'s mimetype');
+                        if (!layer.format) {
+                            throw new Error(`${layer.name}: could not detect layer format, please configure 'layer.format'.`);
                         }
                     });
                 }

commit d9cdc0ecfc5d06ad865626504baa1032b410a0ad

fix(provider): don't restrict outputFormat to mimetype on WFS layers
Fix an issue introduced by b38aa7b: WFS specs says that outputFormat can
also be any string. Therefore we cannot really have a list of supported
formats for WFS layers.

Also fixes the example by reverting to the old value for `layer.format`
for data.grandlyon WFS layers.

examples/globe_wfs_extruded.js
@@ -68,6 +68,7 @@ function acceptFeatureBus(properties) {
 }
 
 globeView.addLayer({
+    name: 'lyon_tcl_bus',
     type: 'geometry',
     update: itowns.FeatureProcessing.update,
     convert: itowns.Feature2Mesh.convert({
@@ -88,7 +89,7 @@ globeView.addLayer({
         south: 5138876.75,
         north: 5205890.19,
     },
-    format: 'application/geojson',
+    format: 'geojson',
 }, globeView.tileLayer);
 
 function colorBuildings(properties) {

examples/wfs.js
@@ -59,6 +59,7 @@ function colorLine(properties) {
 }
 
 view.addLayer({
+    name: 'lyon_tcl_bus',
     update: itowns.FeatureProcessing.update,
     convert: itowns.Feature2Mesh.convert({
         color: colorLine }),
@@ -76,7 +77,7 @@ view.addLayer({
         south: 5138876.75,
         north: 5205890.19,
     },
-    format: 'application/geojson',
+    format: 'geojson',
 }, view.tileLayer);
 
 function colorBuildings(properties) {

src/Core/Scheduler/Providers/WFS_Provider.js
@@ -12,9 +12,6 @@ import Feature2Mesh from '../../../Renderer/ThreeExtended/Feature2Mesh';
 
 const cache = CacheRessource();
 
-// TODO : support xml, gml2
-const supportedFormats = ['application/json', 'application/geojson'];
-
 function url(bbox, layer) {
     const box = bbox.as(layer.projection);
     const w = box.west();
@@ -34,9 +31,6 @@ function preprocessDataLayer(layer) {
     }
 
     layer.format = layer.format || 'application/json';
-    if (!supportedFormats.includes(layer.format)) {
-        throw new Error(`Layer ${layer.name}: unsupported layer.format '${layer.format}', must be one of '${supportedFormats.join('\', \'')}'`);
-    }
 
     layer.crs = layer.projection || 'EPSG:4326';
     layer.version = layer.version || '2.0.2';