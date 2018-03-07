f0738995eb221e259eaada59513c934b70e8f760
Augustin Trancart <augustin.trancart@oslandia.com>
2018-03-07 11:57:51 +0100

    chore(build): drop babel-plugin-webpack-loader
    babel-plugin-webpack-loader allowed to use webpack loaders in babel to
    transpile our sources. This commit changes the approach:
    - it lets babel deals with ES6 modules when it runs alone
    - it uses babel-plugin-inline-import to import json and glsl (this was
      done by webpack raw-loader before)
    
    The consequence of that is that glsl and json are now handled by babel
    instead of webpack. I believe this won't change anything.
    
    We have now a slightly more complex webpack.config.js, but we removed
    some copy-pasta and some magic, leading to a simpler config overall.
    
    The fact we no more pass through webpack at each babel invocation gives
    us some nice perf improvement:
    - `npm run transpile` execution time is divided by 2 on my machine
    - `npm run test-unit` execution time divided by 3.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,37 @@
+var fs = require('fs');
 var path = require('path');
 var webpack = require('webpack');
 
-var commonConfig = require('./webpack-common.config.js');
-
+const debugBuild = process.env.NODE_ENV === 'development';
 var definePlugin = new webpack.DefinePlugin({
-    __DEBUG__: JSON.stringify(process.env.NODE_ENV === 'development'),
+    __DEBUG__: debugBuild,
 });
+
+/*
+   configuring babel:
+   - when babel runs alone (for `test-unit` for instance), we let him deal with
+   ES6 modules, because node doesn't support them yet (planned for v10 lts).
+   - however, webpack also has ES6 module support and these 2 don't play well
+   together. When running webpack (either `build` or `start` script), we prefer
+   to rely on webpack loaders (much more powerful and gives more possibilities),
+   so let's disable modules for babel here.
+   - we also dynamise the value of __DEBUG__ according to the env var
+*/
+// Note that we don't support .babelrc in parent folders
+var babelrc = fs.readFileSync(path.resolve(__dirname, '.babelrc'));
+var babelConf = JSON.parse(babelrc);
+var newPresets = [];
+for (var preset of babelConf.presets) {
+    if (!Array.isArray(preset)) {
+        preset = [preset];
+    }
+    preset.push({ modules: false });
+    newPresets.push(preset);
+}
+babelConf.presets = newPresets;
+babelConf.babelrc = false; // disabel babelrc reading, as we've just done it
+const replacementPluginConf = babelConf.plugins.find(plugin => Array.isArray(plugin) && plugin[0] === 'minify-replace');
+replacementPluginConf[1].replacements.find(decl => decl.identifierName === '__DEBUG__').replacement.value = debugBuild;
 
 module.exports = {
     entry: {
@@ -44,17 +70,8 @@ module.exports = {
                     path.resolve(__dirname, 'utils'),
                 ],
                 loader: 'babel-loader',
-                // Please consider modifying .babelrc too
-                // .babelrc is used for transpiling src/ into lib/ in the prepublish
-                // phase, see package.json
-                options: {
-                    presets: [['es2015', { modules: false } ]],
-                    plugins: ['transform-runtime'],
-                    babelrc: false,
-                },
+                options: babelConf,
             },
-            commonConfig.glslLoader,
-            commonConfig.jsonLoader,
         ],
     },
     devServer: {
