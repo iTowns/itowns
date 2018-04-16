b30fea454ca499537917540301ba592f24b964f2
Adrien Berthet <adrien.berthet@ign.fr>
2018-04-16 13:38:02 +0200

    docs: add partial doc for Scheduler and Provider

--- src/Core/Scheduler/Scheduler.js
+++ src/Core/Scheduler/Scheduler.js
@@ -62,6 +62,14 @@ function _instanciateQueue() {
     };
 }
 
+/**
+ * The Scheduler is in charge of managing the [Providers]{@link Provider} that
+ * are used to gather resources needed to display the layers on a {@link View}.
+ * There is only one instance of a Scheduler per webview, and it is instanciated
+ * with the creation of the first view.
+ *
+ * @constructor
+ */
 function Scheduler() {
     // Constructor
     if (instanceScheduler !== null) {
@@ -156,6 +164,76 @@ Scheduler.prototype.execute = function execute(command) {
     return command.promise;
 };
 
+/**
+ * A Provider has the responsability to handle protocols and datablobs. Given a
+ * data request (see {@link Provider#executeCommand} for details about this
+ * request), it fetches serialized datasets, file content or even file chunks.
+ *
+ * @interface Provider
+ */
+
+/**
+ * When adding a layer to a view, some preprocessing can be done on it, before
+ * fetching or creating resources attached to it. For example, in the WMTS and
+ * WFS providers (included in iTowns), default options to the layer are added if
+ * some are missing.
+ *
+ * @function
+ * @name Provider#preprocessDataLayer
+ *
+ * @param {Layer} layer
+ * @param {View} [view]
+ * @param {Scheduler} [scheduler]
+ * @param {Layer} [parentLayer]
+ */
+
+/**
+ * In the {@link Scheduler} loop, this function is called every time the layer
+ * needs new information about itself. For tiled layers, it gets the necessary
+ * tiles, given the current position of the camera on the map. For simple layers
+ * like a GPX trace, it gets the data once.
+ * <br><br>
+ * It passes a <code>command</code> object as a parameter, with the
+ * <code>view</code> and the <code>layer</code> always present. The other
+ * parameters are optional.
+ *
+ * @function
+ * @name Provider#executeCommand
+ *
+ * @param {Object} command
+ * @param {View} command.view
+ * @param {Layer} command.layer
+ * @param {TileMesh} [command.requester] - Every layer is attached to a tile.
+ * @param {number} [command.targetLevel] - The target level is used when there
+ * is a tiled layer, such as WMTS or TMS, but not in case like a GPX layer.
+ *
+ * @return {Promise} The {@link Scheduler} always expect a Promise as a result,
+ * resolving to an object containing sufficient information for the associated
+ * processing to the current layer. For example, see the
+ * [LayeredMaterialNodeProcessing#updateLayeredMaterialNodeElevation]{@link
+ * https://github.com/iTowns/itowns/blob/master/src/Process/LayeredMaterialNodeProcessing.js}
+ * class or other processing class.
+ */
+
+/**
+ * Adds a provider for a specified protocol. The provider will be used when
+ * executing the queue to provide resources. See {@link Provider} for more
+ * informations.
+ * By default, some protocols are already set in iTowns: WMTS, WMS, WFS, TMS,
+ * XYZ, PotreeConverter, Rasterizer, 3D-Tiles and Static.
+ * <br><br>
+ * Warning: if the specified protocol has already a provider attached to it, the
+ * current provider will be overwritten by the given provider.
+ *
+ * @param {string} protocol - The name of the protocol to add. This is the
+ * <code>protocol</code> parameter put inside the configuration when adding a
+ * layer. The capitalization of the name is not taken into account here.
+ * @param {Provider} provider - The provider to link to the protocol, that must
+ * respect the {@link Provider} interface description.
+ *
+ * @throws {Error} an error if any method of the {@link Provider} is not present
+ * in the provider.
+ */
 Scheduler.prototype.addProtocolProvider = function addProtocolProvider(protocol, provider) {
     if (typeof (provider.executeCommand) !== 'function') {
         throw new Error(`Can't add provider for ${protocol}: missing a executeCommand function.`);
@@ -167,6 +245,13 @@ Scheduler.prototype.addProtocolProvider = function addProtocolProvider(protocol,
     this.providers[protocol] = provider;
 };
 
+/**
+ * Get a specific {@link Provider} given a particular protocol.
+ *
+ * @param {string} protocol
+ *
+ * @return {Provider}
+ */
 Scheduler.prototype.getProtocolProvider = function getProtocolProvider(protocol) {
     return this.providers[protocol];
 };
