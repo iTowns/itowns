3a18631b8b550e09ca160482a879ce66aab52608
Mathieu Brédif <mathieu.bredif@ign.fr>
2018-05-18 11:39:01 +0200

    refactor(renderer): PointsMaterial shader cleanup and optimization
    it also fixes the updating of the canvas height for automatic sizing

--- src/Renderer/Shader/PointsFS.glsl
+++ src/Renderer/Shader/PointsFS.glsl
@@ -4,30 +4,14 @@ precision highp int;
 #include <logdepthbuf_pars_fragment>
 
 varying vec4 vColor;
-uniform bool pickingMode;
-uniform float opacity;
-
-uniform bool useCustomColor;
-uniform vec3 customColor;
 
 void main() {
     // circular point rendering
-    float u = 2.0 * gl_PointCoord.x - 1.0;
-    float v = 2.0 * gl_PointCoord.y - 1.0;
-    float cc = u*u + v*v;
-    if(cc > 1.0){
+    if(length(gl_PointCoord - 0.5) > 0.5){
         discard;
     }
 
-    if (useCustomColor && !pickingMode) {
-        gl_FragColor = mix(vColor, vec4(customColor, 1.0), 0.5);
-    } else {
-        gl_FragColor = vColor;
-    }
-
-    if (!pickingMode) {
-        gl_FragColor.a = opacity;
-    }
+    gl_FragColor = vColor;
 
     #include <logdepthbuf_fragment>
 }
--- src/Renderer/Shader/PointsVS.glsl
+++ src/Renderer/Shader/PointsVS.glsl
@@ -4,16 +4,16 @@ precision highp int;
 #include <logdepthbuf_pars_vertex>
 #define EPSILON 1e-6
 
-uniform float size;
-uniform float scale;
+attribute vec3 position;
 uniform mat4 projectionMatrix;
 uniform mat4 modelViewMatrix;
-uniform vec2 resolution;
-uniform bool pickingMode;
+uniform float size;
 
-attribute vec4 unique_id;
+uniform bool pickingMode;
+uniform float opacity;
+uniform vec4 overlayColor;
 attribute vec3 color;
-attribute vec3 position;
+attribute vec4 unique_id;
 
 varying vec4 vColor;
 
@@ -21,21 +21,15 @@ void main() {
     if (pickingMode) {
         vColor = unique_id;
     } else {
-        vColor = vec4(color, 1.0);
+        vColor = vec4(mix(color, overlayColor.rgb, overlayColor.a), opacity);
     }
-    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
-    mat4 projModelViewMatrix = projectionMatrix * modelViewMatrix;
-    gl_Position = projModelViewMatrix* vec4( position, 1.0);
 
-    if (size > 0.01) {
+    gl_Position = projectionMatrix * (modelViewMatrix * vec4( position, 1.0 ));
+
+    if (size > 0.) {
         gl_PointSize = size;
     } else {
-        float pointSize = 1.0;
-        float slope = tan(1.0 / 2.0);
-        float projFactor =  -0.5 * resolution.y / (slope * mvPosition.z);
-
-        float z = min(0.5 * -gl_Position.z / gl_Position.w, 1.0);
-        gl_PointSize = max(3.0, min(10.0, 0.05 * projFactor));
+        gl_PointSize = clamp(-size / gl_Position.w, 3.0, 10.0);
     }
 
     #include <logdepthbuf_vertex>
