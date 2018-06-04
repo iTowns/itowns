f0835f3a014f2fc90f0233d01c63c1a336bc8179
Adrien Berthet <adrien.berthet@ign.fr>
2018-06-04 16:02:34 +0200

    feat(parser): add partial z support for geojson parsing
    According to RFC 7946 [1], we can have coordinates set as [lon, lat] but
    also as [lon, lat, alt] in a geojson file. When parsing a geojson to
    provide features, this wasn't respected and the z coordinates was always
    set to 1. While this default value is not customizable yet, it is a
    small improvement.
    
    Unit tests for geojson have been added to reflect this change.
    
    [1]: https://tools.ietf.org/html/rfc7946#section-9

---
+++ test/geojson_unit_test.js
@@ -0,0 +1,26 @@
+import proj4 from 'proj4';
+import GeoJsonParser from '../src/Parser/GeoJsonParser';
+/* global describe, it */
+
+const assert = require('assert');
+const holes = require('./data/geojson/holes.geojson.json');
+const gpx = require('./data/geojson/gpx.geojson.json');
+
+proj4.defs('EPSG:3946',
+    '+proj=lcc +lat_1=45.25 +lat_2=46.75 +lat_0=46 +lon_0=3 +x_0=1700000 +y_0=5200000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
+
+function parse(geojson) {
+    return GeoJsonParser.parse(geojson, { crsIn: 'EPSG:3946', crsOut: 'EPSG:3946', buildExtent: true });
+}
+
+describe('GeoJsonParser', function () {
+    it('should set all z coordinates to 1', () =>
+        parse(holes).then((feature) => {
+            assert.ok(feature[0].geometry.vertices.every(v => v.z() == 1));
+        }));
+
+    it('should respect all z coordinates', () =>
+        parse(gpx).then((feature) => {
+            assert.ok(feature[0].geometry.vertices.every(v => v.z() != 1));
+        }));
+});
