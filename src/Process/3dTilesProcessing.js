bbf0a9bc4b8891393efe8d49833300767c0b82dd
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2018-07-02 10:24:47 +0200

    fix(3dtiles): dont stop subdivision at tileset boundaries
    In this scenario:
      - a tileset references an external tileset at node N
      - the subdivision algorithm decides to stop at N and display N
    We must go one step further and display the content of the root
    node R from the external tileset.
    
    The reason is that R and N are the same node regarding content,
    but are stored in different tilesets (for technical reasons)  and
    thus considered as different node in iTowns (with N having R as
    its only child).

--- src/Process/3dTilesProcessing.js
+++ src/Process/3dTilesProcessing.js
@@ -324,6 +324,9 @@ export function $3dTilesSubdivisionControl(context, layer, node) {
     if (layer.tileIndex.index[node.tileId].children === undefined) {
         return false;
     }
+    if (layer.tileIndex.index[node.tileId].isTileset) {
+        return true;
+    }
     const sse = computeNodeSSE(context.camera, node);
     return sse > layer.sseThreshold;
 }
