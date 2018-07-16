ced513f13a2d6d4a49a584056099af707f30a7dc
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2018-07-16 10:39:57 +0200

    fix(examples): add missing param to notifyChange call in multiglobe
    This fix also add a test to ensure that the globes are properly updated when
    the camera moves.

--- test/examples/multiglobe.js
+++ test/examples/multiglobe.js
@@ -11,4 +11,33 @@ describe('multiglobe', () => {
         assert.ok(result);
         await page.close();
     });
+
+    it('zoom and check that the level is correct', async function _() {
+        const page = await browser.newPage();
+        await loadExample(page,
+            `http://localhost:${itownsPort}/examples/multiglobe.html`);
+
+        // press-space and zoom in
+        await page.evaluate(() => {
+            onKeyPress({ keyCode: 32 });
+            for (let i = 0; i < 50; i++) {
+                onMouseWheel({ detail: -1 });
+            }
+        });
+
+        await waitUntilItownsIsIdle(page, this.test.fullTitle());
+
+        // verify that we properly updated the globe
+        const { layer, level } = await page.evaluate(() => {
+            const pick = globeView.pickObjectsAt({ x: 200, y: 150 })[0];
+            return {
+                layer: pick.layer.id,
+                level: pick.object.level,
+            };
+        });
+
+        assert.equal('globe2', layer);
+        assert.equal(8, level);
+        await page.close();
+    });
 });
