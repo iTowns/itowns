25e35e282e137a1b8621db568001ca8b8aec4fa9
Adrien Berthet <adrien.berthet@ign.fr>
2018-08-22 14:39:51 +0200

    test: retry cubic_planar test if necessary

--- test/examples/cubic_planar.js
+++ test/examples/cubic_planar.js
@@ -3,6 +3,9 @@ const assert = require('assert');
 
 describe('cubic_planar', () => {
     it('should run', async function _() {
+        // This test hangs up one time out of three, so retry it 2 times
+        this.retries(2);
+
         const page = await browser.newPage();
         const result = await loadExample(page,
             `http://localhost:${itownsPort}/examples/cubic_planar.html`,
