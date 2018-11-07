f917be4f02f31e12e0999c68db305fb251d3ec7d
gchoqueux <gerald.choqueux@ign.fr>
2018-11-07 17:16:45 +0100

    fix(core): problem in runtime with bundle itowns and debug

--- webpack.config.js
+++ webpack.config.js
@@ -43,26 +43,8 @@ module.exports = {
         umdNamedDefine: true,
     },
     optimization: {
-        splitChunks: {
-            chunks: 'async',
-            minSize: 30000,
-            maxSize: 0,
-            minChunks: 1,
-            maxAsyncRequests: 5,
-            maxInitialRequests: 3,
-            automaticNameDelimiter: '~',
-            name: true,
-            cacheGroups: {
-                vendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    priority: -10,
-                },
-                default: {
-                    minChunks: 2,
-                    priority: -20,
-                    reuseExistingChunk: true,
-                },
-            },
+        runtimeChunk: {
+            name: 'itowns',
         },
     },
     module: {
