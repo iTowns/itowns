b2d4bd856415714dcd677d651415936576ff2e42
Adrien Berthet <adrien.berthet@ign.fr>
2018-11-08 15:42:11 +0100

    refacto(layer): deprecate adding a layer with only a configuration
    BREAKING CHANGE: layer.options.mimetype is no longer supported, use
    layer.format instead

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,6 @@
 export { default as Coordinates, UNIT, ellipsoidSizes } from './Core/Geographic/Coordinates';
 export { default as Extent } from './Core/Geographic/Extent';
 export { default as Ellipsoid } from './Core/Math/Ellipsoid';
-export { ImageryLayers } from './Layer/Layer';
-export { default as GeometryLayer } from './Layer/GeometryLayer';
-export { STRATEGY_MIN_NETWORK_TRAFFIC, STRATEGY_GROUP, STRATEGY_PROGRESSIVE, STRATEGY_DICHOTOMY } from './Layer/LayerUpdateStrategy';
 export { default as GlobeView, GLOBE_VIEW_EVENTS, createGlobeLayer } from './Core/Prefab/GlobeView';
 export { default as PlanarView, createPlanarLayer } from './Core/Prefab/PlanarView';
 export { default as PanoramaView, createPanoramaLayer } from './Core/Prefab/PanoramaView';
@@ -18,7 +15,6 @@ export { default as CameraCalibrationParser } from './Parser/CameraCalibrationPa
 export { process3dTilesNode, init3dTilesLayer, $3dTilesCulling, $3dTilesSubdivisionControl, pre3dTilesUpdate } from './Process/3dTilesProcessing';
 export { default as FeatureProcessing } from './Process/FeatureProcessing';
 export { updateLayeredMaterialNodeImagery, updateLayeredMaterialNodeElevation } from './Process/LayeredMaterialNodeProcessing';
-export { ColorLayersOrdering } from './Renderer/ColorLayersOrdering';
 export { default as OrientedImageCamera } from './Renderer/OrientedImageCamera';
 export { default as PointsMaterial } from './Renderer/PointsMaterial';
 export { default as PointCloudProcessing } from './Process/PointCloudProcessing';
@@ -32,3 +28,16 @@ export { default as DEMUtils } from './utils/DEMUtils';
 export { default as CameraUtils } from './utils/CameraUtils';
 export { default as OrientationUtils } from './utils/OrientationUtils';
 export { default as ShaderChunk } from './Renderer/Shader/ShaderChunk';
+
+// Layers provided by default in iTowns
+// A custom layer should at least implements Layer
+// See http://www.itowns-project.org/itowns/API_Doc/Layer.html
+export { default as Layer, ImageryLayers } from './Layer/Layer';
+export { default as ColorLayer } from './Layer/ColorLayer';
+export { default as ElevationLayer } from './Layer/ElevationLayer';
+export { default as GeometryLayer } from './Layer/GeometryLayer';
+export { default as TiledGeometryLayer } from './Layer/TiledGeometryLayer';
+export { STRATEGY_MIN_NETWORK_TRAFFIC, STRATEGY_GROUP, STRATEGY_PROGRESSIVE, STRATEGY_DICHOTOMY } from './Layer/LayerUpdateStrategy';
+export { ColorLayersOrdering } from './Renderer/ColorLayersOrdering';
+export { default as GlobeLayer } from './Core/Prefab/Globe/GlobeLayer';
+export { default as PlanarLayer } from './Core/Prefab/Planar/PlanarLayer';
