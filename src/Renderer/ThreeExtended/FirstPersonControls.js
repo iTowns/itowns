iTowns/itowns

commit 1eae7fe61e79dc0a14abaff1e3b67e072dc2c839

fix(FirstPersonControl): It now supports Globe view. Refactor to allow us to change behavior for translation on Y, in order to follow the geodesic normal, used in globe view.

src/Renderer/ThreeExtended/FirstPersonControls.js
@@ -23,13 +23,25 @@ function applyRotation(view, camera3D, state) {
 }
 
 const MOVEMENTS = {
-    38: { method: 'translateZ', sign: -1 }, // FORWARD: up key
-    40: { method: 'translateZ', sign: 1 }, // BACKWARD: down key
+    38: { method: 'translateY', sign: -1 }, // FORWARD: up key
+    40: { method: 'translateY', sign: 1 }, // BACKWARD: down key
     37: { method: 'translateX', sign: -1 }, // STRAFE_LEFT: left key
     39: { method: 'translateX', sign: 1 }, // STRAFE_RIGHT: right key
-    33: { method: 'translateY', sign: 1 }, // UP: PageUp key
-    34: { method: 'translateY', sign: -1 }, // DOWN: PageDown key
+    33: { method: 'translateZ', sign: 1 }, // UP: PageUp key
+    34: { method: 'translateZ', sign: -1 }, // DOWN: PageDown key
 };
+
+function translateZPlanar(dt, sign) {
+    this.camera.position.z += sign * this.options.moveSpeed * dt / 1000;
+}
+
+const normal = new THREE.Vector3();
+function translateZGlobe(dt, sign) {
+    // compute geodesic normale
+    normal.copy(this.camera.position);
+    normal.normalize();
+    this.camera.position.add(normal.multiplyScalar(sign * this.options.moveSpeed * dt / 1000));
+}
 
 class FirstPersonControls extends THREE.EventDispatcher {
     /**
@@ -99,6 +111,12 @@ class FirstPersonControls extends THREE.EventDispatcher {
         if (options.focusOnClick) {
             domElement.addEventListener('click', () => domElement.focus());
         }
+
+        if (view.referenceCrs == 'EPSG:4978') {
+            this.translateZ = translateZGlobe;
+        } else {
+            this.translateZ = translateZPlanar;
+        }
     }
 
     isUserInteracting() {
@@ -145,8 +163,8 @@ class FirstPersonControls extends THREE.EventDispatcher {
         }
 
         for (const move of this.moves) {
-            if (move.method === 'translateY') {
-                this.camera.position.z += move.sign * this.options.moveSpeed * dt / 1000;
+            if (move.method === 'translateZ') {
+                this.translateZ(move.sign, dt);
             } else {
                 this.camera[move.method](move.sign * this.options.moveSpeed * dt / 1000);
             }

commit 3c247585245a4d35fbd0e8d462ef0e3c6a1e4d65

fix(FirstPersonControl): Fix a bug introduced in #881. Before this change, this controls was going up when we pressed key up instead of going forward as expected. Now it goes forward again when we press key up.

src/Renderer/ThreeExtended/FirstPersonControls.js
@@ -23,24 +23,24 @@ function applyRotation(view, camera3D, state) {
 }
 
 const MOVEMENTS = {
-    38: { method: 'translateY', sign: -1 }, // FORWARD: up key
-    40: { method: 'translateY', sign: 1 }, // BACKWARD: down key
+    38: { method: 'translateZ', sign: -1 }, // FORWARD: up key
+    40: { method: 'translateZ', sign: 1 }, // BACKWARD: down key
     37: { method: 'translateX', sign: -1 }, // STRAFE_LEFT: left key
     39: { method: 'translateX', sign: 1 }, // STRAFE_RIGHT: right key
-    33: { method: 'translateZ', sign: 1 }, // UP: PageUp key
-    34: { method: 'translateZ', sign: -1 }, // DOWN: PageDown key
+    33: { method: 'translateY', sign: 1 }, // UP: PageUp key
+    34: { method: 'translateY', sign: -1 }, // DOWN: PageDown key
 };
 
-function translateZPlanar(dt, sign) {
-    this.camera.position.z += sign * this.options.moveSpeed * dt / 1000;
+function moveCameraVerticalPlanar(value) {
+    this.camera.position.z += value;
 }
 
 const normal = new THREE.Vector3();
-function translateZGlobe(dt, sign) {
+function moveCameraVerticalGlobe(value) {
     // compute geodesic normale
     normal.copy(this.camera.position);
     normal.normalize();
-    this.camera.position.add(normal.multiplyScalar(sign * this.options.moveSpeed * dt / 1000));
+    this.camera.position.add(normal.multiplyScalar(value));
 }
 
 class FirstPersonControls extends THREE.EventDispatcher {
@@ -113,9 +113,9 @@ class FirstPersonControls extends THREE.EventDispatcher {
         }
 
         if (view.referenceCrs == 'EPSG:4978') {
-            this.translateZ = translateZGlobe;
+            this.moveCameraVertical = moveCameraVerticalGlobe;
         } else {
-            this.translateZ = translateZPlanar;
+            this.moveCameraVertical = moveCameraVerticalPlanar;
         }
     }
 
@@ -163,8 +163,8 @@ class FirstPersonControls extends THREE.EventDispatcher {
         }
 
         for (const move of this.moves) {
-            if (move.method === 'translateZ') {
-                this.translateZ(move.sign, dt);
+            if (move.method === 'translateY') {
+                this.moveCameraVertical(move.sign * this.options.moveSpeed * dt / 1000);
             } else {
                 this.camera[move.method](move.sign * this.options.moveSpeed * dt / 1000);
             }