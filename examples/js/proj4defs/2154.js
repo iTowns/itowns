c358845a26169baf3cb3b5c56c10f6a15865ac2d
gliegard <guillaume.liegard@ign.fr>
2018-11-22 16:24:06 +0100

    example(projection): File to store projection definitions, like EPSG:2154... So it can be used in many examples

---
+++ examples/js/proj4defs/2154.js
@@ -0,0 +1,3 @@
+/* global itowns */
+// Define projection that we will use (taken from https://epsg.io/2154, Proj4js section)
+itowns.proj4.defs('EPSG:2154', '+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
---
+++ examples/js/proj4defs/3946.js
@@ -0,0 +1,3 @@
+/* global itowns */
+// Define projection that we will use (taken from https://epsg.io/3946, Proj4js section)
+itowns.proj4.defs('EPSG:3946', '+proj=lcc +lat_1=45.25 +lat_2=46.75 +lat_0=46 +lon_0=3 +x_0=1700000 +y_0=5200000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
