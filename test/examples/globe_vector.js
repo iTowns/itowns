b998fce66c7d368d388f82f02df6b441adb21a43
gchoqueux <gerald.choqueux@ign.fr>
2019-01-21 15:14:24 +0100

    test: pick feature from Layer with SourceFile

--- test/examples/globe_vector.js
+++ test/examples/globe_vector.js
@@ -9,4 +9,24 @@ describe('globe_vector', function _() {
     it('should run', async () => {
         assert.ok(result);
     });
+
+    it('should pick feature from Layer with SourceFile', async () => {
+        const pickFeatureCount = await page.evaluate(() => {
+            const precision = view.controls.pixelsToDegrees(5);
+            const layers = view.getLayers(l => l.source && l.source.isFileSource);
+            const geoCoord = new itowns.Coordinates('EPSG:4326', 1.41955, 42.88613, 0);
+            for (i = 0; i < layers.length; i++) {
+                const p = itowns.FeaturesUtils.filterFeaturesUnderCoordinate(
+                    geoCoord, layers[i].source.parsedData, precision,
+                );
+                if (p.length) {
+                    return Promise.resolve(p.length);
+                }
+            }
+
+            return 0;
+        });
+
+        assert.equal(1, pickFeatureCount);
+    });
 });
