6040bdb56fe62a94879408444586faf5c628e063
Adrien Berthet <adrien.berthet@ign.fr>
2019-03-04 12:13:07 +0100

    chore(examples): clean eslintrc

--- examples/.eslintrc.js
+++ examples/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     'extends': [
         'eslint-config-airbnb-base',
         'eslint-config-airbnb-base/rules/strict',
+        '../.eslintrc.js',
     ],
     parserOptions: {
         ecmaVersion: 5,
@@ -17,47 +18,15 @@ module.exports = {
         commonjs: true
     },
     rules: {
-        'no-plusplus': 'off',
-        // this option sets a specific tab width for your code
-        // http://eslint.org/docs/rules/indent
-        indent: ['error', 4, {
-            SwitchCase: 1,
-            VariableDeclarator: 1,
-            outerIIFEBody: 1,
-            // MemberExpression: null,
-            // CallExpression: {
-            // parameters: null,
-            // },
-            FunctionDeclaration: {
-                parameters: 1,
-                body: 1
-            },
-            FunctionExpression: {
-                parameters: 1,
-                body: 1
-            }
-        }],
-        'one-var': ['error', 'never'],
-        'valid-jsdoc': ['error', {
-            requireReturn: false,
-            requireParamDescription: false,
-            requireReturnDescription: false,
-        }],
-
-        // deactivated rules for es5
-        'no-var': 'off',
         'prefer-arrow-callback': 'off',
         'object-shorthand': 'off',
         'no-param-reassign': ['error', { 'props': false }],
         'no-mixed-operators': ['error', { allowSamePrecedence: true }],
         'prefer-template': 'off',
         'prefer-rest-params': 'off',
-        'prefer-destructuring': 'off',
-        'function-paren-newline': 'off',
 
         // deactivated rules for `examples/`
         'no-console': 'off',
-
         // TODO reactivate all the following rules
         'no-underscore-dangle': 'off',
 
