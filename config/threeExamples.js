7cc9a4cddbb88900adede58001a0d294f291bf98
gchoqueux <gerald.choqueux@ign.fr>
2019-12-09 11:47:43 +0100

    chore: update packages.
    LegacyGLTFLoader was removed from THREE v111.0.0.

--- config/threeExamples.js
+++ config/threeExamples.js
@@ -2,7 +2,6 @@ module.exports = {
     patchedPath: './src/ThreeExtended/',
     filesExamples: [
         './loaders/GLTFLoader.js',
-        './loaders/deprecated/LegacyGLTFLoader.js',
         './loaders/DRACOLoader.js',
         './loaders/DDSLoader.js',
         './WebGL.js',
