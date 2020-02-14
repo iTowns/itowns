4fd93dd12da9d4c857fd6d71d9f420318e071fa6
gchoqueux <gerald.choqueux@ign.fr>
2020-02-14 10:41:02 +0100

    refactor(PointCloud): change tests units.

--- test/unit/pointcloudlayerparsing.js
+++ test/unit/pointcloudlayerparsing.js
@@ -1,51 +1,92 @@
 import assert from 'assert';
-import PointCloudLayer, { parseMetadata } from 'Layer/PointCloudLayer';
+import PointCloudLayer from 'Layer/PointCloudLayer';
+import PointCloudSource from 'Source/PointCloudSource';
+import Coordinates from 'Core/Geographic/Coordinates';
+import GlobeView from 'Core/Prefab/GlobeView';
+import HttpsProxyAgent from 'https-proxy-agent';
+import Renderer from './mock';
 
 describe('PointCloudProvider', function () {
-    it('should correctly parse normal information in metadata', function () {
-        const layer = {
-            material: { defines: {} },
-        };
+    const renderer = new Renderer();
+    const placement = { coord: new Coordinates('EPSG:4326', 1.5, 43), range: 300000 };
+    const view = new GlobeView(renderer.domElement, placement, { renderer });
 
-        // no normals
-        const metadata = {
-            boundingBox: {
-                lx: 0,
-                ly: 1,
-                ux: 2,
-                uy: 3,
-            },
+    it('should correctly parse normal information in cloud', function (done) {
+        // No normals
+        const cloud = {
+            boundingBox: { lx: 0, ly: 1, ux: 2, uy: 3 },
             scale: 1.0,
             pointAttributes: ['POSITION', 'RGB'],
+            octreeDir: 'eglise_saint_blaise_arles',
         };
 
-        parseMetadata(metadata, layer);
-        const normalDefined = layer.material.defines.NORMAL || layer.material.defines.NORMAL_SPHEREMAPPED || layer.material.defines.NORMAL_OCT16;
-        assert.ok(!normalDefined);
+        const ps = [];
+        let source = new PointCloudSource({
+            file: 'eglise_saint_blaise_arles.js',
+            url: 'https://raw.githubusercontent.com/gmaillet/dataset/master/',
+            networkOptions: process.env.HTTPS_PROXY ? { agent: new HttpsProxyAgent(process.env.HTTPS_PROXY) } : {},
+            cloud,
+        });
+        ps.push(new PointCloudLayer('pointsCloud', { source }, view).whenReady.then((l) => {
+            const normalDefined = l.material.defines.NORMAL || l.material.defines.NORMAL_SPHEREMAPPED || l.material.defines.NORMAL_OCT16;
+            assert.ok(!normalDefined);
+        }));
 
-        // normals as vector
-        layer.material = { defines: {} };
-        metadata.pointAttributes = ['POSITION', 'NORMAL', 'CLASSIFICATION'];
-        parseMetadata(metadata, layer);
-        assert.ok(layer.material.defines.NORMAL);
-        assert.ok(!layer.material.defines.NORMAL_SPHEREMAPPED);
-        assert.ok(!layer.material.defines.NORMAL_OCT16);
+        // // // normals as vector
+        source = new PointCloudSource({
+            file: 'eglise_saint_blaise_arles.js',
+            url: 'https://raw.githubusercontent.com/gmaillet/dataset/master/',
+            networkOptions: process.env.HTTPS_PROXY ? { agent: new HttpsProxyAgent(process.env.HTTPS_PROXY) } : {},
+            cloud: {
+                boundingBox: { lx: 0, ly: 1, ux: 2, uy: 3 },
+                scale: 1.0,
+                pointAttributes: ['POSITION', 'NORMAL', 'CLASSIFICATION'],
+                octreeDir: 'eglise_saint_blaise_arles',
+            },
+        });
+        ps.push(new PointCloudLayer('pointsCloud', { source }, view).whenReady.then((l) => {
+            assert.ok(l.material.defines.NORMAL);
+            assert.ok(!l.material.defines.NORMAL_SPHEREMAPPED);
+            assert.ok(!l.material.defines.NORMAL_OCT16);
+        }));
 
-        // spheremapped normals
-        layer.material = { defines: {} };
-        metadata.pointAttributes = ['POSITION', 'COLOR_PACKED', 'NORMAL_SPHEREMAPPED'];
-        parseMetadata(metadata, layer);
-        assert.ok(!layer.material.defines.NORMAL);
-        assert.ok(layer.material.defines.NORMAL_SPHEREMAPPED);
-        assert.ok(!layer.material.defines.NORMAL_OCT16);
+        // // spheremapped normals
+        source = new PointCloudSource({
+            file: 'eglise_saint_blaise_arles.js',
+            url: 'https://raw.githubusercontent.com/gmaillet/dataset/master/',
+            networkOptions: process.env.HTTPS_PROXY ? { agent: new HttpsProxyAgent(process.env.HTTPS_PROXY) } : {},
+            cloud: {
+                boundingBox: { lx: 0, ly: 1, ux: 2, uy: 3 },
+                scale: 1.0,
+                pointAttributes: ['POSITION', 'COLOR_PACKED', 'NORMAL_SPHEREMAPPED'],
+                octreeDir: 'eglise_saint_blaise_arles',
+            },
+        });
+        ps.push(new PointCloudLayer('pointsCloud', { source }, view).whenReady.then((l) => {
+            assert.ok(!l.material.defines.NORMAL);
+            assert.ok(l.material.defines.NORMAL_SPHEREMAPPED);
+            assert.ok(!l.material.defines.NORMAL_OCT16);
+        }));
 
-        // oct16 normals
-        layer.material = { defines: {} };
-        metadata.pointAttributes = ['POSITION', 'COLOR_PACKED', 'CLASSIFICATION', 'NORMAL_OCT16'];
-        parseMetadata(metadata, layer);
-        assert.ok(!layer.material.defines.NORMAL);
-        assert.ok(!layer.material.defines.NORMAL_SPHEREMAPPED);
-        assert.ok(layer.material.defines.NORMAL_OCT16);
+        // // oct16 normals
+        source = new PointCloudSource({
+            file: 'eglise_saint_blaise_arles.js',
+            url: 'https://raw.githubusercontent.com/gmaillet/dataset/master/',
+            networkOptions: process.env.HTTPS_PROXY ? { agent: new HttpsProxyAgent(process.env.HTTPS_PROXY) } : {},
+            cloud: {
+                boundingBox: { lx: 0, ly: 1, ux: 2, uy: 3 },
+                scale: 1.0,
+                pointAttributes: ['POSITION', 'COLOR_PACKED', 'CLASSIFICATION', 'NORMAL_OCT16'],
+                octreeDir: 'eglise_saint_blaise_arles',
+            },
+        });
+        ps.push(new PointCloudLayer('pointsCloud', { source }, view).whenReady.then((l) => {
+            assert.ok(!l.material.defines.NORMAL);
+            assert.ok(!l.material.defines.NORMAL_SPHEREMAPPED);
+            assert.ok(l.material.defines.NORMAL_OCT16);
+        }));
+
+        Promise.all(ps).then(() => done());
     });
 });
 
