721733f79c7be9ebba1548f20e26ba6964cd0293
jailln <vincentjaillot@gmail.com>
2020-05-04 17:33:02 +0200

    refactor(3dtiles): rename tileIndex property of C3DTilesLayer to tileset.
    BREAKING CHANGE: C3DTilesLayer tileIndex renamed to tileset.
    
    BREAKING CHANGE: C3DTileset's index attribute renamed to tiles.

--- src/Layer/C3DTilesLayer.js
+++ src/Layer/C3DTilesLayer.js
@@ -50,7 +50,7 @@ class C3DTilesLayer extends GeometryLayer {
         const resolve = this.addInitializationStep();
 
         this.source.whenReady.then((tileset) => {
-            this.tileIndex = new C3DTileset(tileset, this.source.baseUrl);
+            this.tileset = new C3DTileset(tileset, this.source.baseUrl);
             this.asset = tileset.asset;
             // TODO: Move all init3dTilesLayer code to constructor
             init3dTilesLayer(view, view.mainLoop.scheduler, this, tileset.root).then(resolve);
