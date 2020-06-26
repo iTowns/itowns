172fe8de407f45644dbd3f8ab9edf34dc784831a
jailln <vincentjaillot@gmail.com>
2020-06-26 15:45:19 +0200

    fix(examples): remove redundant parameter

--- examples/js/3dTilesHelper.js
+++ examples/js/3dTilesHelper.js
@@ -1,14 +1,11 @@
 // Function allowing picking on a given 3D tiles layer and filling an html div
 // with information on the picked feature
-// This function expects arguments passed through the binding of this (see
-// 3dtiles.html and 3dtiles_hierarchy.html for more information on how to
-// bind this).
 // Expected arguments:
-// this.htmlDiv (div element which contains the picked information)
-// this.view : iTowns view where the picking must be done
-// this.layer : the layer on which the picking must be done
+// pickingArg.htmlDiv (div element which contains the picked information)
+// pickingArg.view : iTowns view where the picking must be done
+// pickingArg.layer : the layer on which the picking must be done
 // eslint-disable-next-line
-function fillHTMLWithPickingInfo(event, view, pickingArg) {
+function fillHTMLWithPickingInfo(event, pickingArg) {
     if (!pickingArg.layer.isC3DTilesLayer) {
         console.warn('Function fillHTMLWithPickingInfo only works' +
             ' for C3DTilesLayer layers.');
@@ -21,7 +18,7 @@ function fillHTMLWithPickingInfo(event, view, pickingArg) {
     }
 
     // Get intersected objects
-    var intersects = view.pickObjectsAt(event, 5, pickingArg.layer);
+    var intersects = pickingArg.view.pickObjectsAt(event, 5, pickingArg.layer);
     if (intersects.length === 0) { return; }
 
     // Get information from intersected objects (from the batch table and
