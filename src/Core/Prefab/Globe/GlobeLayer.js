3d2e704f062fdc897657eba35f02b7ec3f0ad7c6
Adrien Berthet <adrien.berthet@ign.fr>
2020-09-22 11:29:40 +0200

    fix(label): horizon culling
    Add a proper horizon culling mechanism, by splitting the GlobeLayer
    culling to use it.

--- src/Core/Prefab/Globe/GlobeLayer.js
+++ src/Core/Prefab/Globe/GlobeLayer.js
@@ -117,8 +117,12 @@ class GlobeLayer extends TiledGeometryLayer {
             return false;
         }
 
+        return this.horizonCulling(node.horizonCullingPointElevationScaled);
+    }
+
+    horizonCulling(point) {
         // see https://cesiumjs.org/2013/04/25/Horizon-culling/
-        scaledHorizonCullingPoint.copy(node.horizonCullingPointElevationScaled).applyMatrix4(worldToScaledEllipsoid);
+        scaledHorizonCullingPoint.copy(point).applyMatrix4(worldToScaledEllipsoid);
         scaledHorizonCullingPoint.sub(cameraPosition);
 
         const vtMagnitudeSquared = scaledHorizonCullingPoint.lengthSq();
