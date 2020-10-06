bc6ab91208a5d9f7e5e067465167bea1634e0c21
gchoqueux <gerald.choqueux@ign.fr>
2020-10-06 17:31:11 +0200

    doc: add webGL 2.0 docs, examples and a tutorial.

--- src/Core/Prefab/PlanarView.js
+++ src/Core/Prefab/PlanarView.js
@@ -6,6 +6,25 @@ import CameraUtils from 'Utils/CameraUtils';
 import PlanarLayer from './Planar/PlanarLayer';
 
 class PlanarView extends View {
+    /**
+     * @constructor
+     * @extends View
+     *
+     * @example <caption><b>Enable WebGl 1.0 instead of WebGl 2.0.</b></caption>
+     * var viewerDiv = document.getElementById('viewerDiv');
+     * const extent = new Extent('EPSG:3946', 1837816.94334, 1847692.32501, 5170036.4587, 5178412.82698);
+     * var view = new itowns.GlobeView(viewerDiv, extent, {  renderer: { isWebGL2: false } });
+     *
+     * @example <caption><b>Instance with placement on the ground.</b></caption>
+     * var viewerDiv = document.getElementById('viewerDiv');
+     * const extent = new Extent('EPSG:3946', 1837816.94334, 1847692.32501, 5170036.4587, 5178412.82698);
+     * var view = new itowns.GlobeView(viewerDiv, extent, { placement: { heading: -49.6, range: 6200, tilt: 17 } });
+     *
+     * @param {HTMLDivElement} viewerDiv - Where to attach the view and display it
+     * in the DOM.
+     * @param {Extent} extent - The ground extent.
+     * @param {object=} options - See options of {@link View}.
+     */
     constructor(viewerDiv, extent, options = {}) {
         THREE.Object3D.DefaultUp.set(0, 0, 1);
 
