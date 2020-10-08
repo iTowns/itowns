f242a1f935f72a4711dd3541b3c8119ce791309f
gchoqueux <gerald.choqueux@ign.fr>
2020-10-08 11:58:53 +0200

    test: update opacity/visibility handling.

--- test/unit/layeredmaterial.js
+++ test/unit/layeredmaterial.js
@@ -1,45 +1,57 @@
 import assert from 'assert';
-import LayerUpdateState from 'Layer/LayerUpdateState';
+import ColorLayer from 'Layer/ColorLayer';
+import TMSSource from 'Source/TMSSource';
 import { updateLayeredMaterialNodeImagery } from 'Process/LayeredMaterialNodeProcessing';
+import GlobeView from 'Core/Prefab/GlobeView';
+import Coordinates from 'Core/Geographic/Coordinates';
+import TileMesh from 'Core/TileMesh';
+import * as THREE from 'three';
+import Extent from 'Core/Geographic/Extent';
+import OBB from 'Renderer/OBB';
+import LayeredMaterial from 'Renderer/LayeredMaterial';
+import Renderer from './bootstrap';
 
 describe('material state vs layer state', function () {
-    const nodeLayer = { };
-    const node = {
-        parent: { },
-        layerUpdateState: {
-            test: new LayerUpdateState(),
-        },
-        material: {
-            getLayer: () => nodeLayer,
-            visible: true,
-        },
-        getExtentsByProjection: () => ([{ zoom: 0 }]),
-    };
-    const layer = {
-        id: 'test',
-        visible: true,
-        opacity: 1.0,
-        zoom: { max: Infinity, min: 0 },
-    };
+    const renderer = new Renderer();
+    const p = { coord: new Coordinates('EPSG:4326', -75.6114, 40.03428, 0), heading: 180, range: 4000, tilt: 22 };
+
+    const view = new GlobeView(renderer.domElement, p, { renderer, noControls: true });
+    const layer = new ColorLayer('color', { crs: 'EPSG:4326', source: new TMSSource({ crs: 'EPSG:4326', url: 'url' }) });
+    view.tileLayer.colorLayersOrder = [layer.id];
+    view.addLayer(layer);
+
+
+    const extent = new Extent('TMS:4326', 3, 0, 0).as('EPSG:4326');
+    const material = new LayeredMaterial();
+    const geom = new THREE.Geometry();
+    geom.OBB = new OBB(new THREE.Vector3(), new THREE.Vector3(1, 1, 1));
+    const node = new TileMesh(geom, material, view.tileLayer, extent);
+    node.parent = { };
+
+    const context = { view, scheduler: view.mainLoop.scheduler };
 
     it('should correctly initialize opacity & visibility', () => {
-        node.layerUpdateState.test.failure(new Date(), false, { targetLevel: 10 });
-        updateLayeredMaterialNodeImagery(null, layer, node, node.parent);
+        updateLayeredMaterialNodeImagery(context, layer, node, node.parent);
+        const nodeLayer = material.getLayer(layer.id);
+        nodeLayer.textures.push(new THREE.Texture());
         assert.equal(nodeLayer.opacity, layer.opacity);
         assert.equal(nodeLayer.visible, layer.visible);
     });
     it('should update material opacity & visibility', () => {
         layer.opacity = 0.5;
         layer.visible = false;
-        updateLayeredMaterialNodeImagery(null, layer, node, node.parent);
+        const nodeLayer = material.getLayer(layer.id);
         assert.equal(nodeLayer.opacity, layer.opacity);
         assert.equal(nodeLayer.visible, layer.visible);
     });
-    it('should update material opacity & visibility even if layer is cannot be updated', () => {
-        node.layerUpdateState.test.noMoreUpdatePossible();
-        layer.opacity = 0.75;
-        updateLayeredMaterialNodeImagery(null, layer, node, node.parent);
-        assert.equal(nodeLayer.opacity, layer.opacity);
-        assert.equal(nodeLayer.visible, layer.visible);
+    it('should update material uniforms', () => {
+        layer.visible = false;
+        node.onBeforeRender();
+        assert.equal(material.uniforms.colorLayers.value[0].id, undefined);
+
+        layer.visible = true;
+        node.onBeforeRender();
+        assert.equal(material.uniforms.colorLayers.value[0].id, layer.id);
+        assert.equal(material.uniforms.colorLayers.value[0].opacity, layer.opacity);
     });
 });
