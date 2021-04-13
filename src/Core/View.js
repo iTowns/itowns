fcc16da3b9b4a3546ca7940106f2bf700746bee1
gchoqueux <gerald.choqueux@ign.fr>
2021-04-13 13:59:54 +0200

    doc(View): add diffuse parameter documentation.

--- src/Core/View.js
+++ src/Core/View.js
@@ -145,7 +145,10 @@ class View extends THREE.EventDispatcher {
      * configure the renderer (see {@link c3DEngine}.  If not present, a new &lt;canvas> will be created and
      * added to viewerDiv (mutually exclusive with mainLoop)
      * @param {boolean} [options.renderer.isWebGL2=true] - enable webgl 2.0 for THREE.js.
-     * @param {?Scene} options.scene3D - {@link Scene} instance to use, otherwise a default one will be constructed
+     * @param {?Scene} [options.scene3D] - [THREE.Scene](https://threejs.org/docs/#api/en/scenes/Scene) instance to use, otherwise a default one will be constructed
+     * @param {?Color} options.diffuse - [THREE.Color](https://threejs.org/docs/?q=color#api/en/math/Color) Diffuse color terrain material.
+     * This color is applied to terrain if there isn't color layer on terrain extent (by example on pole).
+     *
      * @constructor
      */
     constructor(crs, viewerDiv, options = {}) {
