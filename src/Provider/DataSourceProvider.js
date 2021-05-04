f3879b734f6e9bf2b0ddeb7cad04bf8a519921cb
gchoqueux <gerald.choqueux@ign.fr>
2021-05-04 14:40:12 +0200

    refactor(ColorLayer): remove useless features in ColorLayer command.

--- src/Provider/DataSourceProvider.js
+++ src/Provider/DataSourceProvider.js
@@ -1,10 +1,9 @@
 export default {
     executeCommand(command) {
         const layer = command.layer;
-        const features = command.features || [];
         const src = command.extentsSource;
         const dst = command.extentsDestination || src;
 
-        return Promise.all(src.map((from, i) => (layer.getData(from, dst[i], features[i]))));
+        return Promise.all(src.map((from, i) => (layer.getData(from, dst[i]))));
     },
 };
