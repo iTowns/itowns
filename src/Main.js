ba2b2d4b1bde5c46a52b257fe68a425fec838690
gchoqueux <gerald.choqueux@ign.fr>
2021-07-13 14:47:53 +0200

    chore: expose 3dtiles process methods.

--- src/Main.js
+++ src/Main.js
@@ -96,3 +96,4 @@ export { default as C3DTBatchTable } from './Core/3DTiles/C3DTBatchTable';
 export { default as C3DTExtensions } from './Core/3DTiles/C3DTExtensions';
 export { default as C3DTilesTypes } from './Core/3DTiles/C3DTilesTypes';
 export { default as C3DTBatchTableHierarchyExtension } from './Core/3DTiles/C3DTBatchTableHierarchyExtension';
+export { process3dTilesNode, $3dTilesCulling, $3dTilesSubdivisionControl } from 'Process/3dTilesProcessing';
