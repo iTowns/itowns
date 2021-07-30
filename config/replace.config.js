2f5079510af5f4bdca7e68213edc4a6956bc78aa
gchoqueux <gerald.choqueux@ign.fr>
2021-07-30 14:19:40 +0200

    chore: remove import three examples polyfill.

--- config/replace.config.js
+++ config/replace.config.js
@@ -1,10 +1,9 @@
 const replace = require('replace-in-file');
 const path = require('path');
 const chalk = require('chalk');
-const { filesExamples, patchedPath } = require('./threeExamples.js');
+const { patchedPath } = require('./threeExamples');
 
 const fileGLTFLoader = path.resolve(__dirname.replace('config', ''), patchedPath, './loaders/GLTFLoader.js');
-const files = filesExamples.map(f => path.resolve(__dirname.replace('config', ''), patchedPath, f));
 
 // Patch _BATCHID in GLTFLoader
 const patchBatchID = /JOINTS_0: +'skinIndex',/g;
@@ -14,13 +13,7 @@ replace({
     files: fileGLTFLoader,
     from: patchBatchID,
     to: patchGLTFLoader,
-}).then(() => replace({
-    files,
-    from: /..\/..\/..\/build\/three.module.js/g,
-    to: 'three',
 }).then((t) => {
     // eslint-disable-next-line no-console
-    console.log(chalk.green('Patched Files:'));
-    // eslint-disable-next-line no-console
-    console.log(chalk.green(`\t${t.map(f => path.basename(f.file)).join('\n\t')}`));
-}));
+    console.log(chalk.green(`Patched Files: ${t.map(f => path.basename(f.file)).join('\n\t')}`));
+});
