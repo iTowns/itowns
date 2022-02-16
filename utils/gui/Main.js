d3a01545e0001b69b50472c59b736dd4ac3ff6fc
mgermerie <73115044+mgermerie@users.noreply.github.com>
2022-02-16 17:43:28 +0100

    feature(Widgets): add a widget to display a scale

--- utils/gui/Main.js
+++ utils/gui/Main.js
@@ -1,3 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
 export { default as Navigation } from './Navigation';
 export { default as Minimap } from './Minimap';
+export { default as Scale } from './Scale';
