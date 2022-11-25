47db4c742733e72d39048f45408c065994c1cf68
Vincent JAILLOT <vjaillot@cirilgroup.com>
2022-11-25 12:32:13 +0100

    test(3dtiles): test 3d tiles sources and 3d tiles ion example

---
+++ test/functional/3dtiles_ion.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+
+describe('3dtiles_ion', function _() {
+    let result;
+    before(async () => {
+        result = await loadExample(
+            'examples/3dtiles_ion.html',
+            this.fullTitle(),
+        );
+    });
+
+    it('should run', async () => {
+        assert.ok(result);
+    });
+
+    it('should pick the globe', async () => {
+        const layers = await page.evaluate(
+            () => view.pickObjectsAt({ x: 195, y: 146 }).map(p => p.layer.id),
+        );
+
+        assert.ok(layers.indexOf('globe') >= 0);
+    });
+
+    it('should pick a building from 3D', async () => {
+        await page.evaluate(() => {
+            const lyonExtent = new itowns.Extent('EPSG:4326', 4.85, 4.9, 45.75, 45.77);
+            itowns.CameraUtils.transformCameraToLookAtTarget(view, view.camera.camera3D, lyonExtent);
+        });
+        await waitUntilItownsIsIdle(this.fullTitle());
+        const layers = await page.evaluate(
+            () => view.pickObjectsAt({
+                x: 166,
+                y: 65,
+            })
+                .map(p => p.layer.id),
+        );
+
+        assert.ok(layers.indexOf('3d-tiles-cesium-ion') >= 0);
+    });
+});
