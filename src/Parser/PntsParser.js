b245301e873626d001ad1827e50d9197ebbf1368
Anthony GULLIENT <agullient@ciril.lan>
2023-03-15 11:44:42 +0100

    fix(3dtiles): fix batchtable reading

--- src/Parser/PntsParser.js
+++ src/Parser/PntsParser.js
@@ -66,8 +66,10 @@ export default {
             if (pntsHeader.BTJSONLength > 0) {
                 // parse batch table
                 const sizeBegin = byteOffset + pntsHeader.FTJSONLength + pntsHeader.FTBinaryLength;
-                const BTBuffer = buffer.slice(sizeBegin, pntsHeader.BTJSONLength + sizeBegin);
-                batchTable = new C3DTBatchTable(BTBuffer, pntsHeader.BTJSONLength, pntsHeader.BTBinaryLength, FTJSON.BATCH_LENGTH, registeredExtensions);
+                const BTBuffer = buffer.slice(sizeBegin, pntsHeader.BTJSONLength + pntsHeader.BTBinaryLength + sizeBegin);
+
+                // If the BATCH_ID semantic is not defined, then the Batch Table stores per-point metadata, and the length of the Batch Table arrays will equal POINTS_LENGTH.
+                batchTable = new C3DTBatchTable(BTBuffer, pntsHeader.BTJSONLength, pntsHeader.BTBinaryLength, (FTJSON.BATCH_ID && FTJSON.BATCH_LENGTH) ? FTJSON.BATCH_LENGTH : FTJSON.POINTS_LENGTH, registeredExtensions);
             }
 
             const pnts = { point, batchTable };
