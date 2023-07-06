d26f29e4bc499b3c8cde2e4d3d0d9ec6c07ac86c
ftoromanoff <francois.toromanoff@ign.fr>
2023-07-06 15:52:21 +0200

    refactor(Layer): instanciate new Style at Layer instead of at examples

--- src/Layer/Layer.js
+++ src/Layer/Layer.js
@@ -3,6 +3,7 @@ import { STRATEGY_MIN_NETWORK_TRAFFIC } from 'Layer/LayerUpdateStrategy';
 import InfoLayer from 'Layer/InfoLayer';
 import Source from 'Source/Source';
 import Cache from 'Core/Scheduler/Cache';
+import Style from 'Core/Style';
 
 /**
  * @property {boolean} isLayer - Used to checkout whether this layer is a Layer.
@@ -92,6 +93,9 @@ class Layer extends THREE.EventDispatcher {
             throw new Error(`Layer ${id} needs Source`);
         }
         super();
+        if (config.style && !(config.style instanceof Style)) {
+            config.style = new Style(config.style);
+        }
         this.isLayer = true;
 
         Object.assign(this, config);
