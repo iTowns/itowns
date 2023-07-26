21a363c06bb3434b1b7da21e7442cfb8f9c9ad7f
Alexandre f26 <alexandre@futurmap.com>
2023-07-26 16:04:49 +0200

    examples(glb): add functional test

---
+++ test/functional/itowns_GLB.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+
+describe('itowns_GLB', function _() {
+    let result;
+    before(async () => {
+        result = await loadExample(
+            'examples/itowns_GLB.html',
+            this.fullTitle(),
+        );
+    });
+    
+    it('should run', async () => {
+        assert.ok(result);
+    });
+});
