2f90e3337d633e269a8e48547a9743a8c20451a2
Aymeric DUTREMBLE <adutremble@cirilgroup.com>
2023-09-20 12:20:28 +0200

    feature(VectorTileSource): add support for multiple source

--- test/unit/vectortiles.js
+++ test/unit/vectortiles.js
@@ -10,11 +10,13 @@ import sinon from 'sinon';
 import style from '../data/vectortiles/style.json';
 import tilejson from '../data/vectortiles/tilejson.json';
 import sprite from '../data/vectortiles/sprite.json';
+import mapboxStyle from '../data/mapboxMulti.json';
 
 const resources = {
     'test/data/vectortiles/style.json': style,
     'https://test/tilejson.json': tilejson,
     'https://test/sprite.json': sprite,
+    'https://api.mapbox.com/v4/mapbox.mapbox-terrain-v2,mapbox.mapbox-streets-v7.json': mapboxStyle,
 };
 
 function parse(pbf, layers) {
@@ -27,7 +29,7 @@ function parse(pbf, layers) {
             crs: 'EPSG:3857',
         },
     });
-}
+};
 
 describe('Vector tiles', function () {
     let stub;
@@ -89,159 +91,257 @@ describe('Vector tiles', function () {
             done();
         }).catch(done);
     });
+});
 
-    describe('VectorTilesSource', function () {
-        it('throws an error because no style was provided', () => {
-            assert.throws(() => new VectorTilesSource({}), {
-                name: 'Error',
-                message: 'New VectorTilesSource: style is required',
+describe('VectorTilesSource', function () {
+    let stub;
+    before(function () {
+        stub = sinon.stub(Fetcher, 'json')
+            .callsFake((url) => {
+                url = url.split('?')[0];
+                return Promise.resolve(JSON.parse(resources[url]));
             });
+    });
+    after(function () {
+        stub.restore();
+    });
+
+    it('throws an error because no style was provided', () => {
+        assert.throws(() => new VectorTilesSource({}), {
+            name: 'Error',
+            message: 'New VectorTilesSource: style is required',
+        });
+    });
+
+    it('reads tiles URL directly from the style', (done) => {
+        const source = new VectorTilesSource({
+            style: {
+                sources: { sourceTiles: { tiles: ['http://server.geo/{z}/{x}/{y}.pbf'] } },
+                layers: [],
+            },
+        });
+        source.whenReady.then(() => {
+            assert.equal(source.url.size, 1);
+            // eslint-disable-next-line no-template-curly-in-string
+            assert.ok(source.url.has('http://server.geo/${z}/${x}/${y}.pbf'));
+            done();
+        })
+            .catch(done);
+    });
+
+    it('reads tiles URL from an url', (done) => {
+        const source = new VectorTilesSource({
+            style: {
+                sources: { sourceUrl: { url: 'mapbox://mapbox.mapbox-terrain-v2,mapbox.mapbox-streets-v7' } },
+                layers: [{
+                    id: 'building',
+                    source: 'sourceUrl',
+                    'source-layer': 'building',
+                    type: 'fill',
+                    paint: {
+                        'fill-color': 'red',
+                    },
+                }],
+            },
+            accessToken: 'pk.eyJ1IjoiZnRvcm9tYW5vZmYiLCJhIjoiY2xramhzM2xrMDVibjNpcGNvdGRlZWQ5YyJ9.NibhjJNVTxArsNSH4v_kIA',
+        });
+        source.whenReady
+            .then(() => {
+                assert.equal(source.url, '.');
+                assert.equal(source.urls.size, 1);
+                done();
+            })
+            .catch(done);
+    });
+
+    it('reads the background layer', (done) => {
+        const source = new VectorTilesSource({
+            url: 'fakeurl',
+            style: {
+                sources: { tilejson: {} },
+                layers: [{ type: 'background' }],
+            },
+        });
+        source.whenReady.then(() => {
+            assert.ok(source.backgroundLayer);
+            done();
+        })
+            .catch(done);
+    });
+
+    it('creates styles and assigns filters', (done) => {
+        const source = new VectorTilesSource({
+            url: 'fakeurl',
+            style: {
+                sources: { tilejson: {} },
+                layers: [{
+                    id: 'land',
+                    type: 'fill',
+                    paint: {
+                        'fill-color': 'rgb(255, 0, 0)',
+                    },
+                }],
+            },
+        });
+        source.whenReady.then(() => {
+            assert.ok(source.styles.land);
+            assert.equal(source.styles.land.fill.color, 'rgb(255,0,0)');
+            done();
+        })
+            .catch(done);
+    });
+
+    it('loads the style from a file', function _it(done) {
+        const source = new VectorTilesSource({
+            style: 'test/data/vectortiles/style.json',
+            networkOptions: process.env.HTTPS_PROXY ? { agent: new HttpsProxyAgent(process.env.HTTPS_PROXY) } : {},
+        });
+        source.whenReady
+            .then(() => {
+                assert.equal(source.styles.land.fill.color, 'rgb(255,0,0)');
+                assert.equal(source.styles.land.fill.opacity, 1);
+                assert.equal(source.styles.land.zoom.min, 5);
+                assert.equal(source.styles.land.zoom.max, 13);
+                done();
+            }).catch(done);
+    });
+
+    it('sets the correct Style#zoom.min', (done) => {
+        const source = new VectorTilesSource({
+            url: 'fakeurl',
+            style: {
+                sources: { tilejson: {} },
+                layers: [{
+                    // minzoom is 0 (default value)
+                    id: 'first',
+                    type: 'fill',
+                    paint: {
+                        'fill-color': 'rgb(255, 0, 0)',
+                    },
+                }, {
+                    // minzoom is 5 (specified)
+                    id: 'second',
+                    type: 'fill',
+                    paint: {
+                        'fill-color': 'rgb(255, 0, 0)',
+                    },
+                    minzoom: 5,
+                }, {
+                    // minzoom is 4 (first stop)
+                    // If a style have `stops` expression, should it be used to determine the min zoom?
+                    id: 'third',
+                    type: 'fill',
+                    paint: {
+                        'fill-color': 'rgb(255, 0, 0)',
+                        'fill-opacity': { stops: [[4, 1], [7, 0.5]] },
+                    },
+                }, {
+                    // minzoom is 1 (first stop and no specified minzoom)
+                    id: 'fourth',
+                    type: 'fill',
+                    paint: {
+                        'fill-color': 'rgb(255, 0, 0)',
+                        'fill-opacity': { stops: [[1, 1], [7, 0.5]] },
+                    },
+                }, {
+                    // minzoom is 4 (first stop is higher than specified)
+                    id: 'fifth',
+                    type: 'fill',
+                    paint: {
+                        'fill-color': 'rgb(255, 0, 0)',
+                        'fill-opacity': { stops: [[4, 1], [7, 0.5]] },
+                    },
+                    minzoom: 3,
+                }],
+            },
         });
 
-        it('reads tiles URL from the style', (done) => {
+        source.whenReady.then(() => {
+            assert.equal(source.styles.first.zoom.min, 0);
+            assert.equal(source.styles.second.zoom.min, 5);
+            assert.equal(source.styles.third.zoom.min, 0);
+            assert.equal(source.styles.fourth.zoom.min, 0);
+            assert.equal(source.styles.fifth.zoom.min, 3);
+            done();
+        })
+            .catch(done);
+    });
+
+    it('Vector tile source mapbox url', () => {
+        const accessToken = 'pk.xxxxx';
+        const baseurl = 'mapbox://styles/mapbox/outdoors-v11';
+
+        const styleUrl = urlParser.normalizeStyleURL(baseurl, accessToken);
+        assert.ok(styleUrl.startsWith('https://api.mapbox.com'));
+        assert.ok(styleUrl.endsWith(accessToken));
+
+        const spriteUrl = urlParser.normalizeSpriteURL(baseurl, '', '.json', accessToken);
+        assert.ok(spriteUrl.startsWith('https'));
+        assert.ok(spriteUrl.endsWith(accessToken));
+        assert.ok(spriteUrl.includes('sprite.json'));
+
+        const imgUrl = urlParser.normalizeSpriteURL(baseurl, '', '.png', accessToken);
+        assert.ok(imgUrl.includes('sprite.png'));
+
+        const url = 'mapbox://mapbox.mapbox-streets-v8,mapbox.mapbox-terrain-v2';
+        const urlSource = urlParser.normalizeSourceURL(url, accessToken);
+        assert.ok(urlSource.startsWith('https'));
+        assert.ok(urlSource.endsWith(accessToken));
+        assert.ok(urlSource.includes('.json'));
+    });
+
+    describe('multisource', function () {
+        it('2 sources with different url tiles', (done) => {
             const source = new VectorTilesSource({
                 style: {
-                    sources: { tilesurl: { tiles: ['http://server.geo/{z}/{x}/{y}.pbf'] } },
+                    sources: {
+                        source1: {
+                            type: 'vector',
+                            tiles: ['http://server.geo/{z}/{x}/{y}.pbf'],
+                        },
+                        source2: {
+                            type: 'vector',
+                            tiles: ['http://server.geo2/{z}/{x}/{y}.pbf'],
+                        },
+                    },
                     layers: [],
                 },
             });
-            source.whenReady.then(() => {
-                // eslint-disable-next-line no-template-curly-in-string
-                assert.equal(source.url, 'http://server.geo/${z}/${x}/${y}.pbf');
-                done();
-            }).catch(done);
+            source.whenReady
+                .then(() => {
+                    assert.equal(source.url.size, 2);
+                    // eslint-disable-next-line no-template-curly-in-string
+                    assert.ok(source.url.has('http://server.geo/${z}/${x}/${y}.pbf'));
+                    // eslint-disable-next-line no-template-curly-in-string
+                    assert.ok(source.url.has('http://server.geo2/${z}/${x}/${y}.pbf'));
+                    done();
+                })
+                .catch(done);
         });
-
-        it('reads the background layer', (done) => {
+        it('2 sources with same url tiles', (done) => {
             const source = new VectorTilesSource({
-                url: 'fakeurl',
                 style: {
-                    sources: { tilejson: {} },
-                    layers: [{ type: 'background' }],
-                },
-            });
-            source.whenReady.then(() => {
-                assert.ok(source.backgroundLayer);
-                done();
-            }).catch(done);
-        });
-
-        it('creates styles and assigns filters', (done) => {
-            const source = new VectorTilesSource({
-                url: 'fakeurl',
-                style: {
-                    sources: { tilejson: {} },
-                    layers: [{
-                        id: 'land',
-                        type: 'fill',
-                        paint: {
-                            'fill-color': 'rgb(255, 0, 0)',
+                    sources: {
+                        source1: {
+                            type: 'vector',
+                            tiles: ['http://server.geo/{z}/{x}/{y}.pbf'],
                         },
-                    }],
+                        source2: {
+                            type: 'vector',
+                            tiles: ['http://server.geo/{z}/{x}/{y}.pbf'],
+                        },
+                    },
+                    layers: [],
                 },
-            });
-            source.whenReady.then(() => {
-                assert.ok(source.styles.land);
-                assert.equal(source.styles.land.fill.color, 'rgb(255,0,0)');
-                done();
-            }).catch(done);
-        });
-
-        it('loads the style from a file', function _it(done) {
-            const source = new VectorTilesSource({
-                style: 'test/data/vectortiles/style.json',
             });
             source.whenReady
                 .then(() => {
-                    assert.equal(source.styles.land.fill.color, 'rgb(255,0,0)');
-                    assert.equal(source.styles.land.fill.opacity, 1);
-                    assert.equal(source.styles.land.zoom.min, 5);
-                    assert.equal(source.styles.land.zoom.max, 13);
+                    assert.equal(source.url.size, 1);
+                    // eslint-disable-next-line no-template-curly-in-string
+                    assert.ok(source.url.has('http://server.geo/${z}/${x}/${y}.pbf'));
                     done();
-                }).catch(done);
-        });
-
-        it('sets the correct Style#zoom.min', (done) => {
-            const source = new VectorTilesSource({
-                url: 'fakeurl',
-                style: {
-                    sources: { tilejson: {} },
-                    layers: [{
-                        // minzoom is 0 (default value)
-                        id: 'first',
-                        type: 'fill',
-                        paint: {
-                            'fill-color': 'rgb(255, 0, 0)',
-                        },
-                    }, {
-                        // minzoom is 5 (specified)
-                        id: 'second',
-                        type: 'fill',
-                        paint: {
-                            'fill-color': 'rgb(255, 0, 0)',
-                        },
-                        minzoom: 5,
-                    }, {
-                        // minzoom is 4 (first stop)
-                        // If a style have `stops` expression, should it be used to determine the min zoom?
-                        id: 'third',
-                        type: 'fill',
-                        paint: {
-                            'fill-color': 'rgb(255, 0, 0)',
-                            'fill-opacity': { stops: [[4, 1], [7, 0.5]] },
-                        },
-                    }, {
-                        // minzoom is 1 (first stop and no specified minzoom)
-                        id: 'fourth',
-                        type: 'fill',
-                        paint: {
-                            'fill-color': 'rgb(255, 0, 0)',
-                            'fill-opacity': { stops: [[1, 1], [7, 0.5]] },
-                        },
-                    }, {
-                        // minzoom is 4 (first stop is higher than specified)
-                        id: 'fifth',
-                        type: 'fill',
-                        paint: {
-                            'fill-color': 'rgb(255, 0, 0)',
-                            'fill-opacity': { stops: [[4, 1], [7, 0.5]] },
-                        },
-                        minzoom: 3,
-                    }],
-                },
-            });
-
-            source.whenReady.then(() => {
-                assert.equal(source.styles.first.zoom.min, 0);
-                assert.equal(source.styles.second.zoom.min, 5);
-                assert.equal(source.styles.third.zoom.min, 0);
-                assert.equal(source.styles.fourth.zoom.min, 0);
-                assert.equal(source.styles.fifth.zoom.min, 3);
-                done();
-            }).catch(done);
-        });
-
-        it('Vector tile source mapbox url', () => {
-            const accessToken = 'pk.xxxxx';
-            const baseurl = 'mapbox://styles/mapbox/outdoors-v11';
-
-            const styleUrl = urlParser.normalizeStyleURL(baseurl, accessToken);
-            assert.ok(styleUrl.startsWith('https://api.mapbox.com'));
-            assert.ok(styleUrl.endsWith(accessToken));
-
-            const spriteUrl = urlParser.normalizeSpriteURL(baseurl, '', '.json', accessToken);
-            assert.ok(spriteUrl.startsWith('https'));
-            assert.ok(spriteUrl.endsWith(accessToken));
-            assert.ok(spriteUrl.includes('sprite.json'));
-
-            const imgUrl = urlParser.normalizeSpriteURL(baseurl, '', '.png', accessToken);
-            assert.ok(imgUrl.includes('sprite.png'));
-
-            const url = 'mapbox://mapbox.mapbox-streets-v8,mapbox.mapbox-terrain-v2';
-            const urlSource = urlParser.normalizeSourceURL(url, accessToken);
-            assert.ok(urlSource.startsWith('https'));
-            assert.ok(urlSource.endsWith(accessToken));
-            assert.ok(urlSource.includes('.json'));
+                })
+                .catch(done);
         });
     });
 });
