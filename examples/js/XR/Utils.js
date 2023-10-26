fb5125c5b7d66e889361fe16d028bbf789a4bb17
Jonathan GARNIER <jgarnier@cirilgroup.com>
2023-10-26 17:13:54 +0200

    feat debug tool now hideable
    + better documentation

--- examples/js/XR/Utils.js
+++ examples/js/XR/Utils.js
@@ -1,6 +1,17 @@
+/**
+ * Reads parameter contextXR.showDebug
+ */
 const XRUtils = {};
 
-XRUtils.showPosition = function(name, coordinates, color, radius = 50) {
+XRUtils.objects = [];
+
+XRUtils.updateDebugVisibilities = function(showDebugValue) {
+    XRUtils.objects.forEach((obj) => { obj.visible = showDebugValue;});
+    contextXR.visibleBbox.visible = showDebugValue;
+    view.notifyChange();
+}
+
+XRUtils.showPosition = function(name, coordinates, color, radius = 50, isDebug = false) {
     var existingChild = findExistingRef(name);
 
     if(existingChild) {
@@ -13,7 +24,7 @@ XRUtils.showPosition = function(name, coordinates, color, radius = 50) {
         );
         previousPos.name = name;
         previousPos.position.copy(coordinates);
-        view.scene.add(previousPos);
+        XRUtils.addToScene(previousPos, isDebug);
         existingChild = previousPos;
     }
     return existingChild;
@@ -23,6 +34,9 @@ XRUtils.removeReference = function(name) {
     var existingChild = findExistingRef(name);
     if(existingChild) {
         view.scene.remove(existingChild);
+        var indexToRemove = null;
+        XRUtils.objects.forEach((child, index) => {if(child.name === name) { indexToRemove = index;} });
+        XRUtils.objects.splice(indexToRemove, 1);
     } 
 }
 
@@ -32,7 +46,7 @@ XRUtils.removeReference = function(name) {
  * @param {Vector3} coordinates 
  * @param {String} color hexa color
  */
-XRUtils.addPositionPoints = function(name, coordinates, color, size) {
+XRUtils.addPositionPoints = function(name, coordinates, color, size, isDebug = false) {
     var existingChild = findExistingRef(name);
     if(existingChild) {
         var verticesUpdated = existingChild.geometry.attributes.position.array.values().toArray();
@@ -47,11 +61,11 @@ XRUtils.addPositionPoints = function(name, coordinates, color, size) {
         geometry.setAttribute( 'position', new itowns.THREE.Float32BufferAttribute(vertices, 3));
         var particle = new itowns.THREE.Points( geometry, material );
         particle.name = name;
-        view.scene.add(particle);
+        XRUtils.addToScene(particle, isDebug);
     }
 }
 
-XRUtils.showPositionVerticalLine = function(name, coordinates, color, upSize) {
+XRUtils.showPositionVerticalLine = function(name, coordinates, color, upSize, isDebug = false) {
     var existingChild = findExistingRef(name);
     if(existingChild) {
         existingChild.position.copy(coordinates);
@@ -69,11 +83,11 @@ XRUtils.showPositionVerticalLine = function(name, coordinates, color, upSize) {
         //necessary to "look" vertically
         line.lookAt(new itowns.THREE.Vector3(0, 0, 1));
         line.name = name;
-        view.scene.add(line);
+        XRUtils.addToScene(line, isDebug);
     }
 }
 
-XRUtils.renderdirectionArrow = function(name, originVector3, directionVector3, scale, color) {
+XRUtils.renderdirectionArrow = function(name, originVector3, directionVector3, scale, color, isDebug = false) {
     var existingChild = findExistingRef(name);
     if(existingChild) {
         existingChild.setDirection(directionVector3);
@@ -82,8 +96,66 @@ XRUtils.renderdirectionArrow = function(name, originVector3, directionVector3, s
     else {
         const arrow = new itowns.THREE.ArrowHelper(directionVector3, originVector3, scale, color);
         arrow.name = name;
-        view.scene.add(arrow);
+        XRUtils.addToScene(arrow, isDebug);
     }
+}
+
+XRUtils.generateVRBox = function(object) {
+    const objectBbox = new itowns.THREE.Box3();
+    // better than object.geometry.computeBoundingBox(); as it copy parent position.
+    objectBbox.setFromObject(object);
+    object.VRBbox = objectBbox;
+    object.VRBbox = new itowns.THREE.Box3Helper(object.VRBbox, 0xffff00);
+
+    object.VRBbox.name = object.name +'_VRBbox';
+    console.log('adding VRBbox to scene : ', object.name);
+    // no need to add each bbox to the Utils memory
+    view.scene.add(object.VRBbox);
+    object.VRBbox.visible = false;
+    return object.VRBbox;
+}
+
+XRUtils.updateBboxVisibility = function(object) {
+    if(!contextXR.showDebug){
+        return;
+    }
+    if(contextXR.visibleBbox && contextXR.visibleBbox === object.VRBbox){
+        return;
+    }
+    // proper to box3Helper
+    if(object.box) { 
+        if (!object.visible) {
+            resetPreviousVisibleeBbox();
+            contextXR.visibleBbox = object;
+            object.visible = true;
+        }
+    }
+    else if(object.geometry) {
+        if(!object.VRBbox) {
+            XRUtils.generateVRBox(object);
+        }
+        if (!object.VRBbox.visible) {
+            resetPreviousVisibleeBbox();
+            contextXR.visibleBbox = object.VRBbox;
+            object.VRBbox.visible = true;
+        }
+    } else if(contextXR.visibleBbox) {
+        resetPreviousVisibleeBbox();
+    }
+
+    function resetPreviousVisibleeBbox() {
+        if(contextXR.visibleBbox) {
+            contextXR.visibleBbox.visible = false;
+            contextXR.visibleBbox = undefined;
+        }
+    }
+}
+
+XRUtils.addToScene = function(object, isDebug) {
+    console.log('adding object to scene : ', object.name);
+    object.visible = !isDebug || (isDebug && contextXR.showDebug);
+    view.scene.add(object);
+    XRUtils.objects.push(object);
 }
 
 function findExistingRef(name) {
@@ -91,3 +163,5 @@ function findExistingRef(name) {
     view.scene.children.forEach((child) => {if(child.name === name) { existingChild = child;} });
     return existingChild;
 }
+
+
