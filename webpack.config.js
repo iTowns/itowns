abc6bbb223c0f520df58c205d465435250bfdb2c
Bouillaguet Quentin <git@desplandis.fr>
2023-12-22 21:19:26 +0100

    chore(polyfills): remove polyfill for async/await

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,6 @@ module.exports = () => {
         entry: {
             itowns: [
                 'core-js',
-                'regenerator-runtime/runtime',
                 'url-polyfill',
                 'whatwg-fetch',
                 './src/MainBundle.js',
