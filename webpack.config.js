1928986659590433a3f6fd4db1adf94eb1fe9ded
Bouillaguet Quentin <git@desplandis.fr>
2024-01-24 15:31:53 +0100

    chore(webpack): remove URL polyfill on bundle

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,6 @@ module.exports = () => {
         entry: {
             itowns: [
                 'core-js',
-                'url-polyfill',
                 'whatwg-fetch',
                 './src/MainBundle.js',
             ],
