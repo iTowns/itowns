21a458443caf96f3284b05c394739bf3efd2833a
ftoromanoff <francois.toromanoff@ign.fr>
2024-04-30 17:26:55 +0200

    fix(crs): add support for non EPSG projection

--- src/Core/Geographic/Crs.js
+++ src/Core/Geographic/Crs.js
@@ -64,7 +64,7 @@ function toUnit(crs) {
         case 'EPSG:4326' : return UNIT.DEGREE;
         case 'EPSG:4978' : return UNIT.METER;
         default: {
-            const p = proj4.defs(formatToEPSG(crs));
+            const p = proj4.defs(crs);
             if (!p) {
                 return undefined;
             }
