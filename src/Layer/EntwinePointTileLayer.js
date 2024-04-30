69302ab17473f3118fdf932d1c71e0258eac29be
ftoromanoff <francois.toromanoff@ign.fr>
2024-04-30 17:32:01 +0200

    refactor(EntwinePointTileLayer): proj boundsConforming in view crs

--- src/Layer/EntwinePointTileLayer.js
+++ src/Layer/EntwinePointTileLayer.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import EntwinePointTileNode from 'Core/EntwinePointTileNode';
 import PointCloudLayer from 'Layer/PointCloudLayer';
 import Extent from 'Core/Geographic/Extent';
+import Coordinates from 'Core/Geographic/Coordinates';
 
 const bboxMesh = new THREE.Mesh();
 const box3 = new THREE.Box3();
@@ -51,21 +52,38 @@ class EntwinePointTileLayer extends PointCloudLayer {
 
         const resolve = this.addInitializationStep();
         this.whenReady = this.source.whenReady.then(() => {
+            const crs = this.crs || 'EPSG:4326';
+            if (this.crs !== config.crs) { console.warn('layer.crs is different from View.crs'); }
             this.root = new EntwinePointTileNode(0, 0, 0, 0, this, -1);
 
-            this.root.bbox.min.fromArray(this.source.boundsConforming, 0);
-            this.root.bbox.max.fromArray(this.source.boundsConforming, 3);
+            const coord = new Coordinates(this.source.crs || config.crs, 0, 0, 0);
+            const coordBoundsMin = new Coordinates(crs, 0, 0, 0);
+            const coordBoundsMax = new Coordinates(crs, 0, 0, 0);
+            coord.setFromValues(
+                this.source.boundsConforming[0],
+                this.source.boundsConforming[1],
+                this.source.boundsConforming[2],
+            );
+            coord.as(crs, coordBoundsMin);
+            coord.setFromValues(
+                this.source.boundsConforming[3],
+                this.source.boundsConforming[4],
+                this.source.boundsConforming[5],
+            );
+            coord.as(crs, coordBoundsMax);
+
+            this.root.bbox.setFromPoints([coordBoundsMin.toVector3(), coordBoundsMax.toVector3()]);
 
             this.minElevationRange = this.source.boundsConforming[2];
             this.maxElevationRange = this.source.boundsConforming[5];
 
-            this.extent = Extent.fromBox3(config.crs || 'EPSG:4326', this.root.bbox);
+            this.extent = Extent.fromBox3(crs, this.root.bbox);
 
             // NOTE: this spacing is kinda arbitrary here, we take the width and
             // length (height can be ignored), and we divide by the specified
             // span in ept.json. This needs improvements.
-            this.spacing = (Math.abs(this.source.boundsConforming[3] - this.source.boundsConforming[0])
-                + Math.abs(this.source.boundsConforming[4] - this.source.boundsConforming[1])) / (2 * this.source.span);
+            this.spacing = (Math.abs(coordBoundsMax.x - coordBoundsMin.x)
+                + Math.abs(coordBoundsMax.y - coordBoundsMin.y)) / (2 * this.source.span);
 
             return this.root.loadOctree().then(resolve);
         });
