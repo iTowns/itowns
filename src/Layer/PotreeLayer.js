5cc5626294111e2fbeb22941c2eb6ea8138ebd31
ftoromanoff <francois.toromanoff@ign.fr>
2024-05-02 10:55:23 +0200

    refactor: potree and copc Data -> add node.obb

--- src/Layer/PotreeLayer.js
+++ src/Layer/PotreeLayer.js
@@ -62,6 +62,7 @@ class PotreeLayer extends PointCloudLayer {
             this.root = new PotreeNode(0, 0, this);
             this.root.bbox.min.set(cloud.boundingBox.lx, cloud.boundingBox.ly, cloud.boundingBox.lz);
             this.root.bbox.max.set(cloud.boundingBox.ux, cloud.boundingBox.uy, cloud.boundingBox.uz);
+            this.root.obb.position = new THREE.Vector3();
 
             this.extent = Extent.fromBox3(this.source.crs || 'EPSG:4326', this.root.bbox);
             return this.root.loadOctree().then(resolve);
