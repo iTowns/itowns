5e60bb1438f2ff3e5a3c767b6fd6d391d6475fc3
Vincent JAILLOT <vjaillot@cirilgroup.com>
2024-06-21 19:23:23 +0200

    feat(3dtiles): create OGC3DTiles sources

---
+++ src/Source/OGC3DTilesSource.js
@@ -0,0 +1,29 @@
+import Source from 'Source/Source';
+
+/**
+ * @classdesc
+ * An object defining the source connection to a 3DTiles dataset from a web server.
+ *
+ * @extends Source
+ *
+ * @property {boolean} isOGC3DTilesSource - Used to check if this source is an isOGC3DTilesSource. Set to true.
+ * You should not change this, as it is used internally for optimisation.
+ * @property {string} url - The URL of the tileset json.
+ */
+class OGC3DTilesSource extends Source {
+    /**
+     * Create a new Source for 3D Tiles data from a web server.
+     *
+     * @constructor
+     *
+     * @param {Object} source An object that can contain all properties of OGC3DTilesSource and of {@link Source}.
+     * Only `url` is mandatory.
+     * @param {string} source.url - The URL of the tileset json.
+     */
+    constructor(source) {
+        super(source);
+        this.isOGC3DTilesSource = true;
+    }
+}
+
+export default OGC3DTilesSource;
