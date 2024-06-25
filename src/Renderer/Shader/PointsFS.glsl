7f937241c636f27968dc12b582d6d0b5c39d9468
Bouillaguet Quentin <git@desplandis.fr>
2024-06-25 19:59:40 +0200

    refacto(points): material as superset of three PointsMaterial
    BREAKING CHANGE:
    * Remove overlayColor property (replaced by the standard diffuse property)

--- src/Renderer/Shader/PointsFS.glsl
+++ src/Renderer/Shader/PointsFS.glsl
@@ -1,13 +1,23 @@
-#include <itowns/precision_qualifier>
-#include <logdepthbuf_pars_fragment>
+#define USE_COLOR_ALPHA
 
-varying vec4 vColor;
+#include <color_pars_fragment>
+#include <map_particle_pars_fragment>
+#include <alphatest_pars_fragment>
+#include <alphahash_pars_fragment>
+#include <fog_pars_fragment>
+#include <logdepthbuf_pars_fragment>
+#include <clipping_planes_pars_fragment>
+
+uniform vec3 diffuse;
+uniform float opacity;
+
 uniform bool picking;
 uniform int shape;
 
 void main() {
-    #include <logdepthbuf_fragment>
-    //square shape does not require any change.
+
+// Early discard (clipping planes and shape)
+#include <clipping_planes_pars_fragment>
     if (shape == PNTS_SHAPE_CIRCLE) {
         //circular rendering in glsl
         if ((length(gl_PointCoord - 0.5) > 0.5) || (vColor.a == 0.0)) {
@@ -15,5 +25,19 @@ void main() {
         }
     }
 
-    gl_FragColor = vColor;
+#include <logdepthbuf_fragment>
+
+    vec4 diffuseColor = vec4(diffuse, opacity);
+#include <map_particle_fragment>
+#include <color_fragment>
+
+#include <alphatest_fragment>
+#include <alphahash_fragment>
+
+    vec3 outgoingLight = diffuseColor.rgb;
+#include <opaque_fragment> // gl_FragColor
+#include <tonemapping_fragment>
+#include <fog_fragment>
+#include <premultiplied_alpha_fragment>
+
 }
