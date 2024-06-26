14a2431c121d3d56b91ee19b8e929f5dfcd688f0
Raphaël Duhen <raphael.duhen@ign.fr>
2024-06-26 16:55:24 +0200

    feat: adapt InputNode to the new static mode

--- src/Graph/Nodes/InputNode.ts
+++ src/Graph/Nodes/InputNode.ts
@@ -4,14 +4,16 @@ import { Type, DumpDotNodeStyle, Mappings, Graph } from '../Prelude';
 /** Represents a node that outputs a constant value. */
 export default class InputNode extends GraphNode {
     private _type: Type;
+    private _value: unknown;
 
-    public constructor(public value: any, type?: Type) {
+    public constructor(value: unknown, type?: Type) {
         const ty = type ?? Mappings.typeOf(value);
         if (ty == undefined) {
             throw new Error('Input node type could not be inferred');
         }
 
         super(new Map(), ty, true);
+        this._value = value;
         this._type = ty;
     }
 
@@ -20,16 +22,21 @@ export default class InputNode extends GraphNode {
     }
 
     protected override _apply(_graph?: Graph, frame: number = 0): void {
-        this.updateOutputs({ [GraphNode.defaultIoName]: this.value }, frame);
+        this.updateOutputs({ [GraphNode.defaultIoName]: this._value }, frame);
     }
 
     public override get nodeType(): string {
         return InputNode.name;
     }
 
+    public set value(value: unknown) {
+        this._value = value;
+        this.needsUpdate();
+    }
+
     public override get dumpDotStyle(): DumpDotNodeStyle {
         return {
-            label: name => `${name}: ${Mappings.stringify(this.value)}`,
+            label: name => `${name}: ${Mappings.stringify(this._value)}`,
             attrs: {
                 color: 'goldenrod',
             },
