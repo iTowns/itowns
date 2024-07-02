173aa3a7cc9e3b274c0f7b942a3b4a2adefb93a5
gchoqueux <gerald.choqueux@ign.fr>
2024-07-02 18:04:25 +0200

    chore: use babelrc.cjs to configuring babel

---
+++ packages/Main/.babelrc.cjs
@@ -0,0 +1,48 @@
+const BABEL_ENV = process.env.BABEL_ENV;
+
+const alias_itowns_geodesy = `../Geodesy/${BABEL_ENV == 'test' ? 'lib' : 'src'}/Main.js`;
+
+module.exports = (api) => {
+    api.cache(true);
+    return {
+        presets: [
+            ['@babel/preset-env', {
+                targets: {
+                    browsers: "defaults and supports webgl2"
+                },
+                modules: false
+            }]
+        ],
+        plugins: [
+            ['module-resolver', {
+                cwd: 'packagejson',
+                root: ['./src'],
+                alias: {
+                    '@itowns/geodesy': alias_itowns_geodesy
+                }
+            }],
+            ['babel-plugin-inline-import', {
+                extensions: [
+                    '.json',
+                    '.geojson',
+                    '.glsl',
+                    '.css'
+                ]
+            }],
+            ['module-extension-resolver'],
+            ['@babel/plugin-transform-runtime', {
+                'regenerator': false
+            }],
+            ['minify-replace', {
+                replacements: [{
+                    identifierName: "__DEBUG__",
+                    replacement: {
+                        type: 'booleanLiteral',
+                        value: false
+                    }
+                }]
+            }],
+            ['minify-dead-code-elimination']
+        ]
+    };
+};
