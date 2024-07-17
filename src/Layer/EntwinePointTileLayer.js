090c2bdcd7547bf0046d84e9c8f64bc15cec4d3e
ftoromanoff <francois.toromanoff@ign.fr>
2024-07-17 14:25:23 +0200

    refactor(AllPointCloudLayer): supp this.extent

--- src/Layer/EntwinePointTileLayer.js
+++ src/Layer/EntwinePointTileLayer.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import EntwinePointTileNode from 'Core/EntwinePointTileNode';
 import PointCloudLayer from 'Layer/PointCloudLayer';
-import Extent from 'Core/Geographic/Extent';
 import Coordinates from 'Core/Geographic/Coordinates';
 import proj4 from 'proj4';
 
@@ -48,7 +47,6 @@ class EntwinePointTileLayer extends PointCloudLayer {
 
         const resolve = this.addInitializationStep();
         this.whenReady = this.source.whenReady.then(() => {
-            const crs = this.crs || 'EPSG:4326';
             if (this.crs !== config.crs) { console.warn('layer.crs is different from View.crs'); }
 
             // NOTE: this spacing is kinda arbitrary here, we take the width and
@@ -87,7 +85,6 @@ class EntwinePointTileLayer extends PointCloudLayer {
             ];
 
             this.root.bbox.setFromArray(bounds);
-            this.extent = Extent.fromBox3(crs, this.root.bbox);
 
             // for OBB
             // Get the transformation between the data coordinate syteme and the view's.
