5545d5e2b18aea9cf747ac64dec507d4dcb0da95
Vincent JAILLOT <vjaillot@cirilgroup.com>
2024-07-18 11:00:55 +0200

    feat(3d-tiles): clean OGC3DTilesLayer

--- src/Layer/OGC3DTilesLayer.js
+++ src/Layer/OGC3DTilesLayer.js
@@ -11,6 +11,11 @@ import ReferLayerProperties from 'Layer/ReferencingLayerProperties';
 // Temporary exported to be used in deprecated B3dmParser
 export const itownsGLTFLoader = new iGLTFLoader();
 
+// Instantiated by the first tileset. Used to share cache and download and parse queues between tilesets
+let lruCache = null;
+let downloadQueue = null;
+let parseQueue = null;
+
 /**
  * Enable loading 3D Tiles with [Draco](https://google.github.io/draco/) geometry extension.
  *
@@ -49,19 +54,11 @@ export function enableKtx2Loader(path, renderer) {
     itownsGLTFLoader.setKTX2Loader(ktx2Loader);
 }
 
-// TODO: This syntax is currently not possible with the current 3d-tiles-renderer-js API -> open a PR
-// It will allow to share cache and download/parse queue between tilesets
-// const lruCache = new LRUCache();
-// const downloadQueue = new PriorityQueue();
-// const parseQueue = new PriorityQueue();
-
 class OGC3DTilesLayer extends GeometryLayer {
     constructor(id, config) {
         super(id, new THREE.Group(), { source: config.source });
         this.isOGC3DTilesLayer = true;
 
-        // TODO: should this really be done here and like this (i.e. each option is passed in individually?)
-        //  I think we should use the Style API instead :)
         this._handlePointsMaterialConfig(config);
 
         if (config.source.isOGC3DTilesIonSource) {
@@ -76,23 +73,50 @@ class OGC3DTilesLayer extends GeometryLayer {
 
         this.tilesRenderer.manager.addHandler(/\.gltf$/, itownsGLTFLoader);
 
-        // Set cache, download and parse queues to be shared amongst 3D tiles layers (waiting for 3d-tiles-renderer-js api change)
-        // this.tilesRenderer.lruCache = lruCache;
-        // this.tilesRenderer.downloadQueue = downloadQueue;
-        // this.tilesRenderer.parseQueue = parseQueue;
+        this._setupCacheAndQueues();
 
         this.object3d.add(this.tilesRenderer.group);
 
-        // Add an initialization step that is resolved when the root tileset is loaded (see this._setup below)
+        // Add an initialization step that is resolved when the root tileset is loaded (see this._setup below), meaning
+        // that the layer will be marked ready when the tileset has been loaded.
         this._res = this.addInitializationStep();
     }
 
-    // TODO: what happens if the layer is added to multiple views? Should we store multiple tilesRenderer?
-    // How does it work for other layer types?
+    // Store points material config so they can be used later to substitute points tiles material by our own PointsMaterial
+    // These properties should eventually be managed through the Style API (see https://github.com/iTowns/itowns/issues/2336)
+    _handlePointsMaterialConfig(config) {
+        this.pntsMode = config.pntsMode;
+        this.pntsShape = config.pntsShape;
+        this.classification = config.classification;
+        this.pntsSizeMode = config.pntsSizeMode;
+        this.pntsMinAttenuatedSize = config.pntsMinAttenuatedSize || 3;
+        this.pntsMaxAttenuatedSize = config.pntsMaxAttenuatedSize || 10;
+    }
+
+    // Sets the lruCache and download and parse queues so they are shared amongst all tilesets
+    _setupCacheAndQueues() {
+        if (lruCache === null) {
+            lruCache = this.tilesRenderer.lruCache;
+        } else {
+            this.tilesRenderer.lruCache = lruCache;
+        }
+        if (downloadQueue === null) {
+            downloadQueue = this.tilesRenderer.downloadQueue;
+        } else {
+            this.tilesRenderer.downloadQueue = downloadQueue;
+        }
+        if (parseQueue === null) {
+            parseQueue = this.tilesRenderer.parseQueue;
+        } else {
+            this.tilesRenderer.parseQueue = parseQueue;
+        }
+    }
+
+    // Setup 3D tiles renderer js TilesRenderer with the camera, binds events and start updating. Executed when the
+    // layer has been added to the view.
     _setup(view) {
         this.tilesRenderer.setCamera(view.camera3D);
         this.tilesRenderer.setResolutionFromRenderer(view.camera3D, view.renderer);
-        // TODO: should we store the tileset and our own list of models? or at least provide an API to access them
         // Setup whenReady to be fullfiled when the root tileset has been loaded
         let rootTilesetLoaded = false;
         this.tilesRenderer.onLoadTileSet = () => {
@@ -112,8 +136,7 @@ class OGC3DTilesLayer extends GeometryLayer {
         this.tilesRenderer.update();
     }
 
-    // TODO: Not fond of this implementation, I would rather add the possibility to pass a PointsMaterial to
-    //  3d-tiles-renderer-js TilesRenderer instead
+    // Replaces points tiles material with our own PointsMaterial
     _replacePointsMaterial(model) {
         if (!model || !model.isPoints) { return; }
         const oldMat = model.material;
@@ -134,7 +157,6 @@ class OGC3DTilesLayer extends GeometryLayer {
         oldMat.dispose();
         ReferLayerProperties(model.material, this);
         // Setup classification bufferAttribute
-        // TODO: add the possibility to configure the classification attribute name
         if (model.batchTable) {
             const classificationData =  model.batchTable.getData('Classification');
             if (classificationData) {
@@ -143,19 +165,8 @@ class OGC3DTilesLayer extends GeometryLayer {
         }
     }
 
-    _handlePointsMaterialConfig(config) {
-        this.pntsMode = config.pntsMode;
-        this.pntsShape = config.pntsShape;
-        this.classification = config.classification;
-        this.pntsSizeMode = config.pntsSizeMode;
-        this.pntsMinAttenuatedSize = config.pntsMinAttenuatedSize || 3;
-        this.pntsMaxAttenuatedSize = config.pntsMaxAttenuatedSize || 10;
-    }
-
     preUpdate() {
         this.tilesRenderer.update();
-        // const str = `Downloading: ${this.tilesRenderer.stats.downloading} Parsing: ${this.tilesRenderer.stats.parsing} Visible: ${this.tilesRenderer.visibleTiles.size}`;
-        // console.log(str);
         return null; // don't return any element because 3d-tiles-renderer updates them
     }
 
@@ -167,7 +178,21 @@ class OGC3DTilesLayer extends GeometryLayer {
         this.tilesRenderer.dispose();
     }
 
-    // TODO Methods: attach; detach; getObjectToUpdateForAttachedLayers; getC3DTileFeatureFromIntersectsArray?
+    // eslint-disable-next-line no-unused-vars
+    attach(layer) {
+        console.warn('[OGC3DTilesLayer]: Attaching / detaching layers is not yet implemented for OGC3DTilesLayer.');
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    detach(layer) {
+        console.warn('[OGC3DTilesLayer]: Attaching / detaching layers is not yet implemented for OGC3DTilesLayer.');
+        return true;
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    getObjectToUpdateForAttachedLayers(obj) {
+        return null;
+    }
 }
 
 export default OGC3DTilesLayer;
