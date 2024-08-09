3f5266924c7d846a0d3e95cdb51fe734f72bdbdf
Vincent JAILLOT <vjaillot@cirilgroup.com>
2024-08-09 09:29:21 +0200

    Add draco to itowns bundle

--- src/MainBundle.js
+++ src/MainBundle.js
@@ -1,8 +1,10 @@
 import * as THREE from 'three';
+import { DRACOLoader } from 'ThreeExtended/loaders/DRACOLoader';
 import { KTX2Loader } from 'ThreeExtended/loaders/KTX2Loader';
 import { CSS2DObject, CSS2DRenderer } from 'ThreeExtended/renderers/CSS2DRenderer';
 
 export { THREE };
+export { DRACOLoader };
 export { KTX2Loader };
 export { CSS2DObject, CSS2DRenderer };
 
