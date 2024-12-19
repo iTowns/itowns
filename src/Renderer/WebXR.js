59ee691e1f3784cf53dd3205fdabc8dbc7bb10d7
Anthony GULLIENT <agullient@cirilgroup.com>
2024-12-19 12:06:52 +0100

    working controller

--- src/Renderer/WebXR.js
+++ src/Renderer/WebXR.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { XRControllerModelFactory } from  'ThreeExtended/webxr/XRControllerModelFactory';
 import Coordinates from 'Core/Geographic/Coordinates';
 import DEMUtils from 'Utils/DEMUtils';
+import { Matrix4, Vector3 } from 'three';
 
 async function shutdownXR(session) {
     if (session) {
@@ -16,100 +17,156 @@ async function shutdownXR(session) {
  */
 const initializeWebXR = (view, options) => {
     const scale = options.scale || 1.0;
-    let xrControllers = null;
-    const xr = view.mainLoop.gfxEngine.renderer.xr;
+    const camera = view.camera.camera3D;
 
+    const xr = view.renderer.xr;
     xr.addEventListener('sessionstart', () => {
-        const camera = view.camera.camera3D;
+        const initpos = view.controls.getCameraCoordinate().as(view.referenceCrs);
 
-        const exitXRSession = (event) => {
-            if (event.key === 'Escape') {
-                document.removeEventListener('keydown', exitXRSession);
-                xr.enabled = false;
-                view.camera.camera3D = camera;
+        // xr.enabled = true;
 
-                view.scene.scale.multiplyScalar(1 / scale);
-                view.scene.updateMatrixWorld();
 
-                shutdownXR(xr.getSession());
-                view.notifyChange(view.camera.camera3D, true);
-            }
-        };
 
         const vrHeadSet = new THREE.Object3D();
         vrHeadSet.name = 'xrHeadset';
 
-        view.scene.scale.multiplyScalar(scale);
-        view.scene.updateMatrixWorld();
+        // view.scene.scale.multiplyScalar(scale);
+        // view.scene.updateMatrixWorld();
 
-        xrControllers = initControllers(xr, vrHeadSet);
+        const xrControllers = initControllers(xr, vrHeadSet);
 
         const position = view.controls.getCameraCoordinate().as(view.referenceCrs);
         // To avoid controllers precision issues, headset should handle camera position and camera should be reset to origin
+        view.scene.add(vrHeadSet);
+        view.camera.camera3D.updateMatrixWorld(true);
+
+        const matrixWorld = new Matrix4().copy(view.camera.camera3D.matrixWorld);
+        //
+        view.camera.camera3D.getWorldPosition(vrHeadSet.position);
+        view.camera.camera3D.getWorldQuaternion(vrHeadSet.quaternion);
+        // view.camera.camera3D.getWorldPosition(view.scene.position);
+        // view.camera.camera3D.getWorldQuaternion(view.scene.quaternion);
+        // view.scene.updateMatrixWorld( true );
+        // // vrHeadSet.matrixWorld.copy( matrixWorld.invert() );
+        //
+        //
+        // vrHeadSet.updateMatrixWorld( true );
+        //
 
 
+        // xr.getReferenceSpace('local');
 
-        xr.enabled = true;
-        xr.getReferenceSpace('local');
+        // const geodesicNormal = new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 0, 1), position.geodesicNormal);
+        // // // const geodesicNormal = position.geodesicNormal;
+        // // const quat = new THREE.Quaternion(-1, 0, 0, 1).normalize().multiply(geodesicNormal);
+        // const quat = new THREE.Quaternion(0, 0, 0, 1).normalize();
+        // // // https://github.com/immersive-web/webxr/issues/1236 for high position value
+        // const trans = camera.position.clone().multiplyScalar(-scale).applyQuaternion(quat);
+        // const transform = new XRRigidTransform(trans, quat);
+        // const transform = new XRRigidTransform(new Vector3(4485948.637198923, 476198.0416370128, 4497216.056600053), quat);
+        // const mat2 = new Float64Array(transform.matrix);
+        // mat2[12] = 4485948.637198923;
+        // mat2[13] = 476198.0416370128;
+        // mat2[14] = 4497216.056600053;
+        // const transform2 = {};
+        // transform2.matrix = mat2;
 
-        const geodesicNormal = new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 0, 1), position.geodesicNormal).invert();
 
-        const quat = new THREE.Quaternion(-1, 0, 0, 1).normalize().multiply(geodesicNormal);
-        // https://github.com/immersive-web/webxr/issues/1236 for high position value
-        const trans = camera.position.clone().multiplyScalar(-scale).applyQuaternion(quat);
-        const transform = new XRRigidTransform(trans, quat);
         // here position seems ok {x: 4485948.637198923, y: 476198.0416370128, z: 4497216.056600053, w: 1}
         // const baseReferenceSpace = xr.getReferenceSpace();
         // const teleportSpaceOffset = baseReferenceSpace.getOffsetReferenceSpace(transform);
         // // there it is not anymore : originOffset Matrix is :  4485948.5, 476198.03125, 4497216
+        //
+        // vrHeadSet.matrixWorld.copy( matrixWorld );
+        //
+        //
+        //
+        //
+        // vrHeadSet.matrix.decompose( camera.position, camera.quaternion, camera.scale );
+        // vrHeadSet.updateMatrixWorld( true );
+        // vrHeadSet.position.set(4485948.637198923, 476198.0416370128, 4497216.056600053);
 
-        // // Must delay replacement to allow user listening to sessionstart to get original ReferenceSpace
+
+        // vrHeadSet.position.set(camera.position.x,camera.position.y, camera.position.z);
+        // vrHeadSet.quaternion.set(quat);
+        // vrHeadSet.updateMatrixWorld( true );
+
+        // // // // Must delay replacement to allow user listening to sessionstart to get original ReferenceSpace
         // setTimeout(() => {
-        //      xr.setReferenceSpace(teleportSpaceOffset);
-        //      // does a regression over controller matrixWorld update...
-        //  });
-
-// Update the camera's position directly without using XRRigidTransform
-        vrHeadSet.position.copy(position);
-
-        // Apply the same rotation to the VR headset (vrHeadSet)
-        // vrHeadSet.rotation.copy(quat);
-
-// Now apply the position and rotation to the controllers
-        xrControllers.left.position.copy(vrHeadSet.position);
-        xrControllers.right.position.copy(vrHeadSet.position);
-
-// Apply the same rotation to the controllers
-        xrControllers.left.rotation.copy(vrHeadSet.rotation);
-        xrControllers.right.rotation.copy(vrHeadSet.rotation);
-
-// Update the reference space for XR session
-//         const baseReferenceSpace = xr.getReferenceSpace();
-//         xr.setReferenceSpace(baseReferenceSpace);
-        view.scene.add(vrHeadSet);
-
-        view.scene.updateMatrixWorld();
-
-
-        view.notifyChange();
-
         // const baseReferenceSpace = xr.getReferenceSpace();
         // const teleportSpaceOffset = baseReferenceSpace.getOffsetReferenceSpace(transform);
-        // xr.setReferenceSpace(teleportSpaceOffset);
-        view.camera.camera3D = xr.getCamera();
+        //     // teleportSpaceOffset[Symbol(@@webxr-polyfill/XRReferenceSpace)].originOffset = transform2;
+        // // // there it is not anymore : originOffset Matrix is :  4485948.5, 476198.03125, 4497216
+        //     xr.setReferenceSpace(teleportSpaceOffset);
+        //
+        //     // does a regression over controller matrixWorld update...
+        // });
+
+        // view.camera.camera3D.getWorldPosition(xr.getCamera().position);
+        // view.camera.camera3D.getWorldQuaternion(xr.getCamera().quaternion);
+        // xr.getCamera().updateMatrixWorld( true );
+        // xr.getCamera().name = "alala"
+        // // view.camera.camera3D = xr.getCamera();
+        //
+        // // view.camera.cameraXR = xr.getCamera();
+        // //
+        vrHeadSet.add(xr.getCamera());
+        // // xr.getCamera().matrixAutoUpdate = true;
+        // // xr.getCamera().updateMatrixWorld( true );
+        // // vrHeadSet.updateMatrix( true );
+        // // vrHeadSet.updateMatrixWorld( true );
+        //
+        view.camera.camera3D.far = 2000000;
+        view.camera.camera3D.near = 0.1;
+        view.camera.camera3D.updateMatrixWorld( true );
+
+        view.camXR = view.camera.camera3D.clone();      // placeholder camera to initialize correctly the vr, which needs a parent
+        vrHeadSet.add(view.camXR);
+
+        // vrHeadSet.add(view.camera.camera3D);
+        //
         view.notifyChange();
 
-        view.camera.camera3D.far = 100;
-        view.camera.resize(view.camera.width, view.camera.height);
-        vrHeadSet.add(view.camera.camera3D);
+        // view.camera.camera3D.projectionMatrix.copy( cameraXR.projectionMatrix );
+        // view.camera.camera3D.projectionMatrixInverse.copy( cameraXR.projectionMatrixInverse );
 
-        document.addEventListener('keydown', exitXRSession, false);
+        // view.camera.resize(view.camera.width, view.camera.height);
+        // vrHeadSet.add(view.camera.camera3D);
+        // cameraGroup.add(view.camera.camera3D);
+        // view.scene.add(cameraGroup);
+        // cameraGroup.position.set(initpos.x, initpos.y, initpos.z);
+        // cameraGroup.rotation.set(0, 0, 0);
+        // cameraGroup.lookAt(0, 0, 0);
+
+        // document.addEventListener('keydown', exitXRSession, false);
+        // view.notifyChange();
 
         // TODO Fix asynchronization between xr and MainLoop render loops.
         // (see MainLoop#scheduleViewUpdate).
         xr.setAnimationLoop((timestamp) => {
-            if (xr.isPresenting && view.camera.camera3D.cameras[0]) {
+            if (xr.isPresenting && xr.getCamera().cameras.length > 0) {
+                // const views = xr.getFrame().getViewerPose(xr.getReferenceSpace()).views;
+                // for (let i = 0; i < views.length; i++) {
+                //     // const view1 = views[i];
+                //     // view.camera.camera3D.getWorldPosition(view1.transform.position);
+                //     // view.camera.camera3D.getWorldQuaternion(view1.transform.orientation);
+                //     // view1.transform = view.camera.camera3D.matrix;
+                // }
 
+                //
+                // // view.camera.camera3D = xr.getCamera().cameras[0];
+                xr.getCamera().updateMatrixWorld( true );
+
+                //  update itowns Camera
+                xr.getCamera().getWorldPosition(view.camera.camera3D.position);
+                xr.getCamera().getWorldQuaternion(view.camera.camera3D.quaternion);
+                // view.camXR.getWorldPosition(view.camera.camera3D.position);
+                // view.camXR.getWorldQuaternion(view.camera.camera3D.quaternion);
+                view.camera.camera3D.updateMatrixWorld( true );
+
+
+                xr.getCamera().cameras[0].updateMatrixWorld( true );
+                xr.getCamera().cameras[1].updateMatrixWorld( true );
                 if (xrControllers.left) {
                     listenGamepad(xrControllers.left);
                 }
@@ -119,24 +176,38 @@ const initializeWebXR = (view, options) => {
                 }
 
                 resyncControlCamera();
-
+                //
+                // view.camera.camera3D.matrix.copy( matrixWorld );
+                //
+                //
+                //
+                //
+                // view.camera.camera3D.matrix.decompose( camera.position, camera.quaternion, camera.scale );
+                // view.camera.camera3D.updateMatrixWorld( true );
+                //
                 if (view.scene.matrixWorldAutoUpdate === true) {
                     view.scene.updateMatrixWorld();
                 }
 
                 computeDistanceToGround();
                 updateFarDistance();
-                if (options.callback) {
-                    options.callback();
-                }
-
+                // if (options.callback) {
+                //     options.callback();
+                // }
+                //
+                // // view.camera.camera3D.getWorldPosition(vrHeadSet.position);
+                // // view.camera.camera3D.getWorldQuaternion(vrHeadSet.quaternion);
+                // view.notifyChange(xr.getCamera()[0], true);
+                // view.notifyChange(xr.getCamera()[1], true);
+                // view.camera.camera3D.updateMatrixWorld();
+                view.notifyChange(vrHeadSet, true);
                 view.notifyChange(view.camera.camera3D, true);
-
             }
 
             view.mainLoop.step(view, timestamp);
-
         });
+        // });
+        // });
     });
 
     function resyncControlCamera() {
@@ -224,7 +295,7 @@ const initializeWebXR = (view, options) => {
             this.remove(this.children[0]);
         });
         controller.addEventListener('connected', (event) => {
-           // this.add(buildController(event.data));
+            // this.add(buildController(event.data));
             // {XRInputSource} event.data
             controller.gamepad = event.data.gamepad;
             // controller.inputSource = event.data;
@@ -242,6 +313,19 @@ const initializeWebXR = (view, options) => {
 
     function bindGripController(controllerModelFactory, gripController, vrHeadSet) {
         gripController.add(controllerModelFactory.createControllerModel(gripController));
+        const righthand = new THREE.Mesh(
+            new THREE.CylinderGeometry(0.05, 0.05, 0.4, 16, 1, true),
+            new THREE.MeshBasicMaterial({
+                color: 0x00ff88,
+                wireframe: true,
+            }),
+        );
+        const controllerGrip1 = xr.getControllerGrip(1);
+        controllerGrip1.addEventListener('connected', () => {
+            controllerGrip1.add(righthand);
+        });
+        // vrHeadSet.add(controllerGrip1);
+
         vrHeadSet.add(gripController);
     }
 
