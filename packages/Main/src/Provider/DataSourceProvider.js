5c5d67b6b31f343575897b5d433d6fe7ddf618c2
Irénée Dubourg <idubourg@cirilgroup.com>
2025-02-04 14:49:22 +0100

    fix(RasterTile): partial loading for raster tiles

--- packages/Main/src/Provider/DataSourceProvider.js
+++ packages/Main/src/Provider/DataSourceProvider.js
@@ -3,7 +3,23 @@ export default {
         const layer = command.layer;
         const src = command.extentsSource;
         const dst = command.extentsDestination || src;
+        const promises = src.map((from, i) => (layer.getData(from, dst[i])));
 
-        return Promise.all(src.map((from, i) => (layer.getData(from, dst[i]))));
+        // partialLoading sets the return promise as fulfilled if at least one sub-promise is fulfilled
+        // It waits until all promises are resolved
+        if (command.partialLoading) {
+            return Promise.allSettled(promises)
+                .then((results) => {
+                    const anyFulfilledPromise = results.find(promise => promise.status === 'fulfilled');
+                    if (!anyFulfilledPromise) {
+                        // All promises failed -> reject
+                        return Promise.reject(new Error('Failed to load any data'));
+                    }
+                    return results.map(prom => (prom.value ? prom.value : null));
+                });
+        }
+
+        // Without partialLoading, the return promise is rejected as soon as any sub-promise is rejected
+        return Promise.all(promises);
     },
 };
