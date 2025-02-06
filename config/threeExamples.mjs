ffe89c794aba56b4d7a105f7d52bfe75c29c08a8
Bouillaguet Quentin <git@desplandis.fr>
2025-02-06 10:01:21 +0100

    chore(deps): bump three from 0.168.0 to 0.170.0

--- config/threeExamples.mjs
+++ config/threeExamples.mjs
@@ -5,10 +5,11 @@ export default {
         './loaders/DRACOLoader.js',
         './loaders/KTX2Loader.js',
         './loaders/DDSLoader.js',
+        './math/ColorSpaces.js',
         './utils/BufferGeometryUtils.js',
         './utils/WorkerPool.js',
         './capabilities/WebGL.js',
         './libs/ktx-parse.module.js',
-        './libs/zstddec.module.js'
+        './libs/zstddec.module.js',
     ],
 };
