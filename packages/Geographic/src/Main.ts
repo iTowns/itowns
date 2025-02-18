2be8afe06cd9eb9e62ae9b1adfa881701272fcc1
Bouillaguet Quentin <git@desplandis.fr>
2025-02-18 16:35:59 +0100

    chore(OrientationUtils): use function exports instead of default export

--- packages/Geographic/src/Main.ts
+++ packages/Geographic/src/Main.ts
@@ -3,6 +3,6 @@ export { default as Extent } from './Extent';
 export { default as Coordinates } from './Coordinates';
 export * as CRS from './Crs';
 export { default as CoordStars } from './CoordStars';
-export { default as OrientationUtils } from './OrientationUtils';
+export * as OrientationUtils from './OrientationUtils';
 export { default as Ellipsoid, ellipsoidSizes } from './Ellipsoid';
 
