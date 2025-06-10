9b8c9b4688b5fd4e70f8707d9a91bae75767ea12
ftoromanoff <francois.toromanoff@ign.fr>
2025-06-10 17:49:18 +0200

    refactor(Potree): fix potree and potree2

--- packages/Main/src/Layer/Potree2Layer.js
+++ packages/Main/src/Layer/Potree2Layer.js
@@ -145,7 +145,7 @@ class Potree2Layer extends PointCloudLayer {
          * @type {boolean}
          * @readonly
          */
-        this.isPotreeLayer = true;
+        this.isPotree2Layer = true;
 
         const resolve = this.addInitializationStep();
         this.whenReady = this.source.whenReady.then((metadata) => {
@@ -180,8 +180,20 @@ class Potree2Layer extends PointCloudLayer {
     }
 
     setRootBbox(min, max) {
-        this.root._bbox.min.set(...min);
-        this.root._bbox.max.set(...max);
+        this.root.voxelOBB.box3D.min.set(...min);
+        this.root.voxelOBB.box3D.max.set(...max);
+        this.root.voxelOBB.matrixWorldInverse = this.root.voxelOBB.matrixWorld.clone().invert();
+
+        this.root.clampOBB.copy(this.root.voxelOBB);
+        const clampBBox = this.root.clampOBB.box3D;
+        if (clampBBox.min.z < this.zmax) {
+            clampBBox.max.z = Math.min(clampBBox.max.z, this.zmax);
+        }
+        if (clampBBox.max.z > this.zmin) {
+            clampBBox.min.z = Math.max(clampBBox.min.z, this.zmin);
+        }
+
+        this.root.clampOBB.matrixWorldInverse = this.root.voxelOBB.matrixWorldInverse;
     }
 }
 
--- packages/Main/src/Layer/PotreeLayer.js
+++ packages/Main/src/Layer/PotreeLayer.js
@@ -75,8 +75,20 @@ class PotreeLayer extends PointCloudLayer {
     }
 
     setRootBbox(min, max) {
-        this.root._bbox.min.set(...min);
-        this.root._bbox.max.set(...max);
+        this.root.voxelOBB.box3D.min.set(...min);
+        this.root.voxelOBB.box3D.max.set(...max);
+        this.root.voxelOBB.matrixWorldInverse = this.root.voxelOBB.matrixWorld.clone().invert();
+
+        this.root.clampOBB.copy(this.root.voxelOBB);
+        const clampBBox = this.root.clampOBB.box3D;
+        if (clampBBox.min.z < this.zmax) {
+            clampBBox.max.z = Math.min(clampBBox.max.z, this.zmax);
+        }
+        if (clampBBox.max.z > this.zmin) {
+            clampBBox.min.z = Math.max(clampBBox.min.z, this.zmin);
+        }
+
+        this.root.clampOBB.matrixWorldInverse = this.root.voxelOBB.matrixWorldInverse;
     }
 }
 
