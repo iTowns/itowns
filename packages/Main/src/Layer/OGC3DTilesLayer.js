49ff6176758c12b852d9da11e69a15fd89558a4a
gchoqueux <gerald.choqueux@ign.fr>
2025-07-03 16:30:06 +0200

    chore: use jsm three gltfLoader directly

--- packages/Main/src/Layer/OGC3DTilesLayer.js
+++ packages/Main/src/Layer/OGC3DTilesLayer.js
@@ -515,9 +515,9 @@ class OGC3DTilesLayer extends GeometryLayer {
         /** @type{number|null} */
         let batchId;
         if (object.isPoints && index) {
-            batchId = object.geometry.getAttribute('_BATCHID')?.getX(index) ?? index;
+            batchId = object.geometry.getAttribute('_batchid')?.getX(index) ?? index;
         } else if (object.isMesh && face) {
-            batchId = object.geometry.getAttribute('_BATCHID')?.getX(face.a) ?? instanceId;
+            batchId = object.geometry.getAttribute('_batchid')?.getX(face.a) ?? instanceId;
         }
 
         if (batchId === undefined) {
