f566f0ca2ab3ef8342cf1f18a47fee0f31dbcc4b
ftoromanoff <francois.toromanoff@ign.fr>
2025-10-31 11:30:22 +0100

    fix(test): add test for CRS.defsFromWkt()

--- packages/Geographic/test/unit/crs.js
+++ packages/Geographic/test/unit/crs.js
@@ -60,3 +60,16 @@ describe('CRS assertions', function () {
         assert.equal(CRS.axisOrder('EPSG:4269'), 'neu');
     });
 });
+
+describe('CRS.defsFromWkt()', function () {
+    it('Compound coordinate system', function () {
+        const wkt = 'COMPD_CS["NAD83(2011) / UTM zone 12N + NAVD88 height - Geoid12B (metres)",PROJCS["NAD83(2011) / UTM zone 12N",GEOGCS["NAD83(2011)",DATUM["NAD83_National_Spatial_Reference_System_2011",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],AUTHORITY["EPSG","1116"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","6318"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",-111],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],AUTHORITY["EPSG","6341"]],VERT_CS["NAVD88 height",VERT_DATUM["North American Vertical Datum 1988",2005,AUTHORITY["EPSG","5103"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Up",UP],AUTHORITY["EPSG","5703"]]]';
+        const crs = CRS.defsFromWkt(wkt);
+        assert.equal(crs, 'EPSG:6341');
+    });
+    it('Simple coordinate system', function () {
+        const wkt = 'PROJCS["RGF93 / CC46",GEOGCS["RGF93",DATUM["Reseau_Geodesique_Francais_1993",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6171"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4171"]],PROJECTION["Lambert_Conformal_Conic_2SP"],PARAMETER["standard_parallel_1",45.25],PARAMETER["standard_parallel_2",46.75],PARAMETER["latitude_of_origin",46],PARAMETER["central_meridian",3],PARAMETER["false_easting",1700000],PARAMETER["false_northing",5200000],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],AUTHORITY["EPSG","3946"]]';
+        const crs = CRS.defsFromWkt(wkt);
+        assert.equal(crs, 'EPSG:3946');
+    });
+});
