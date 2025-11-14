3bd7eea1c204f6447706d1eaffbf5c2faf7f051b
Moncef Hassani <moncef.hassani03@gmail.com>
2025-11-14 17:02:21 +0100

    feat(demo): added scene ready state + allow scene to have different coordinate projection
    - onCreate promise for scene first instantiation to set ready state
    - onEnter for each entry in scene
    - Scenes manage their layers to handle dependencies, transitionToScene only manages visibility
    - better ImmersiveView types

--- examples/demo/src/Config/config.ts
+++ examples/demo/src/Config/config.ts
@@ -1,1 +1,5 @@
-export const DURATION = 3000;
+const config = {
+    DURATION: 3000,
+};
+
+export default config;
--- examples/demo/src/Utils/transitionToScene.ts
+++ examples/demo/src/Utils/transitionToScene.ts
@@ -1,78 +1,62 @@
 import * as itowns from 'itowns';
-import * as config from '../Config/config';
+import config from '../Config/config';
+import type { Scene } from '../Scenes/Scene';
 
-const moveCameraTo = (view: itowns.View, placement: {
-    coord: { long: number, lat: number },
-    range: number,
-    tilt: number,
-    heading: number,
-}) => {
-    if (!placement) { return Promise.resolve(); }
+const moveCameraTo = (view: itowns.View, placement: Scene['placement']) => itowns.CameraUtils
+    .animateCameraToLookAtTarget(view, view.camera3D, {
+        coord: placement.coord,
+        range: placement.range,
+        time: config.DURATION,
+        tilt: placement.tilt,
+        heading: placement.heading,
+    });
 
-    const targetCoord = new itowns.Coordinates('EPSG:4326', placement.coord.long, placement.coord.lat);
-
-    return itowns.CameraUtils
-        .animateCameraToLookAtTarget(view, view.camera3D, {
-            coord: targetCoord,
-            range: placement.range,
-            time: config.DURATION,
-            tilt: placement.tilt,
-            heading: placement.heading,
-        });
-};
-
-export const transitionToScene = async (scene1: any, scene2: any) => {
+export const transitionToScene = async (scene1: Scene, scene2: Scene) => {
     // disable camera controls during transition
     // scene2.view.getView().controls.states.enabled = false;
 
     // stop any ongoing camera animation
     // scene1.view.getView().controls.player.stop();
 
-    const cameraPromises = [];
 
-    if (scene1.view.id === scene2.view.id) {
-        // potentially only do this with View3D
-        cameraPromises.push(moveCameraTo(scene1.view.getView(), scene2.placement).catch(console.error));
-    } else {
-        if (scene1.view.id === 'View3D') {
-            cameraPromises.push(moveCameraTo(scene1.view.getView(), scene2.placement).catch(console.error));
-            cameraPromises[0].then(() => {
-                scene1.view.setVisible(false);
-                scene2.view.setVisible(true);
-            });
-        } else if (scene2.view.id === 'View3D') {
-            scene1.view.setVisible(false);
-            scene2.view.setVisible(true);
-            cameraPromises.push(moveCameraTo(scene2.view.getView(), scene2.placement).catch(console.error));
-        } else {
-            scene1.view.setVisible(false);
-            scene2.view.setVisible(true);
-        }
-        cameraPromises.push(moveCameraTo(scene2.view.getView(), scene2.placement).catch(console.error));
+    if (!scene2.ready) {
+        await scene2.onCreate();
     }
+    scene1.onExit?.();
+    scene2.onEnter?.();
 
-    const layersPromise = (async () => {
-        scene1.onExit();
+    const cameraPromises: Promise<void>[] = [];
 
+    if (scene1.view.id === 'View3D') {
+        cameraPromises.push(moveCameraTo(scene1.view.getView(),
+            scene2.placement).catch(console.error));
+        cameraPromises[0].then(() => {
+            scene1.view.setVisible(false);
+            scene2.view.setVisible(true);
+        });
+    } else {
+        scene1.view.setVisible(false);
+        scene2.view.setVisible(true);
+    }
+    cameraPromises.push(moveCameraTo(scene2.view.getView(),
+        scene2.placement).catch(console.error));
+
+    const layerPromise = (async () => {
         for (const layer of scene1.layers) {
-            if (scene2.layers.find((l: any) => l.id === layer.id) == null) {
+            if (scene2.layers.find(l => l.id === layer.id) == null) {
+                // @ts-expect-error visible property undefined
                 layer.visible = false;
             }
         }
 
         for (const layer of scene2.layers) {
-            if (!scene2.view.getView().getLayerById(layer.id)) {
-                scene2.view.getView().addLayer(layer);
-            } else {
-                layer.visible = true;
-            }
+            // @ts-expect-error visible property undefined
+            layer.visible = true;
         }
     })();
 
     // load layers and move camera in parallel
-    await Promise.all([...cameraPromises, layersPromise]);
-
-    scene2.onEnter();
+    await Promise.all([...cameraPromises, layerPromise]);
 
     // scene2.view.getView().controls.states.enabled = true;
 };
