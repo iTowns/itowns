d3da65ecab03cee43a84f74959d839b5f2251005
Moncef Hassani <moncef.hassani03@gmail.com>
2025-11-14 17:02:17 +0100

    feat(demo): add demo section with multiple scenes and transitions
    camera movement between scene coordinates, layers removal/additions, onEnter/onExit for extra properties

---
+++ examples/demo/Scenes/Empty.js
@@ -0,0 +1,11 @@
+import * as itowns from "itowns";
+
+export const Empty = {
+    placement: {
+        coord: { long: 80, lat: 50 },
+        range: 25000000,
+    },
+    layers: [],
+    onEnter: (view) => {},
+    onExit: (view) => {}
+};
