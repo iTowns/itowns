17f3afe307676d6c7f47a45260fda747a15c496c
Moncef Hassani <moncef.hassani03@gmail.com>
2025-11-14 17:02:25 +0100

    feat(demo): added intermediate view for cleaner camera movements between scenes

--- examples/demo/src/Utils/transitionToScene.ts
+++ examples/demo/src/Utils/transitionToScene.ts
@@ -1,19 +1,35 @@
 import * as itowns from 'itowns';
 import config from '../Config/config';
 import type { Scene } from '../Scenes/Scene';
+import View3D from '../Views/View3D';
 
-const moveCameraTo = (view: itowns.View, placement: Scene['placement']) => itowns.CameraUtils
-    .animateCameraToLookAtTarget(view, view.camera3D, {
+/**
+ * 
+ * @param view current view whose camera will be moved
+ * @param placement @see Scene.placement - tilt and heading are optional
+ * @param duration 
+ * @returns Promise<any>
+ */
+const moveCameraTo = (view: itowns.View, placement: Scene['placement'], duration=config.DURATION) => {
+    return itowns.CameraUtils.animateCameraToLookAtTarget(view, view.camera3D, {
         coord: placement.coord,
         range: placement.range,
-        time: config.DURATION,
+        time: duration,
         tilt: placement.tilt,
         heading: placement.heading,
     });
+};
 
-export const transitionToScene = async (scene1: Scene, scene2: Scene) => {
-    const view1 = scene1.view.getView();
-    const view2 = scene2.view.getView();
+/**
+ * 
+ * @param currentScene
+ * @param nextScene
+ * @returns Promise<void>
+ */
+export const transitionToScene = async (currentScene: Scene, nextScene: Scene) => {
+    const view1 = currentScene.view.getView();
+    const view2 = nextScene.view.getView();
+    const transitionView = new View3D();
 
     // disable camera controls during transition
     // @ts-expect-error controls and states property possibly undefined
@@ -29,47 +45,64 @@ export const transitionToScene = async (scene1: Scene, scene2: Scene) => {
         view1.controls.player.stop();
     }
 
-    if (!scene2.ready) {
-        await scene2.onCreate();
+    if (!nextScene.ready) {
+        await nextScene.onCreate(); // only called once per scene
     }
-    scene1.onExit?.();
-    scene2.onEnter?.();
 
-    const cameraPromises: Promise<void>[] = [];
+    // if scenes require any action on exit/enter, call them
+    currentScene.onExit?.(); 
+    nextScene.onEnter?.();
 
-    if (scene1.view.id === 'View3D') {
-        cameraPromises.push(moveCameraTo(scene1.view.getView(),
-            scene2.placement).catch(console.error));
-        cameraPromises[0].then(() => {
-            scene1.view.setVisible(false);
-            scene2.view.setVisible(true);
+    let cameraPromise: Promise<void>;
+
+    currentScene.view.setVisible(false);
+    transitionView.setVisible(true);
+
+    // set transition view camera to currentScene placement first
+    await moveCameraTo(transitionView.getView(), currentScene.placement, 1).catch(console.error);
+
+    // compute minimum range based on distance between scenes
+    const distance = currentScene.placement.coord.planarDistanceTo(nextScene.placement.coord);
+    const minRange = Math.min(Math.round(distance * config.DISTANCE_SCALER), config.MAX_RANGE);
+
+    // if current range is less than minimum range, unzoom first then move
+    // to make transition smoother
+    if (currentScene.placement.range < minRange) {
+        cameraPromise = moveCameraTo(
+            transitionView.getView(), {
+                coord: currentScene.placement.coord,
+                range: minRange,
+                tilt: 89.5,
+            },
+            config.DURATION / 2) // half duration since there are two steps in this case
+        .catch(console.error).then(() => {
+            return moveCameraTo(transitionView.getView(), nextScene.placement, config.DURATION / 2).catch(console.error);
         });
     } else {
-        scene1.view.setVisible(false);
-        scene2.view.setVisible(true);
+        cameraPromise = moveCameraTo(transitionView.getView(), nextScene.placement).catch(console.error);
     }
 
-    if (scene2.view.id === 'View3D') {
-        cameraPromises.push(moveCameraTo(scene2.view.getView(),
-            scene2.placement).catch(console.error));
-    }
+    cameraPromise.then(() => {
+        transitionView.setVisible(false);
+        nextScene.view.setVisible(true);
+    });
 
     const layerPromise = (async () => {
-        for (const layer of scene1.layers) {
-            if (scene2.layers.find(l => l.id === layer.id) == null) {
+        for (const layer of currentScene.layers) {
+            if (nextScene.layers.find(l => l.id === layer.id) == null) {
                 // @ts-expect-error visible property undefined
                 layer.visible = false;
             }
         }
 
-        for (const layer of scene2.layers) {
+        for (const layer of nextScene.layers) {
             // @ts-expect-error visible property undefined
             layer.visible = true;
         }
     })();
 
     // load layers and move camera in parallel
-    await Promise.all([...cameraPromises, layerPromise]);
+    await Promise.all([cameraPromise, layerPromise]);
 
     // @ts-expect-error controls and states property possibly undefined
     if (view2.controls && view2.controls.states) {
