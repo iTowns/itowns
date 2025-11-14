3b86fc6aa0d5794b5ca362b6cc9d507135a4a9ce
Moncef Hassani <moncef.hassani03@gmail.com>
2025-11-14 17:02:24 +0100

    feat(demo): added InstancedData scene with trees in Lyon using GLTF loader

--- examples/demo/src/Scenes/InstancedData.ts
+++ examples/demo/src/Scenes/InstancedData.ts
@@ -5,6 +5,7 @@ import * as IgnMntHighResLayer from '../Layers/IgnMntHighResLayer';
 import * as ParksLayer from '../Layers/ParksLayer';
 import * as FlatBuildingsLayer from '../Layers/FlatBuildingsLayer';
 import * as BuildingsLayer3D from '../Layers/BuildingsLayer3D';
+import * as TreesLayer from '../Layers/TreesLayer';
 import View3D from '../Views/View3D';
 import type { Scene as SceneType } from './Scene';
 
@@ -34,31 +35,35 @@ export const Scene: SceneType & { meshes: THREE.Object3D<THREE.Object3DEventMap>
     },
     onCreate: async () => {
         const view = Scene.view.getView();
+        const ambLight = new THREE.AmbientLight(0xffffff, 0.3);
+        view.scene.add(ambLight);
 
         function scaleZ(mesh: THREE.Mesh) {
-            for (let i = 0; i < mesh.children.length; i++) {
-                const c = mesh.children[i];
+            mesh.children.forEach((c) => {
                 c.scale.z = 0.01;
                 Scene.meshes.push(c);
-            }
+            });
         }
 
         const orthoLayer = await OrthoLayer.getLayer();
         const ignMntHighResLayer = await IgnMntHighResLayer.getLayer();
-        const flatBuildingsLayer = await FlatBuildingsLayer.getLayer();
         const parksLayer = await ParksLayer.getLayer();
+        const flatBuildingsLayer = await FlatBuildingsLayer.getLayer();
         const buildingsLayer3D = await BuildingsLayer3D.getLayer(scaleZ);
+        const treesLayer = await TreesLayer.getLayer();
 
+        Scene.layers.push(treesLayer as unknown as itowns.Layer);
         Scene.layers.push(orthoLayer);
         Scene.layers.push(ignMntHighResLayer);
-        Scene.layers.push(flatBuildingsLayer);
         Scene.layers.push(parksLayer);
+        Scene.layers.push(flatBuildingsLayer);
         Scene.layers.push(buildingsLayer3D as unknown as itowns.Layer);
 
+        await view.addLayer(treesLayer);
         await view.addLayer(orthoLayer);
         await view.addLayer(ignMntHighResLayer);
-        await view.addLayer(flatBuildingsLayer);
         await view.addLayer(parksLayer);
+        await view.addLayer(flatBuildingsLayer);
         await view.addLayer(buildingsLayer3D);
 
         Scene.ready = true;
