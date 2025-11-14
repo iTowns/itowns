0db51c3601395da586d867ba9bc99eb0b76d8410
Moncef Hassani <moncef.hassani03@gmail.com>
2025-11-14 17:02:23 +0100

    feat(demo): updated 2D projected data and 3D extruded data scenes to fit specification with same pov and shared layers

--- examples/demo/src/Scenes/ExtrudedData3D.ts
+++ examples/demo/src/Scenes/ExtrudedData3D.ts
@@ -1,67 +1,73 @@
-// import * as itowns from 'itowns';
+import * as itowns from 'itowns';
+import * as THREE from 'three';
+import * as OrthoLayer from '../Layers/OrthoLayer';
+import * as IgnMntHighResLayer from '../Layers/IgnMntHighResLayer';
+import * as ParksLayer from '../Layers/ParksLayer';
+import * as FlatBuildingsLayer from '../Layers/FlatBuildingsLayer';
+import * as BuildingsLayer3D from '../Layers/BuildingsLayer3D';
+import View3D from '../Views/View3D';
+import type { Scene as SceneType } from './Scene';
 
-// export const Scene4 = {
-//     placement: {
-//         coord: { long: 2.351323, lat: 48.856712 },
-//         range: 2000,
-//         tilt: 45,
-//         heading: 0,
-//     },
-//     layers: [],
-//     scaler: function update(/* dt */) {
-//         if (Scene4.meshes.length) {
-//             for (let i = 0; i < Scene4.meshes.length; i++) {
-//                 const mesh = Scene4.meshes[i];
-//                 if (mesh && mesh.scale.z < 1) {
-//                     mesh.scale.z = Math.min(1.0, mesh.scale.z + 0.005);
-//                     mesh.updateMatrixWorld(true);
-//                 }
-//             }
-//             Scene4.meshes = Scene4.meshes.filter(m => m.scale.z < 1);
-//         }
-//     },
-//     onEnter: (view) => {
-//         const func = () => {
-//             view.notifyChange(view.camera3D, true);
-//             Scene4.scaler();
-//             view.notifyChange(view.camera3D, true);
-//         };
-//         view.addFrameRequester(itowns.MAIN_LOOP_EVENTS.BEFORE_RENDER, func);
-//     },
-//     onExit: (view) => {
-//         const func = () => {
-//             view.notifyChange(view.camera3D, true);
-//             Scene4.scaler();
-//             view.notifyChange(view.camera3D, true);
-//         };
-//         view.removeFrameRequester(itowns.MAIN_LOOP_EVENTS.BEFORE_RENDER, func);
-//     },
-//     extrusion_height: 0,
-//     meshes: [],
-// };
+export const Scene: SceneType & { meshes: THREE.Object3D<THREE.Object3DEventMap>[] } = {
+    placement: {
+        coord: new itowns.Coordinates('EPSG:4326', 4.828, 45.7254),
+        range: 2000,
+        tilt: 45,
+        heading: 0,
+    },
+    layers: [],
+    view: new View3D(),
+    ready: false,
+    meshes: [],
+    event: function update(/* dt */) {
+        if (Scene.meshes.length) {
+            for (let i = 0; i < Scene.meshes.length; i++) {
+                const mesh = Scene.meshes[i];
+                if (mesh && mesh.scale.z < 1) {
+                    mesh.scale.z = Math.min(1.0, mesh.scale.z + 0.005);
+                    mesh.updateMatrixWorld(true);
+                }
+            }
+            Scene.meshes = Scene.meshes.filter(m => m.scale.z < 1);
+            Scene.view.getView().notifyChange(Scene.view.getView().camera3D, true);
+        }
+    },
+    onCreate: async () => {
+        const view = Scene.view.getView();
 
-// const buildingsSource = new itowns.VectorTilesSource({
-//     style: 'https://data.geopf.fr/annexes/ressources/vectorTiles/styles/PLAN.IGN/standard.json',
-//     filter: layer => layer['source-layer'].includes('bati_surf')
-//             && layer.paint['fill-color'],
-// });
+        function scaleZ(mesh: THREE.Mesh) {
+            mesh.children.forEach((c) => {
+                c.scale.z = 0.01;
+                Scene.meshes.push(c);
+            });
+        }
 
-// const buildingsLayer = new itowns.FeatureGeometryLayer('VTBuilding', {
-//     source: buildingsSource,
-//     zoom: { min: 15 },
-//     onMeshCreated: function scaleZ(mesh) {
-//         mesh.children.forEach((c) => {
-//             c.scale.z = 0.01;
-//             Scene4.meshes.push(c);
-//         });
-//     },
-//     accurate: false,
-//     style: {
-//         fill: {
-//             base_altitude: p => p.alti_sol || 0,
-//             extrusion_height: p => p.hauteur || 0,
-//         },
-//     },
-// });
+        const orthoLayer = await OrthoLayer.getLayer();
+        const ignMntHighResLayer = await IgnMntHighResLayer.getLayer();
+        const parksLayer = await ParksLayer.getLayer();
+        const flatBuildingsLayer = await FlatBuildingsLayer.getLayer();
+        const buildingsLayer3D = await BuildingsLayer3D.getLayer(scaleZ);
 
-// Scene4.layers.push(buildingsLayer);
+        Scene.layers.push(orthoLayer);
+        Scene.layers.push(ignMntHighResLayer);
+        Scene.layers.push(parksLayer);
+        Scene.layers.push(flatBuildingsLayer);
+        Scene.layers.push(buildingsLayer3D as unknown as itowns.Layer);
+
+        await view.addLayer(orthoLayer);
+        await view.addLayer(ignMntHighResLayer);
+        await view.addLayer(parksLayer);
+        await view.addLayer(flatBuildingsLayer);
+        await view.addLayer(buildingsLayer3D);
+
+        Scene.ready = true;
+    },
+    onEnter: () => {
+        Scene.view.getView().addFrameRequester(
+            itowns.MAIN_LOOP_EVENTS.BEFORE_RENDER, Scene.event);
+    },
+    onExit: () => {
+        Scene.view.getView().removeFrameRequester(
+            itowns.MAIN_LOOP_EVENTS.BEFORE_RENDER, Scene.event);
+    },
+};
