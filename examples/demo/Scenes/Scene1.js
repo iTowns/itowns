cff3f3e189759c7017b1e15e0c035099fa29ff0b
Moncef Hassani <moncef.hassani03@gmail.com>
2025-11-14 17:02:18 +0100

    feat(demo): sceneManager with navigation + added tilt to scenes

--- examples/demo/Scenes/Scene1.js
+++ examples/demo/Scenes/Scene1.js
@@ -2,23 +2,11 @@ import * as itowns from "itowns";
 
 export const Scene1 = {
     placement: {
-        coord: { long: 3, lat: 50 },
-        range: 250000,
+        coord: { long: 2, lat: 48 },
+        range: 25000000,
+        tilt: 90,
     },
     layers: [],
-    onEnter: (view) => {
-    },
-    onExit: (view) => {
-    }
+    onEnter: (_) => {},
+    onExit: (_) => {},
 };
-
-function addElevationLayerFromConfig(config) {
-    config.source = new itowns.WMTSSource(config.source);
-    Scene1.layers.push(new itowns.ElevationLayer(config.id, config));
-}
-await itowns.Fetcher.json("../layers/JSONLayers/IGN_MNT_HIGHRES.json").then(
-    addElevationLayerFromConfig
-);
-await itowns.Fetcher.json("../layers/JSONLayers/WORLD_DTM.json").then(
-    addElevationLayerFromConfig
-);
