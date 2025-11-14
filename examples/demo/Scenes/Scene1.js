b12b2c9f7b164910d36c2dd311c99a40404688e3
Moncef Hassani <moncef.hassani03@gmail.com>
2025-11-14 17:02:19 +0100

    fix(demo): render the same scene twice + scene heading
    - instead of removing / readding same layers which is bug-prone, don't remove and just toggle visibility
    
    - added heading to scenes to ensure camera ends up in the right spot

--- examples/demo/Scenes/Scene1.js
+++ examples/demo/Scenes/Scene1.js
@@ -1,10 +1,9 @@
-import * as itowns from "itowns";
-
 export const Scene1 = {
     placement: {
         coord: { long: 2, lat: 48 },
         range: 25000000,
         tilt: 90,
+        heading: 0,
     },
     layers: [],
     onEnter: (_) => {},
--- examples/demo/Scenes/Scene3.js
+++ examples/demo/Scenes/Scene3.js
@@ -4,64 +4,72 @@ import * as itowns from "itowns";
 export const Scene3 = {
     placement: {
         coord: { long: 3.05, lat: 48.97 },
-        range: 15000,
+        range: 30000,
         tilt: 90,
+        heading: 0,
     },
     layers: [],
     onEnter: (view) => {
-        const floodSource = new itowns.FileSource({
-            url: "https://raw.githubusercontent.com/iTowns/iTowns2-sample-data/master/multipolygon.geojson",
-            crs: "EPSG:4326",
-            format: "application/json",
-        });
-        const floodStyle = {
-            fill: {
-                color: "cyan",
-                opacity: 0.5,
-            },
-            stroke: {
-                color: "blue",
-            },
-        };
-        const floodLayer = new itowns.ColorLayer("flood", {
-            source: floodSource,
-            style: floodStyle,
-        });
-        view.addLayer(floodLayer);
-
-        const citySource = new itowns.FileSource({
-            url: "https://raw.githubusercontent.com/iTowns/iTowns2-sample-data/master/cities.geojson",
-            crs: "EPSG:4326",
-            format: "application/json",
-        });
-        const cityStyle = {
-            stroke: {
-                color: "red",
-            },
-            point: {
-                color: "white",
-                line: "red",
-                radius: 3,
-            },
-            text: {
-                field: "{name}",
-                anchor: "bottom-left",
-                size: 18,
-                haloColor: "white",
-                haloWidth: 1,
-                font: ["monospace"],
-            },
-        };
-        const cityLayer = new itowns.ColorLayer("cities", {
-            source: citySource,
-            style: cityStyle,
-            addLabelLayer: true,
-        });
-        view.addLayer(cityLayer);
+        const labelsLayer = view.getLayerById("cities-label");
+        if (labelsLayer) {
+            labelsLayer.visible = true;
+        }
     },
     onExit: (view) => {
-        view.removeLayer("flood");
-        view.removeLayer("cities");
-        // view.removeLayer("cities-label");
+        const labelsLayer = view.getLayerById("cities-label");
+        if (labelsLayer) {
+            labelsLayer.visible = false;
+        }
     },
 };
+
+const floodSource = new itowns.FileSource({
+    url: "https://raw.githubusercontent.com/iTowns/iTowns2-sample-data/master/multipolygon.geojson",
+    crs: "EPSG:4326",
+    format: "application/json",
+});
+const floodStyle = {
+    fill: {
+        color: "cyan",
+        opacity: 0.5,
+    },
+    stroke: {
+        color: "blue",
+    },
+};
+const floodLayer = new itowns.ColorLayer("flood", {
+    source: floodSource,
+    style: floodStyle,
+});
+
+const citySource = new itowns.FileSource({
+    url: "https://raw.githubusercontent.com/iTowns/iTowns2-sample-data/master/cities.geojson",
+    crs: "EPSG:4326",
+    format: "application/json",
+});
+const cityStyle = {
+    stroke: {
+        color: "red",
+    },
+    point: {
+        color: "white",
+        line: "red",
+        radius: 3,
+    },
+    text: {
+        field: "{name}",
+        anchor: "bottom-left",
+        size: 18,
+        haloColor: "white",
+        haloWidth: 1,
+        font: ["monospace"],
+    },
+};
+const cityLayer = new itowns.ColorLayer("cities", {
+    source: citySource,
+    style: cityStyle,
+    addLabelLayer: true,
+});
+
+Scene3.layers.push(floodLayer);
+Scene3.layers.push(cityLayer);
