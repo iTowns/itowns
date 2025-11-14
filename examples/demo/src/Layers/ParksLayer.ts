fc9799fd55173a345a4a82435a72edb44bf09330
Moncef Hassani <moncef.hassani03@gmail.com>
2025-11-14 17:02:23 +0100

    feat(demo): updated 2D projected data and 3D extruded data scenes to fit specification with same pov and shared layers

---
+++ examples/demo/src/Layers/ParksLayer.ts
@@ -0,0 +1,32 @@
+import * as itowns from 'itowns';
+
+let layerPromise: Promise<itowns.ColorLayer>;
+let cachedLayer: itowns.ColorLayer | undefined;
+
+export async function getLayer() {
+    if (cachedLayer) {
+        return Promise.resolve(cachedLayer);
+    }
+    if (!layerPromise) {
+        const parksSource = new itowns.FileSource({
+            url: 'https://data.grandlyon.com/fr/geoserv/ogc/features/v1/collections/metropole-de-lyon:com_donnees_communales.comparcjardin_1_0_0/items?&f=application/geo%2Bjson&crs=EPSG:4326&startIndex=0&sortby=gid',
+            crs: 'EPSG:4326',
+            format: 'application/json',
+        });
+
+        layerPromise = (async () => {
+            cachedLayer = new itowns.ColorLayer('parks', {
+                source: parksSource,
+                // @ts-expect-error style property undefined
+                style: {
+                    fill: {
+                        color: '#00FF00',
+                        opacity: 0.5,
+                    },
+                },
+            });
+            return cachedLayer;
+        })();
+    }
+    return layerPromise;
+}
