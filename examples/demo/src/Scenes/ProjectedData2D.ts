5e15af09931b33b10cb08c596bfe145cd80ee79d
Moncef Hassani <moncef.hassani03@gmail.com>
2025-11-14 17:02:23 +0100

    feat(demo): updated 2D projected data and 3D extruded data scenes to fit specification with same pov and shared layers

--- examples/demo/src/Scenes/ProjectedData2D.ts
+++ examples/demo/src/Scenes/ProjectedData2D.ts
@@ -1,75 +1,39 @@
-// /* eslint-disable quotes */
-// import * as itowns from "itowns";
+import * as itowns from 'itowns';
+import * as OrthoLayer from '../Layers/OrthoLayer';
+import * as IgnMntHighResLayer from '../Layers/IgnMntHighResLayer';
+import * as ParksLayer from '../Layers/ParksLayer';
+import * as FlatBuildingsLayer from '../Layers/FlatBuildingsLayer';
+import View3D from '../Views/View3D';
+import type { Scene as SceneType } from './Scene';
 
-// export const Scene3 = {
-//     placement: {
-//         coord: { long: 3.05, lat: 48.97 },
-//         range: 30000,
-//         tilt: 89.5,
-//         heading: 0,
-//     },
-//     layers: [],
-//     onEnter: (view) => {
-//         const labelsLayer = view.getLayerById("cities-label");
-//         if (labelsLayer) {
-//             labelsLayer.visible = true;
-//         }
-//     },
-//     onExit: (view) => {
-//         const labelsLayer = view.getLayerById("cities-label");
-//         if (labelsLayer) {
-//             labelsLayer.visible = false;
-//         }
-//     },
-// };
+export const Scene: SceneType = {
+    placement: {
+        coord: new itowns.Coordinates('EPSG:4326', 4.828, 45.7254),
+        range: 2000,
+        tilt: 45,
+        heading: 0,
+    },
+    layers: [],
+    view: new View3D(),
+    ready: false,
+    onCreate: async () => {
+        const view = Scene.view.getView();
 
-// const floodSource = new itowns.FileSource({
-//     url: "https://raw.githubusercontent.com/iTowns/iTowns2-sample-data/master/multipolygon.geojson",
-//     crs: "EPSG:4326",
-//     format: "application/json",
-// });
-// const floodStyle = {
-//     fill: {
-//         color: "cyan",
-//         opacity: 0.5,
-//     },
-//     stroke: {
-//         color: "blue",
-//     },
-// };
-// const floodLayer = new itowns.ColorLayer("flood", {
-//     source: floodSource,
-//     style: floodStyle,
-// });
+        const orthoLayer = await OrthoLayer.getLayer();
+        const ignMntHighResLayer = await IgnMntHighResLayer.getLayer();
+        const flatBuildingsLayer = await FlatBuildingsLayer.getLayer();
+        const parksLayer = await ParksLayer.getLayer();
 
-// const citySource = new itowns.FileSource({
-//     url: "https://raw.githubusercontent.com/iTowns/iTowns2-sample-data/master/cities.geojson",
-//     crs: "EPSG:4326",
-//     format: "application/json",
-// });
-// const cityStyle = {
-//     stroke: {
-//         color: "red",
-//     },
-//     point: {
-//         color: "white",
-//         line: "red",
-//         radius: 3,
-//     },
-//     text: {
-//         field: "{name}",
-//         anchor: "bottom-left",
-//         size: 18,
-//         haloColor: "white",
-//         haloWidth: 1,
-//         font: ["monospace"],
-//     },
-// };
-// const cityLayer = new itowns.ColorLayer("cities", {
-//     source: citySource,
-//     style: cityStyle,
-//     addLabelLayer: true,
-// });
+        Scene.layers.push(orthoLayer);
+        Scene.layers.push(ignMntHighResLayer);
+        Scene.layers.push(flatBuildingsLayer);
+        Scene.layers.push(parksLayer);
 
-// Scene3.layers.push(floodLayer);
-// Scene3.layers.push(cityLayer);
+        await view.addLayer(orthoLayer);
+        await view.addLayer(ignMntHighResLayer);
+        await view.addLayer(flatBuildingsLayer);
+        await view.addLayer(parksLayer);
+
+        Scene.ready = true;
+    },
+};
