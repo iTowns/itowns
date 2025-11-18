24121265b6734342dc712b0dfb406192383a66f9
ftoromanoff <francois.toromanoff@ign.fr>
2025-11-18 11:51:26 +0100

    fix(test): vpc test time out

--- packages/Main/test/unit/vpc.js
+++ packages/Main/test/unit/vpc.js
@@ -4,17 +4,45 @@ import GlobeView from 'Core/Prefab/GlobeView';
 import { Coordinates, CRS } from '@itowns/geographic';
 import VpcSource from 'Source/VpcSource';
 import VpcLayer from 'Layer/VpcLayer';
+import sinon from 'sinon';
+import Fetcher from 'Provider/Fetcher';
 import Renderer from './bootstrap';
 
-const vpcEptUrl = 'https://data.geopf.fr/chunk/telechargement/download/lidarhd_fxx_ept/vpc/index.vpc';
-const vpcCopcUrl = 'https://data.geopf.fr/chunk/telechargement/download/LiDARHD-NUALID/VPC/amiens.vpc';
+import eptStack from '../data/vpc/eptStack.json';
+import copcStack from '../data/vpc/copcStack.json';
+
+import eptFile from '../data/entwine/ept.json';
+import eptHierarchyFile from '../data/entwine/ept-hierarchy/0-0-0-0.json';
+
+// url of the vpc stack
+const baseurl = 'https://data.geopf.fr/chunk/telechargement/download';
+const vpcEptUrl = `${baseurl}/lidarhd_fxx_ept/vpc/index.vpc`;
+const vpcCopcUrl = `${baseurl}/LiDARHD-NUALID/VPC/amiens.vpc`;
+
+const resources = {
+    [vpcEptUrl]: JSON.parse(eptStack),
+    [vpcCopcUrl]: JSON.parse(copcStack),
+    [`${baseurl}/lidarhd_fxx_ept/kg/ept.json`]: JSON.parse(eptFile),
+    [`${baseurl}/lidarhd_fxx_ept/kg/ept-hierarchy/0-0-0-0.json`]: JSON.parse(eptHierarchyFile),
+};
+
 CRS.defs('EPSG:2154', '+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
 
 describe('VPC', function () {
     let vpcEptSource;
     let vpcCopcSource;
 
-    describe('Source', function () {
+    describe('VPC Source', function () {
+        let stubFetcherJson;
+        before(function () {
+            stubFetcherJson = sinon.stub(Fetcher, 'json')
+                .callsFake(url => Promise.resolve(resources[url]));
+        });
+
+        after(async function () {
+            stubFetcherJson.restore();
+        });
+
         describe('ept stack', function () {
             it('instantiation', function () {
                 vpcEptSource = new VpcSource({
@@ -30,7 +58,7 @@ describe('VPC', function () {
                         assert.ok(vpcEptSource.sources.length > 0);
                         done();
                     }).catch(done);
-            }).timeout(5000);
+            });
         });
 
         describe('copc stack', function () {
@@ -48,31 +76,31 @@ describe('VPC', function () {
                         assert.ok(vpcCopcSource.sources.length > 0);
                         done();
                     }).catch(done);
-            }).timeout(5000);
-        });
-
-        describe('stack sources', function (done) {
-            it('ept stack', function () {
-                const eptMockSource = vpcEptSource.sources[0];
-                vpcEptSource.instantiate(eptMockSource);
-
-                eptMockSource.whenReady
-                    .then(() => {
-                        assert.ok(vpcEptSource.sources[0].isEntwinePointTileSource);
-                        done();
-                    }).catch(done);
-            });
-            it('copc stack', function () {
-                const copcMockSource = vpcCopcSource.sources[0];
-                vpcCopcSource.instantiate(copcMockSource);
-
-                copcMockSource.whenReady
-                    .then(() => {
-                        assert.ok(vpcCopcSource.sources[0].isCopcSource);
-                        done();
-                    }).catch(done);
             });
         });
+    });
+
+    describe('Stacked sources', function () {
+        it('instantiate ept stacked source', function (done) {
+            const eptMockSource = vpcEptSource.sources[0];
+            vpcEptSource.instantiate(eptMockSource);
+
+            eptMockSource.whenReady
+                .then(() => {
+                    assert.ok(vpcEptSource.sources[0].isEntwinePointTileSource);
+                    done();
+                }).catch(done);
+        }).timeout(10000);
+        it('instanciated copc stacked source', function (done) {
+            const copcMockSource = vpcCopcSource.sources[0];
+            vpcCopcSource.instantiate(copcMockSource);
+
+            copcMockSource.whenReady
+                .then(() => {
+                    assert.ok(vpcCopcSource.sources[0].isCopcSource);
+                    done();
+                }).catch(done);
+        }).timeout(10000);
     });
 
     describe('Layer', function () {
@@ -97,9 +125,19 @@ describe('VPC', function () {
                 }).catch(done);
         });
 
-        let context;
         describe('loadData()', () => {
             let node;
+            let context;
+
+            let stubFetcherJson;
+            before(function () {
+                stubFetcherJson = sinon.stub(Fetcher, 'json')
+                    .callsFake(url => Promise.resolve(resources[url]));
+            });
+
+            after(async function () {
+                stubFetcherJson.restore();
+            });
 
             it('on a mockRoot', async function () {
                 context = {
@@ -110,16 +148,18 @@ describe('VPC', function () {
                     view,
                 };
 
-                const mockRoot = vpcLayer.root.children[0];
-                assert.equal(vpcEptSource.sources[0].isEntwinePointTileSource, undefined, 'source already instanciated');
+                const sources = vpcLayer.source.sources;
+                assert.equal(sources[1].isEntwinePointTileSource, undefined, 'source already instantiated');
+                const mockRoot = vpcLayer.root.children[1];
                 vpcLayer.loadData(mockRoot, context, vpcLayer, mockRoot.bbox);
 
                 await mockRoot.source.whenReady;
-                assert.ok(vpcEptSource.sources[0].isEntwinePointTileSource);
+                assert.ok(sources[1].isEntwinePointTileSource);
                 const root = await mockRoot.loadOctree;
                 node = root.children[0];
                 assert.ok(root.numPoints > 0);
             });
+
             it('on a "commun" node', async function () {
                 vpcLayer.loadData(node, context, vpcLayer, node.bbox);
                 if (node.obj) {
