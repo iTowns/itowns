a0d74026491418012b648770aa73018af18625c1
Victor Ripplinger <vripplinger@cirilgroup.com>
2025-12-03 18:01:01 +0100

    refactor: update style method to accept collection context in Feature2Mesh

--- packages/Main/src/Process/FeatureProcessing.js
+++ packages/Main/src/Process/FeatureProcessing.js
@@ -29,7 +29,7 @@ export default {
 
                 // update existing features
                 for (const featureMesh of f.meshes.children) {
-                    Feature2Mesh.updateStyle(featureMesh, { style: layer.style });
+                    Feature2Mesh.updateStyle(featureMesh, f.collection, layer.style);
                 }
             });
             return;
