eb1c6892a26fcfc1abd55a1188469281d5c276ae
ftoromanoff <francois.toromanoff@ign.fr>
2025-12-05 17:26:22 +0100

    test(potreenode): add potree unit tests file

--- packages/Main/test/unit/nodes.js
+++ packages/Main/test/unit/nodes.js
@@ -2,5 +2,5 @@ import './node/pointcloudnode';
 
 // import './node/entwinenode';
 // import './node/copcnode';
-// import './node/potreenode';
+import './node/potreenode';
 // import './node/potree2node';
