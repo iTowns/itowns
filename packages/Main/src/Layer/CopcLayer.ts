ef8be5e2179c31a7b957a0b92a71d02ff38e7ac6
BOUILLAGUET Quentin <git@desplandis.fr>
2025-12-09 13:38:38 +0100

    chore(pointcloud): migrate layers to typescript

--- packages/Main/src/Layer/CopcLayer.ts
+++ packages/Main/src/Layer/CopcLayer.ts
@@ -1,48 +1,49 @@
+import PointCloudLayer, { type PointCloudLayerParameters } from 'Layer/PointCloudLayer';
 import CopcNode from 'Core/CopcNode';
-import PointCloudLayer from 'Layer/PointCloudLayer';
+import type CopcSource from 'Source/CopcSource';
+
+interface CopcLayerParameters extends PointCloudLayerParameters {
+    source : CopcSource;
+}
 
 /**
  * A layer for [Cloud Optimised Point Cloud](https://copc.io) (COPC) datasets.
  * See {@link PointCloudLayer} class for documentation on base properties.
  *
- * @extends {PointCloudLayer}
- *
  * @example
  * // Create a new COPC layer
- * const copcSource = new CopcSource({
+ * const copcSource = new CopcSource(\{
  *     url: 'https://s3.amazonaws.com/hobu-lidar/autzen-classified.copc.laz',
  *     crs: 'EPSG:4978',
  *     colorDepth: 16, // bit-depth of 'color' attribute (either 8 or 16 bits)
- * });
+ * \});
  *
- * const copcLayer = new CopcLayer('COPC', {
+ * const copcLayer = new CopcLayer('COPC', \{
  *     source: copcSource,
- * });
+ * \});
  *
  * View.prototype.addLayer.call(view, copcLayer);
  */
 class CopcLayer extends PointCloudLayer {
-    /**
-     * @param {string} id - Unique id of the layer.
-     * @param {Object} config - See {@link PointCloudLayer} for base pointcloud
-     * options.
-     */
-    constructor(id, config) {
-        super(id, config);
+    readonly isCopcLayer: true;
 
-        /**
-         * @type {boolean}
-         * @readonly
-         */
+    source: CopcSource;
+    /**
+     * @param id - Unique id of the layer.
+     * @param config - See {@link PointCloudLayer} for base pointcloud options.
+     */
+    constructor(id: string, config: CopcLayerParameters) {
+        super(id, config);
         this.isCopcLayer = true;
+        this.source = config.source;
 
         const resolve = super.addInitializationStep();
-        this.whenReady = this.source.whenReady.then((/** @type {CopcSource} */ source) => {
+        this.whenReady = this.source.whenReady.then((source) => {
             this.setElevationRange();
 
-            const { pageOffset, pageLength } = source.info.rootHierarchyPage;
+            const { rootHierarchyPage, cube } = source.info;
+            const { pageOffset, pageLength } = rootHierarchyPage;
             this.root = new CopcNode(0, 0, 0, 0, pageOffset, pageLength, source, -1, this.crs);
-            const { cube } = source.info;
             this.root.setOBBes(cube.slice(0, 3), cube.slice(3, 6));
 
             return this.root.loadOctree().then(resolve);
--- packages/Main/src/Layer/EntwinePointTileLayer.ts
+++ packages/Main/src/Layer/EntwinePointTileLayer.ts
@@ -1,49 +1,45 @@
+import PointCloudLayer, { type PointCloudLayerParameters } from 'Layer/PointCloudLayer';
 import EntwinePointTileNode from 'Core/EntwinePointTileNode';
-import PointCloudLayer from 'Layer/PointCloudLayer';
+import type EntwinePointTileSource from 'Source/EntwinePointTileSource';
 
-/**
- * @property {boolean} isEntwinePointTileLayer - Used to checkout whether this
- * layer is a EntwinePointTileLayer. Default is `true`. You should not change
- * this, as it is used internally for optimisation.
- *
- * @extends PointCloudLayer
- */
-class EntwinePointTileLayer extends PointCloudLayer {
+interface EntwinePointTileLayerParameters extends PointCloudLayerParameters {
+    crs?: string;
+    source: EntwinePointTileSource;
+}
+
+class EntwinePointTileLayer extends PointCloudLayer<EntwinePointTileSource> {
+    /** Used to checkout whether this
+     * layer is a EntwinePointTileLayer. Default is `true`. You should not
+     * change this, as it is used internally for optimisation. */
+    readonly isEntwinePointTileLayer: true;
+
     /**
      * Constructs a new instance of Entwine Point Tile layer.
      *
      * @example
      * // Create a new point cloud layer
      * const points = new EntwinePointTileLayer('EPT',
-     *  {
-     *      source: new EntwinePointTileSource({
+     *  \{
+     *      source: new EntwinePointTileSource(\{
      *          url: 'https://server.geo/ept-dataset',
-     *      }
-     *  });
+     *      \}
+     *  \});
      *
      * View.prototype.addLayer.call(view, points);
      *
-     * @param {string} id - The id of the layer, that should be unique. It is
+     * @param id - The id of the layer, that should be unique. It is
      * not mandatory, but an error will be emitted if this layer is added a
-     * {@link View} that already has a layer going by that id.
-     * @param {Object} config - Configuration, all elements in it
+     * View that already has a layer going by that id.
+     * @param config - Configuration, all elements in it
      * will be merged as is in the layer. For example, if the configuration
      * contains three elements `name, protocol, extent`, these elements will be
      * available using `layer.name` or something else depending on the property
      * name. See the list of properties to know which one can be specified.
      */
-    constructor(id, config) {
+    constructor(id: string, config: EntwinePointTileLayerParameters) {
         super(id, config);
 
-        /**
-         * @type {boolean}
-         * @readonly
-         */
         this.isEntwinePointTileLayer = true;
-
-        /**
-         * @type {THREE.Vector3}
-         */
 
         const resolve = this.addInitializationStep();
         this.whenReady = this.source.whenReady.then(() => {
