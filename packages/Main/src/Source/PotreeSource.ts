39e0b27610a4ceb2ddaedbe9654c2a369f562bb9
BOUILLAGUET Quentin <git@desplandis.fr>
2025-12-09 13:38:38 +0100

    chore(pointcloud): migrate pointcloud sources to typescript

--- packages/Main/src/Source/PotreeSource.ts
+++ packages/Main/src/Source/PotreeSource.ts
@@ -3,28 +3,72 @@ import Fetcher from 'Provider/Fetcher';
 import PotreeBinParser from 'Parser/PotreeBinParser';
 import PotreeCinParser from 'Parser/PotreeCinParser';
 
+type PotreeBBox = {
+    lx: number; ly: number; lz: number;
+    ux: number; uy: number; uz: number;
+}
+
+interface PotreeCloud {
+    boundingBox: PotreeBBox;
+    tightBoundingBox: PotreeBBox;
+    pointAttributes: string[];
+    spacing: number;
+    scale: number;
+    hierarchyStepSize: number;
+    octreeDir: string;
+}
+
+interface PotreeSourceParameters {
+    url: string;
+    file: string;
+    crs: string;
+    cloud?: PotreeCloud;
+    networkOptions?: RequestInit;
+}
+
 /**
- * PotreeSource are object containing informations on how to fetch points cloud resources.
+ * PotreeSource are object containing informations on how to fetch
+ * points cloud resources.
+ *
+ * @param url - folder url.
+ * @param file - cloud file name.
  */
 
 class PotreeSource extends Source {
+    file: string;
+    extensionOctree: 'hrc';
+
+    // Properties initialized after fetching cloud file
+    boundsConforming!: [number, number, number, number, number, number];
+    pointAttributes!: string[];
+    baseurl!: string;
+    extension!: 'cin' | 'bin';
+    scale!: number;
+    zmin!: number;
+    zmax!: number;
+    spacing!: number;
+    hierarchyStepSize!: number;
+
     /**
-     * @param {Object} source - An object that can contain all properties of a
-     * PotreeSource
-     * @param {string} source.url - folder url.
-     * @param {string} source.file - cloud file name.
+     * @param source - An object that can contain all properties of a
+     * PotreeSource.
      *
-     * This `cloud` file stores information about the potree cloud in JSON format. the structure is :
-     *
-     * * __`version`__ - The cloud.js format may change over time. The version number is
-     * necessary so that parsers know how to interpret the data.
-     * * __`octreeDir`__ - Directory or URL where node data is stored. Usually points to
-     * "data".
-     * * __`boundingBox`__ - Contains the minimum and maximum of the axis aligned bounding box. This bounding box is cubic and aligned to fit to the octree root.
-     * * __`tightBoundingBox`__ - This bounding box thightly fits the point data.
-     * * __`pointAttributes`__ - Declares the point data format. May be 'LAS', 'LAZ' or in case if the BINARY format an array of attributes like
-     * `['POSITION_CARTESIAN', 'COLOR_PACKED', 'INTENSITY']`
-     * * __`POSITION_CARTESIAN`__ - 3 x 32bit signed integers for x/y/z coordinates
+     * This `cloud` file stores information about the potree cloud in
+     * JSON format. The structure is :
+     * * __`version`__ - The cloud.js format may change over time. The version
+     * number is necessary so that parsers know how to interpret the data.
+     * * __`octreeDir`__ - Directory or URL where node data is stored.
+     *          Usually points to "data".
+     * * __`boundingBox`__ - Contains the minimum and maximum of the axis
+     *          aligned bounding box.
+     * This bounding box is cubic and aligned to fit to the octree root.
+     * * __`tightBoundingBox`__ - This bounding box thightly fits
+     *          the point data.
+     * * __`pointAttributes`__ - Declares the point data format. May be 'LAS',
+     *          'LAZ' or in case if the BINARY format an array of attributes
+     *          like `['POSITION_CARTESIAN', 'COLOR_PACKED', 'INTENSITY']`
+     * * __ `POSITION_CARTESIAN`__ - 3 x 32bit signed integers for x/y/z
+     *          coordinates
      * * __`COLOR_PACKED`__ - 4 x unsigned byte for r,g,b,a colors.
      * * __`spacing`__ - The minimum distance between points at root level.
      * ```
@@ -57,9 +101,8 @@ class PotreeSource extends Source {
      * }
      * ```
      *
-     * @extends Source
      */
-    constructor(source) {
+    constructor(source: PotreeSourceParameters) {
         if (!source.file) {
             throw new Error('New PotreeSource: file is required');
         }
@@ -75,7 +118,10 @@ class PotreeSource extends Source {
 
         // For cloud specification visit:
         // https://github.com/PropellerAero/potree-propeller-private/blob/master/docs/file_format.md#cloudjs
-        this.whenReady = (source.cloud ? Promise.resolve(source.cloud) : Fetcher.json(`${this.url}/${this.file}`, this.networkOptions))
+        this.whenReady = (
+            source.cloud ?
+                Promise.resolve(source.cloud) :
+            Fetcher.json(`${this.url}/${this.file}`, this.networkOptions) as Promise<PotreeCloud>)
             .then((cloud) => {
                 this.boundsConforming = [
                     cloud.tightBoundingBox.lx,
@@ -87,8 +133,10 @@ class PotreeSource extends Source {
                 ];
                 this.pointAttributes = cloud.pointAttributes;
                 this.baseurl = `${this.url}/${cloud.octreeDir}/r`;
+                // @ts-expect-error non-standard CIN extension, shall be removed
                 this.extension = cloud.pointAttributes === 'CIN' ? 'cin' : 'bin';
-                this.parser = this.extension === 'cin' ? PotreeCinParser.parse : PotreeBinParser.parse;
+                this.parser = this.extension === 'cin' ?
+                    PotreeCinParser.parse : PotreeBinParser.parse;
                 this.scale = cloud.scale;
 
                 this.zmin = cloud.tightBoundingBox.lz;
