02bd7b5791b4c81f7999a84dcb512a5eab8b3d17
Gerald Choqueux <gerald.choqueux@ign.fr>
2025-12-19 16:51:52 +0100

    chore: update to three v182

--- packages/Main/src/Renderer/Shader/Chunk/mode_depth_fragment.glsl
+++ packages/Main/src/Renderer/Shader/Chunk/mode_depth_fragment.glsl
@@ -1,4 +1,4 @@
-#if defined(USE_LOGDEPTHBUF)
+#if defined(USE_LOGARITHMIC_DEPTH_BUFFER)
 gl_FragColor = packDepthToRGBA(gl_FragDepthEXT);
 #else
 float fragCoordZ = 0.5 * vHighPrecisionZW[0] / vHighPrecisionZW[1] + 0.5;
