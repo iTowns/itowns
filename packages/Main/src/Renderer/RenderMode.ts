744770f6774657fa3ed4c8a3d832126b005ab518
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2026-01-13 12:49:09 +0100

    wip

---
+++ packages/Main/src/Renderer/RenderMode.ts
@@ -0,0 +1,64 @@
+import * as THREE from 'three';
+
+const MODES = {
+    FINAL: 0, // final color
+    DEPTH: 1, // depth buffer
+    ID: 2,    // id object
+};
+
+type RenderMode = typeof MODES[keyof typeof MODES];
+
+type Object = THREE.Object3D & { mode: RenderMode };
+
+function scope<T>(
+    objects: Array<Object>,
+    mode: RenderMode,
+    callback: (objects: Array<Object>) => T,
+): T {
+    const oldModes = objects.map(obj => obj.mode ?? MODES.FINAL);
+    for (const obj of objects) {
+        obj.traverse((node) => {
+            // @ts-expect-error Don't know quite what type this is yet
+            const material = node.material;
+            if (material) {
+                material.mode = mode;
+            }
+        });
+    }
+    const res = callback(objects);
+    for (const [index, obj] of objects.entries()) {
+        obj.traverse((node) => {
+            // @ts-expect-error Don't know quite what type this is yet
+            const material = node.material;
+            if (material) {
+                material.mode = oldModes[index];
+            }
+        });
+    }
+    return res;
+}
+
+// @ts-expect-error Old
+function push(object3d, mode) {
+    const _mode = object3d.mode ?? MODES.FINAL;
+    if (_mode == mode) {
+        return () => { };
+    }
+
+    // @ts-expect-error Old
+    const setMode = m => ((node) => {
+        const material = node.material;
+        if (material) {
+            material.mode = m;
+        }
+    });
+
+    object3d.traverse(setMode(mode));
+    return () => { object3d.traverse(setMode(_mode)); };
+}
+
+// Rendering mode
+// According to the rendering mode, the material's object switches
+// the mode property of the materials
+export default { MODES, push, scope };
+
