d295077f09088bd9d1472b81587c29036a24800e
ftoromanoff <francois.toromanoff@ign.fr>
2026-01-14 17:21:33 +0100

    refactor(OBB): move node.OBBes computation to OBB.ts

--- packages/Main/src/Core/LasNodeBase.ts
+++ packages/Main/src/Core/LasNodeBase.ts
@@ -96,15 +96,7 @@ abstract class LasNodeBase extends PointCloudNode {
 
         // get a clamped bbox from the voxel bbox
         childNode.clampOBB.copy(childNode.voxelOBB);
-
-        const childClampBBox = childNode.clampOBB.box3D;
-
-        if (childClampBBox.min.z < this.source.zmax) {
-            childClampBBox.max.z = Math.min(childClampBBox.max.z, this.source.zmax);
-        }
-        if (childClampBBox.max.z > this.source.zmin) {
-            childClampBBox.min.z = Math.max(childClampBBox.min.z, this.source.zmin);
-        }
+        childNode.clampOBB.clampZ(this.source.zmin, this.source.zmax);
 
         (this.clampOBB.parent as Group).add(childNode.clampOBB);
         childNode.clampOBB.updateMatrixWorld(true);
