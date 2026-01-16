4dedf1dbc060de93af569f6643e0166e268b5597
BOUILLAGUET Quentin <git@desplandis.fr>
2026-01-16 09:59:48 +0100

    feat(pointcloud): add load, error and dispose events

--- packages/Main/src/Layer/PointCloudLayer.ts
+++ packages/Main/src/Layer/PointCloudLayer.ts
@@ -375,7 +375,12 @@ abstract class PointCloudLayer<S extends PointCloudSource = PointCloudSource>
             } else if (!elt.promise) {
                 const distance = Math.max(0.001, distanceToCamera);
                 // Increase priority of nearest node
-                const priority = computeScreenSpaceError(context, layer.pointSize, elt.pointSpacing, distance) / distance;
+                const priority = computeScreenSpaceError(
+                    context,
+                    layer.pointSize,
+                    elt.pointSpacing,
+                    distance,
+                ) / distance;
                 elt.promise = context.scheduler.execute({
                     layer,
                     requester: elt,
@@ -391,7 +396,9 @@ abstract class PointCloudLayer<S extends PointCloudSource = PointCloudSource>
                     this.group.add(elt.obj);
                     elt.obj.updateMatrixWorld(true);
                     context.view.notifyChange(this);
+                    this.dispatchEvent({ type: 'load-model', scene: pts, tile: elt });
                 }).catch((err: { isCancelledCommandException: boolean }) => {
+                    this.dispatchEvent({ type: 'load-error', tile: elt, error: err });
                     if (!err.isCancelledCommandException) {
                         return err;
                     }
@@ -402,7 +409,12 @@ abstract class PointCloudLayer<S extends PointCloudSource = PointCloudSource>
         }
 
         if (elt.children && elt.children.length) {
-            elt.sse = computeScreenSpaceError(context, layer.pointSize, elt.pointSpacing, distanceToCamera) / this.sseThreshold;
+            elt.sse = computeScreenSpaceError(
+                context,
+                layer.pointSize,
+                elt.pointSpacing,
+                distanceToCamera,
+            ) / this.sseThreshold;
             if (elt.sse >= 1) {
                 return elt.children;
             } else {
@@ -471,7 +483,8 @@ abstract class PointCloudLayer<S extends PointCloudSource = PointCloudSource>
         }
 
         if (this.displayedCount > this.pointBudget) {
-            // 2 different point count limit implementation, depending on the potree source
+            // 2 different point count limit implementation, depending on the
+            // potree source
             if (this.supportsProgressiveDisplay) {
                 // In this format, points are evenly distributed within a node,
                 // so we can draw a percentage of each node and still get a
@@ -515,9 +528,11 @@ abstract class PointCloudLayer<S extends PointCloudSource = PointCloudSource>
                 // remove from group
                 this.group.children.splice(i, 1);
 
-                // no need to dispose obj.material, as it is shared by all objects of this layer
+                // no need to dispose obj.material, as it is shared by all
+                // objects of this layer
                 obj.geometry.dispose();
                 obj.userData.node.obj = null;
+                this.dispatchEvent({ type: 'dispose-model', scene: obj, tile: obj.userData.node });
             }
         }
     }
