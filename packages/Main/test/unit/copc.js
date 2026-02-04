ee66bb2cd25c9f7668bcb931ff38059dae526b03
ftoromanoff <francois.toromanoff@ign.fr>
2026-02-04 14:56:07 +0100

    test(PointCloudNode): fix: add crs parameter when instantiating a node

--- packages/Main/test/unit/copc.js
+++ packages/Main/test/unit/copc.js
@@ -44,28 +44,33 @@ describe('COPC', function () {
 
     describe('COPC Node', function () {
         let root;
+        const crs = 'EPSG:4978';
         before('create octree', function () {
             const object3d = new Object3D();
-            const source = { url: 'http://server.geo', extension: 'laz' };
-            root = new CopcNode(0, 0, 0, 0, 0, 1000, source, 4000);
+            const source = {
+                url: 'http://server.geo',
+                extension: 'laz',
+                crs,
+            };
+            root = new CopcNode(0, 0, 0, 0, 0, 1000, source, 4000, crs);
             object3d.add(root.clampOBB);
             root.voxelOBB.box3D.setFromArray([1000, 1000, 1000, 0, 0, 0]);
 
-            root.add(new CopcNode(1, 0, 0, 0, 0, 1000, source, 3000));
-            root.add(new CopcNode(1, 0, 0, 1, 0, 1000, source, 3000));
-            root.add(new CopcNode(1, 0, 1, 1, 0, 1000, source, 3000));
+            root.add(new CopcNode(1, 0, 0, 0, 0, 1000, source, 3000, crs));
+            root.add(new CopcNode(1, 0, 0, 1, 0, 1000, source, 3000, crs));
+            root.add(new CopcNode(1, 0, 1, 1, 0, 1000, source, 3000, crs));
 
-            root.children[0].add(new CopcNode(2, 0, 0, 0, 0, 1000, source, 2000));
-            root.children[0].add(new CopcNode(2, 0, 1, 0, 0, 1000, source, 2000));
-            root.children[1].add(new CopcNode(2, 0, 1, 3, 0, 1000, source, 2000));
-            root.children[2].add(new CopcNode(2, 0, 2, 2, 0, 1000, source, 2000));
-            root.children[2].add(new CopcNode(2, 0, 3, 3, 0, 1000, source, 2000));
+            root.children[0].add(new CopcNode(2, 0, 0, 0, 0, 1000, source, 2000, crs));
+            root.children[0].add(new CopcNode(2, 0, 1, 0, 0, 1000, source, 2000, crs));
+            root.children[1].add(new CopcNode(2, 0, 1, 3, 0, 1000, source, 2000, crs));
+            root.children[2].add(new CopcNode(2, 0, 2, 2, 0, 1000, source, 2000, crs));
+            root.children[2].add(new CopcNode(2, 0, 3, 3, 0, 1000, source, 2000, crs));
 
-            root.children[0].children[0].add(new CopcNode(3, 0, 0, 0, 0, 1000, source, 1000));
-            root.children[0].children[0].add(new CopcNode(3, 0, 1, 0, 0, 1000, source, 1000));
-            root.children[1].children[0].add(new CopcNode(3, 0, 2, 7, 0, 1000, source, 1000));
-            root.children[2].children[0].add(new CopcNode(3, 0, 5, 4, 0, 1000, source, 1000));
-            root.children[2].children[1].add(new CopcNode(3, 1, 6, 7, 0, 1000, source));
+            root.children[0].children[0].add(new CopcNode(3, 0, 0, 0, 0, 1000, source, 1000, crs));
+            root.children[0].children[0].add(new CopcNode(3, 0, 1, 0, 0, 1000, source, 1000, crs));
+            root.children[1].children[0].add(new CopcNode(3, 0, 2, 7, 0, 1000, source, 1000, crs));
+            root.children[2].children[0].add(new CopcNode(3, 0, 5, 4, 0, 1000, source, 1000, crs));
+            root.children[2].children[1].add(new CopcNode(3, 1, 6, 7, 0, 1000, source, 10, crs));
         });
 
         describe('finds the common ancestor of two nodes', () => {
