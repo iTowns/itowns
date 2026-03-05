c574a9f067efc0c083a3f3d5143f7345d476bc5b
Victor Ripplinger <vripplinger@cirilgroup.com>
2026-03-05 10:59:40 +0100

    fix: tests

--- test/functional/preload-jsdom.cjs
+++ test/functional/preload-jsdom.cjs
@@ -5,6 +5,19 @@ const { pathToFileURL } = require('url');
 
 const cleanup = globalJsdom();
 
+// mock document.createElementNS(...).createSVGMatrix
+const window = $jsdom.window;
+const document = window.document;
+const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+function fakeCreateElementNS() {
+    const parser = new window.DOMParser();
+    const doc = parser.parseFromString(svg.outerHTML, 'image/svg+xml');
+    const el = doc.documentElement;
+    el.createSVGMatrix = () => {};
+    return el;
+}
+document.createElementNS = fakeCreateElementNS;
+
 // Load ESM bootstrap after creating JSDOM to run its polyfills on the JSDOM window/document
 (async () => {
     const bootstrapPath = path.resolve(process.cwd(), 'packages/Main/test/unit/bootstrap.js');
