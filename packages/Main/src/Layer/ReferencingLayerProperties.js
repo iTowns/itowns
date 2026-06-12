iTowns/itowns

commit 74811a183fbce7a0da4ff8937843a5ff47e78aa3

feat(style): implement runtime style changes for feature meshes

packages/Main/src/Layer/ReferencingLayerProperties.js
@@ -4,18 +4,21 @@ function ReferLayerProperties(material, layer) {
     if (layer && layer.isGeometryLayer) {
         material.layer = layer;
 
+        const getOpacity = (opacity) => {
+            const styleOpacity = material.layer.style?.fill?.opacity ?? 1;
+            const layerOpacity = material.layer.opacity;
+            return layerOpacity * (opacity ?? styleOpacity);
+        };
+
         let opacity;
         if (material.uniforms && material.uniforms.opacity != undefined) {
             opacity = material.uniforms.opacity.value;
             Object.defineProperty(material.uniforms.opacity, 'value', {
-                get: () => material.layer.opacity * opacity,
-                set: (value) => { opacity = value; },
+                get: () => getOpacity(opacity),
             });
         } else if (material.opacity != undefined) {
-            opacity = material.opacity;
             Object.defineProperty(material, 'opacity', {
-                get: () => material.layer.opacity * opacity,
-                set: (value) => { opacity = value; },
+                get: () => getOpacity(opacity),
             });
         }
 
@@ -65,7 +68,7 @@ function ReferLayerProperties(material, layer) {
         let tPrev = transparent;
         Object.defineProperty(material, 'transparent', {
             get: () => {
-                const t = material.layer.opacity < 1.0 || transparent;
+                const t = getOpacity() < 1.0 || transparent;
                 if (t !== tPrev) {
                     material.needsUpdate = true;
                     tPrev = t;

examples/source_stream_wfs_3d.html
@@ -7,8 +7,7 @@
             "imports": {
                 "itowns": "../dist/itowns.js",
                 "debug": "../dist/debug.js",
-                "dat": "https://unpkg.com/dat.gui@0.7.9/build/dat.gui.module.js",
-                "GuiTools": "./jsm/GUI/GuiTools.js",
+                "lil": "https://unpkg.com/lil-gui@0.19.2/dist/lil-gui.esm.min.js",
                 "LoadingScreen": "./jsm/GUI/LoadingScreen.js",
                 "itowns_widgets": "../dist/itowns_widgets.js",
                 "three": "https://unpkg.com/three@0.182.0/build/three.module.js",
@@ -16,6 +15,12 @@
             }
         }
         </script>
+
+        <style type="text/css">
+            .lil-gui {
+                left: 15px;
+            }
+        </style>
 
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="css/example.css">
@@ -32,7 +37,7 @@
         </div>
         <script type="module">
 
-            import GuiTools from 'GuiTools';
+            import lil from 'lil';
             import setupLoadingScreen from 'LoadingScreen';
             import * as THREE from 'three';
             import * as itowns from 'itowns';
@@ -67,7 +72,7 @@
             itowns.Fetcher.json('./layers/JSONLayers/Ortho.json').then(function _(config) {
                 config.source = new itowns.WMTSSource(config.source);
                 var layer = new itowns.ColorLayer('Ortho', config);
-                view.addLayer(layer).then(menuGlobe.addLayerGUI.bind(menuGlobe));
+                view.addLayer(layer);
             });
 
             // Add two elevation layers.
@@ -75,7 +80,7 @@
             function addElevationLayerFromConfig(config) {
                 config.source = new itowns.WMTSSource(config.source);
                 var layer = new itowns.ElevationLayer(config.id, config);
-                view.addLayer(layer).then(menuGlobe.addLayerGUI.bind(menuGlobe));
+                view.addLayer(layer);
             }
             itowns.Fetcher.json('./layers/JSONLayers/IGN_MNT_HIGHRES.json').then(addElevationLayerFromConfig);
             itowns.Fetcher.json('./layers/JSONLayers/WORLD_DTM.json').then(addElevationLayerFromConfig);
@@ -147,18 +152,18 @@
 
             function colorBuildings(properties) {
                 if (properties.usage_1 === 'Résidentiel') {
-                    return color.set(0xFDFDFF);
+                    return '#FDFDFF';
                 } else if (properties.usage_1 === 'Annexe') {
-                    return color.set(0xC6C5B9);
+                    return '#C6C5B9';
                 } else if (properties.usage_1 === 'Commercial et services') {
-                    return color.set(0x62929E);
+                    return '#62929E';
                 } else if (properties.usage_1 === 'Religieux') {
-                    return color.set(0x393D3F);
+                    return '#393D3F';
                 } else if (properties.usage_1 === 'Sportif') {
-                    return color.set(0x546A7B);
+                    return '#546A7B';
                 }
 
-                return color.set(0x555555);
+                return '#555555';
             }
 
             function altitudeBuildings(properties) {
@@ -168,6 +173,9 @@
             function extrudeBuildings(properties) {
                 return properties.hauteur;
             }
+
+            var useFixedHeight = true;
+            var useFixedColor = true;
 
             function acceptFeature(properties) {
                 return !!properties.hauteur;
@@ -218,25 +226,33 @@
                 filter: acceptFeature,
                 source: wfsBuildingSource,
                 zoom: { min: 14 },
-
-                style: {
-                    fill: {
-                        color: colorBuildings,
-                        base_altitude: altitudeBuildings,
-                        extrusion_height: extrudeBuildings,
-                    }
-                }
+                style: { fill: { base_altitude: altitudeBuildings } }
             });
             view.addLayer(wfsBuildingLayer);
 
-            var menuGlobe = new GuiTools('menuDiv', view);
+            var debugGui = new lil();
+
+            // Add buttons to toggle extrusion height and color
+            const extrusionToggle = {
+                toggleHeight: function () {
+                    useFixedHeight = !useFixedHeight;
+                    wfsBuildingLayer.style.fill.extrusion_height = useFixedHeight ? 8 : extrudeBuildings;
+                    view.notifyChange(wfsBuildingLayer);
+                },
+                toggleColor: function () {
+                    useFixedColor = !useFixedColor;
+                    wfsBuildingLayer.style.fill.color = useFixedColor ? '#00cc00' : colorBuildings;
+                    view.notifyChange(wfsBuildingLayer);
+                }
+            };
+
             // Listen for globe full initialisation event
             view.addEventListener(itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED, function () {
                 // eslint-disable-next-line no-console
                 console.info('Globe initialized');
             });
 
-            debug.createTileDebugUI(menuGlobe.gui, view);
+            debug.createTileDebugUI(debugGui, view);
 
             function picking(event) {
                 if(view.controls.isPaused) {
@@ -269,24 +285,38 @@
             for (var layer of view.getLayers()) {
                 if (layer.id === 'WFS Bus lines') {
                     layer.whenReady.then( function _(layer) {
-                        var gui = debug.GeometryDebug.createGeometryDebugUI(menuGlobe.gui, view, layer);
+                        var gui = debug.GeometryDebug.createGeometryDebugUI(debugGui, view, layer);
                         debug.GeometryDebug.addMaterialLineWidth(gui, view, layer, 1, 10);
+                        gui.close();
                     });
                 }
                 if (layer.id === 'WFS Building') {
                     layer.whenReady.then( function _(layer) {
-                        var gui = debug.GeometryDebug.createGeometryDebugUI(menuGlobe.gui, view, layer);
+                        var gui = debug.GeometryDebug.createGeometryDebugUI(debugGui, view, layer);
                         debug.GeometryDebug.addWireFrameCheckbox(gui, view, layer);
+
+                        // Buttons to demonstrate style changes after initialization
+                        gui.add(extrusionToggle, 'toggleHeight').name('Toggle Extrusion (Property/8)');
+                        gui.add(extrusionToggle, 'toggleColor').name('Toggle Color');
+                        extrusionToggle.toggleHeight();
+                        extrusionToggle.toggleColor();
+
                         window.addEventListener('mousemove', picking, false);
+                        gui.close();
                     });
                 }
                 if (layer.id === 'WFS Route points') {
                     layer.whenReady.then( function _(layer) {
-                        var gui = debug.GeometryDebug.createGeometryDebugUI(menuGlobe.gui, view, layer);
+                        var gui = debug.GeometryDebug.createGeometryDebugUI(debugGui, view, layer);
                         debug.GeometryDebug.addMaterialSize(gui, view, layer, 1, 200);
+                        gui.close();
                     });
                 }
             }
+
+            // Close all GUI folders on start
+            debugGui.foldersRecursive().forEach(folder => folder.close());
+
             window.view = view;
             window.itowns = itowns;
             window.THREE = THREE;

packages/Main/src/Core/Style.js
@@ -1,7 +1,7 @@
 import { Coordinates } from '@itowns/geographic';
 import { LRUCache } from 'lru-cache';
 import Fetcher from 'Provider/Fetcher';
-import { Color } from 'three';
+import { Color, EventDispatcher } from 'three';
 import { deltaE } from 'Renderer/Color';
 
 import itowns_stroke_single_before from './StyleChunk/itowns_stroke_single_before.css';
@@ -83,9 +83,9 @@ function replaceWhitePxl(imgd, color, id) {
         const colorToChange = new Color('white');
         for (let i = 0, n = pix.length; i < n; i += 4) {
             const d = deltaE(pix.slice(i, i + 3), colorToChange) / 100;
-            pix[i] = (pix[i] * d +  newColor.r * 255 * (1 - d));
-            pix[i + 1] = (pix[i + 1] * d +  newColor.g * 255 * (1 - d));
-            pix[i + 2] = (pix[i + 2] * d +  newColor.b * 255 * (1 - d));
+            pix[i] = (pix[i] * d + newColor.r * 255 * (1 - d));
+            pix[i + 1] = (pix[i + 1] * d + newColor.g * 255 * (1 - d));
+            pix[i + 2] = (pix[i + 2] * d + newColor.b * 255 * (1 - d));
         }
         cachedImg.set(`${id}_${color}`, imgd);
         return imgd;
@@ -127,7 +127,7 @@ function defineStyleProperty(style, category, parameter, userValue, defaultValue
         {
             enumerable: true,
             get: () => {
-                // != to check for 'undefined' and 'null' value)
+                // != to check for 'undefined' and 'null' value
                 if (property != undefined) { return readExpression(property, style.context); }
                 const dataValue = style.context.featureStyle?.[category]?.[parameter];
                 if (dataValue != undefined) { return readExpression(dataValue, style.context); }
@@ -138,6 +138,7 @@ function defineStyleProperty(style, category, parameter, userValue, defaultValue
             },
             set: (v) => {
                 property = v;
+                style.dispatchEvent({ type: 'style-property-changed', style, parameter });
             },
         });
 }
@@ -182,6 +183,9 @@ export class StyleContext {
         this.#geometry = g;
     }
 
+    /**
+     * @param {Object} c - The collection object.
+     */
     setCollection(c) {
         this.collection = c;
         this.#localCoordinates.setCrs(c.crs);
@@ -190,6 +194,10 @@ export class StyleContext {
     setLocalCoordinatesFromArray(vertices, offset) {
         this.#worldCoordsComputed = false;
         return this.#localCoordinates.setFromArray(vertices, offset);
+    }
+
+    get geometry() {
+        return this.#geometry;
     }
 
     get properties() {
@@ -210,7 +218,7 @@ export class StyleContext {
     get coordinates() {
         if (!this.#worldCoordsComputed) {
             this.#worldCoordsComputed = true;
-            this.#worldCoord.copy(this.#localCoordinates).applyMatrix4(this.collection.matrixWorld);
+            this.#worldCoord.copy(this.#localCoordinates).applyMatrix4(this.collection.matrix);
             if (this.#localCoordinates.crs == 'EPSG:4978') {
                 return this.#worldCoord.as('EPSG:4326', this.#worldCoord);
             }
@@ -435,15 +443,31 @@ function _addIcon(icon, domElement, opt) {
  * });
  *
  * view.addLayer(layer);
+ *
+ * @example <caption>Updating style properties. Currently only works for FeatureGeometryLayer and color and extrusionHeight properties.</caption>
+ * // Update a single sub-property:
+ * layer.style.fill.color = '#00cc00';
+ *
+ * // Update several sub-properties at once via the category setter.
+ * // Only the provided keys are changed; other properties are kept intact.
+ * layer.style.fill = { color: '#00cc00', opacity: 0.8 };
+ *
+ * // WARNING: do NOT use the spread operator to "copy" the current category:
+ * //   layer.style.fill = { ...layer.style.fill, color: '#00cc00' };
+ * // The spread evaluates every getter immediately, which executes dynamic
+ * // properties (functions, expressions) without a feature context and will
+ * // either crash or freeze their values.
  */
 
-class Style {
+class Style extends EventDispatcher {
     /**
      * @param {StyleOptions} [params={}] An object that contain any properties
      * (zoom, fill, stroke, point, text or/and icon)
      * and sub properties of a Style ({@link StyleOptions}).
      */
     constructor(params = {}) {
+        super();
+
         this.isStyle = true;
         this.context = new StyleContext();
 
@@ -454,19 +478,64 @@ class Style {
         params.text = params.text || {};
         params.icon = params.icon || {};
 
-        this.zoom = {};
-        defineStyleProperty(this, 'zoom', 'min', params.zoom.min);
-        defineStyleProperty(this, 'zoom', 'max', params.zoom.max);
+        this._initZoom(params.zoom);
+        this._initFill(params.fill);
+        this._initStroke(params.stroke);
+        this._initPoint(params.point);
+        this._initText(params.text);
+        this._initIcon(params.icon);
+    }
 
-        this.fill = {};
-        defineStyleProperty(this, 'fill', 'color', params.fill.color);
-        defineStyleProperty(this, 'fill', 'opacity', params.fill.opacity, 1.0);
-        defineStyleProperty(this, 'fill', 'pattern', params.fill.pattern);
-        defineStyleProperty(this, 'fill', 'base_altitude', params.fill.base_altitude, baseAltitudeDefault);
+    /**
+     * Defines a category-level property with a setter that delegates to
+     * individual sub-property setters.
+     * This allows users to do e.g. `style.fill = { color: 'red' }` and have
+     * only the specified sub-properties updated (and their change events
+     * dispatched), while leaving the other sub-properties untouched.
+     * @param {string} category - The category name (fill, stroke, point, text, icon, zoom).
+     * @returns {Object} The category object.
+     * @private
+     */
+    _defineCategoryProperty(category) {
+        const categoryObj = {};
+        Object.defineProperty(this, category, {
+            enumerable: true,
+            configurable: true,
+            get: () => categoryObj,
+            set: (newValue) => {
+                if (newValue && typeof newValue === 'object') {
+                    // Assign only the provided keys through the existing
+                    // sub-property setters, which handle dispatching
+                    // 'style-property-changed' events individually.
+                    for (const key of Object.keys(newValue)) {
+                        categoryObj[key] = newValue[key];
+                    }
+                }
+            },
+        });
+        return categoryObj;
+    }
+
+    /** @param {Object} params - Zoom style parameters.
+     *  @private */
+    _initZoom(params) {
+        this._defineCategoryProperty('zoom');
+        defineStyleProperty(this, 'zoom', 'min', params.min);
+        defineStyleProperty(this, 'zoom', 'max', params.max);
+    }
+
+    /** @param {Object} params - Fill style parameters.
+     *  @private */
+    _initFill(params) {
+        this._defineCategoryProperty('fill');
+        defineStyleProperty(this, 'fill', 'color', params.color);
+        defineStyleProperty(this, 'fill', 'opacity', params.opacity, 1.0);
+        defineStyleProperty(this, 'fill', 'pattern', params.pattern);
+        defineStyleProperty(this, 'fill', 'base_altitude', params.base_altitude, baseAltitudeDefault);
 
         // define a special case for extrusion_height
         // to be able to know if user set it or not without calling the getter
-        this._extrusionHeight = params.fill.extrusion_height;
+        this._extrusionHeight = params.extrusion_height;
         Object.defineProperty(
             this.fill,
             'extrusion_height',
@@ -481,60 +550,81 @@ class Style {
                 },
                 set: (v) => {
                     this._extrusionHeight = v;
+                    this.dispatchEvent({
+                        type: 'style-property-changed',
+                        style: this,
+                        parameter: 'extrusion_height',
+                    });
                 },
             },
         );
+    }
 
-        this.stroke = {};
-        defineStyleProperty(this, 'stroke', 'color', params.stroke.color);
-        defineStyleProperty(this, 'stroke', 'opacity', params.stroke.opacity, 1.0);
-        defineStyleProperty(this, 'stroke', 'width', params.stroke.width, 1.0);
-        defineStyleProperty(this, 'stroke', 'dasharray', params.stroke.dasharray, []);
-        defineStyleProperty(this, 'stroke', 'base_altitude', params.stroke.base_altitude, baseAltitudeDefault);
+    /** @param {Object} params - Stroke style parameters.
+     *  @private */
+    _initStroke(params) {
+        this._defineCategoryProperty('stroke');
+        defineStyleProperty(this, 'stroke', 'color', params.color);
+        defineStyleProperty(this, 'stroke', 'opacity', params.opacity, 1.0);
+        defineStyleProperty(this, 'stroke', 'width', params.width, 1.0);
+        defineStyleProperty(this, 'stroke', 'dasharray', params.dasharray, []);
+        defineStyleProperty(this, 'stroke', 'base_altitude', params.base_altitude, baseAltitudeDefault);
+    }
 
-        this.point = {};
-        defineStyleProperty(this, 'point', 'color', params.point.color);
-        defineStyleProperty(this, 'point', 'line', params.point.line);
-        defineStyleProperty(this, 'point', 'opacity', params.point.opacity, 1.0);
-        defineStyleProperty(this, 'point', 'radius', params.point.radius, 2.0);
-        defineStyleProperty(this, 'point', 'width', params.point.width, 0.0);
-        defineStyleProperty(this, 'point', 'base_altitude', params.point.base_altitude, baseAltitudeDefault);
-        if (params.point.model) {
-            defineStyleProperty(this, 'point', 'model', params.point.model);
+    /** @param {Object} params - Point style parameters.
+     *  @private */
+    _initPoint(params) {
+        this._defineCategoryProperty('point');
+        defineStyleProperty(this, 'point', 'color', params.color);
+        defineStyleProperty(this, 'point', 'line', params.line);
+        defineStyleProperty(this, 'point', 'opacity', params.opacity, 1.0);
+        defineStyleProperty(this, 'point', 'radius', params.radius, 2.0);
+        defineStyleProperty(this, 'point', 'width', params.width, 0.0);
+        defineStyleProperty(this, 'point', 'base_altitude', params.base_altitude, baseAltitudeDefault);
+        if (params.model) {
+            defineStyleProperty(this, 'point', 'model', params.model);
         }
+    }
 
-        this.text = {};
-        defineStyleProperty(this, 'text', 'field', params.text.field);
-        defineStyleProperty(this, 'text', 'zOrder', params.text.zOrder, 'auto');
-        defineStyleProperty(this, 'text', 'color', params.text.color, '#000000');
-        defineStyleProperty(this, 'text', 'anchor', params.text.anchor, 'center');
-        defineStyleProperty(this, 'text', 'offset', params.text.offset, [0, 0]);
-        defineStyleProperty(this, 'text', 'padding', params.text.padding, 2);
-        defineStyleProperty(this, 'text', 'size', params.text.size, 16);
-        defineStyleProperty(this, 'text', 'placement', params.text.placement, 'point');
-        defineStyleProperty(this, 'text', 'rotation', params.text.rotation, 'auto');
-        defineStyleProperty(this, 'text', 'wrap', params.text.wrap, 10);
-        defineStyleProperty(this, 'text', 'spacing', params.text.spacing, 0);
-        defineStyleProperty(this, 'text', 'transform', params.text.transform, 'none');
-        defineStyleProperty(this, 'text', 'justify', params.text.justify, 'center');
-        defineStyleProperty(this, 'text', 'opacity', params.text.opacity, 1.0);
-        defineStyleProperty(this, 'text', 'font', params.text.font, ['Open Sans Regular', 'Arial Unicode MS Regular', 'sans-serif']);
-        defineStyleProperty(this, 'text', 'haloColor', params.text.haloColor, '#000000');
-        defineStyleProperty(this, 'text', 'haloWidth', params.text.haloWidth, 0);
-        defineStyleProperty(this, 'text', 'haloBlur', params.text.haloBlur, 0);
+    /** @param {Object} params - Text style parameters.
+     *  @private */
+    _initText(params) {
+        this._defineCategoryProperty('text');
+        defineStyleProperty(this, 'text', 'field', params.field);
+        defineStyleProperty(this, 'text', 'zOrder', params.zOrder, 'auto');
+        defineStyleProperty(this, 'text', 'color', params.color, '#000000');
+        defineStyleProperty(this, 'text', 'anchor', params.anchor, 'center');
+        defineStyleProperty(this, 'text', 'offset', params.offset, [0, 0]);
+        defineStyleProperty(this, 'text', 'padding', params.padding, 2);
+        defineStyleProperty(this, 'text', 'size', params.size, 16);
+        defineStyleProperty(this, 'text', 'placement', params.placement, 'point');
+        defineStyleProperty(this, 'text', 'rotation', params.rotation, 'auto');
+        defineStyleProperty(this, 'text', 'wrap', params.wrap, 10);
+        defineStyleProperty(this, 'text', 'spacing', params.spacing, 0);
+        defineStyleProperty(this, 'text', 'transform', params.transform, 'none');
+        defineStyleProperty(this, 'text', 'justify', params.justify, 'center');
+        defineStyleProperty(this, 'text', 'opacity', params.opacity, 1.0);
+        defineStyleProperty(this, 'text', 'font', params.font, ['Open Sans Regular', 'Arial Unicode MS Regular', 'sans-serif']);
+        defineStyleProperty(this, 'text', 'haloColor', params.haloColor, '#000000');
+        defineStyleProperty(this, 'text', 'haloWidth', params.haloWidth, 0);
+        defineStyleProperty(this, 'text', 'haloBlur', params.haloBlur, 0);
+    }
 
-        this.icon = {};
-        defineStyleProperty(this, 'icon', 'source', params.icon.source);
-        if (params.icon.key) {
+    /** @param {Object} params - Icon style parameters.
+     *  @private */
+    _initIcon(params) {
+        this._defineCategoryProperty('icon');
+        defineStyleProperty(this, 'icon', 'source', params.source);
+        if (params.key) {
             console.warn("'icon.key' is deprecated: use 'icon.id' instead");
-            params.icon.id = params.icon.key;
+            params.id = params.key;
         }
-        defineStyleProperty(this, 'icon', 'id', params.icon.id);
-        defineStyleProperty(this, 'icon', 'cropValues', params.icon.cropValues);
-        defineStyleProperty(this, 'icon', 'anchor', params.icon.anchor, 'center');
-        defineStyleProperty(this, 'icon', 'size', params.icon.size, 1);
-        defineStyleProperty(this, 'icon', 'color', params.icon.color);
-        defineStyleProperty(this, 'icon', 'opacity', params.icon.opacity, 1.0);
+        defineStyleProperty(this, 'icon', 'id', params.id);
+        defineStyleProperty(this, 'icon', 'cropValues', params.cropValues);
+        defineStyleProperty(this, 'icon', 'anchor', params.anchor, 'center');
+        defineStyleProperty(this, 'icon', 'size', params.size, 1);
+        defineStyleProperty(this, 'icon', 'color', params.color);
+        defineStyleProperty(this, 'icon', 'opacity', params.opacity, 1.0);
     }
 
     setContext(ctx) {
@@ -651,7 +741,7 @@ class Style {
             icon = document.createElement('img');
         }
 
-        const iconPromise  = new Promise((resolve, reject) => {
+        const iconPromise = new Promise((resolve, reject) => {
             const opt = {
                 size: this.icon.size,
                 color: this.icon.color,

packages/Main/src/Layer/FeatureGeometryLayer.js
@@ -72,7 +72,7 @@ class FeatureGeometryLayer extends GeometryLayer {
 
         this.isFeatureGeometryLayer = true;
 
-        this.style = style instanceof Style ? style : new Style(style);
+        this._style = style instanceof Style ? style : new Style(style);
         this.accurate = accurate;
         this.buildExtent = !this.accurate;
         this.filter = filter;
@@ -82,6 +82,32 @@ class FeatureGeometryLayer extends GeometryLayer {
     preUpdate(context, sources) {
         if (sources.has(this.parent)) {
             this.object3d.clear();
+        }
+    }
+
+    /**
+     * The style applied to this layer's features.
+     * When setting a plain object, only the provided categories (fill, stroke,
+     * point, text, icon, zoom) are updated on the existing Style instance,
+     * preserving event listeners. Setting a Style instance replaces it entirely.
+     * @type {Style}
+     */
+    get style() {
+        return this._style;
+    }
+
+    set style(value) {
+        if (value instanceof Style) {
+            this._style = value;
+        } else if (value && typeof value === 'object') {
+            // Update only the provided categories on the existing Style
+            // so that event listeners registered on it are preserved.
+            const categories = ['zoom', 'fill', 'stroke', 'point', 'text', 'icon'];
+            for (const cat of categories) {
+                if (value[cat] !== undefined) {
+                    this._style[cat] = value[cat];
+                }
+            }
         }
     }
 }

packages/Main/src/Process/FeatureProcessing.js
@@ -3,6 +3,7 @@ import ObjectRemovalHelper from 'Process/ObjectRemovalHelper';
 import handlingError from 'Process/handlerNodeError';
 import { Coordinates } from '@itowns/geographic';
 import { geoidLayerIsVisible } from 'Layer/GeoidLayer';
+import { applyStyle } from 'Converter/Feature2Mesh';
 
 const coord = new Coordinates('EPSG:4326', 0, 0, 0);
 
@@ -25,6 +26,23 @@ export default {
                 f.layer.object3d.add(f);
                 f.meshes.position.z = geoidLayerIsVisible(layer.parent) ? node.geoidHeight : 0;
                 f.meshes.updateMatrixWorld();
+                const updateColor = f.styleColorVersion !== (layer._styleColorVersion ?? 0);
+                const updatePosition = f.stylePositionVersion !== (layer._stylePositionVersion ?? 0);
+                if (updateColor || updatePosition) {
+                    const buffersToUpdate = [];
+                    if (updateColor) { buffersToUpdate.push('color'); }
+                    if (updatePosition) { buffersToUpdate.push('position'); }
+                    for (const mesh of f.meshes.children) {
+                        applyStyle(
+                            mesh,
+                            f.collection,
+                            layer.style,
+                            buffersToUpdate,
+                        );
+                    }
+                    f.styleColorVersion = layer._styleColorVersion ?? 0;
+                    f.stylePositionVersion = layer._stylePositionVersion ?? 0;
+                }
             });
             return;
         }

commit 891f31cb2e6aa5fb6f44882b18c0175e28caa31d

fix: fix fully transparent Mapbox buildings by preserving material intrinsic opacity
Bug was introduced in 74811a1

packages/Main/src/Layer/ReferencingLayerProperties.js
@@ -17,6 +17,7 @@ function ReferLayerProperties(material, layer) {
                 get: () => getOpacity(opacity),
             });
         } else if (material.opacity != undefined) {
+            opacity = material.opacity;
             Object.defineProperty(material, 'opacity', {
                 get: () => getOpacity(opacity),
             });